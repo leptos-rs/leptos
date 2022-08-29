96f1dd1eb659bc332c484ca381db5378ca837a53
Greg Johnston <greg.johnston@gmail.com>
2022-08-29 08:06:59 -0400

    Minimize calls to Memo function by wrapping it in its own type with distinct functionality

--- leptos_reactive/src/memo.rs
+++ leptos_reactive/src/memo.rs
@@ -1,20 +1,80 @@
 use crate::{create_effect, create_signal, ReadSignal, Scope};
 use std::fmt::Debug;
 
-pub type Memo<T> = ReadSignal<T>;
+#[derive(Debug, PartialEq, Eq)]
+pub struct Memo<T>(ReadSignal<Option<T>>)
+where
+    T: 'static;
 
 pub fn create_memo<T>(cx: Scope, mut f: impl FnMut(Option<T>) -> T + 'static) -> Memo<T>
 where
     T: Clone + Debug + 'static,
 {
-    let initial = f(None);
-    let (read, set) = create_signal(cx, initial);
+    let (read, set) = create_signal(cx, None);
 
     create_effect(cx, move |prev| {
         let new = f(prev);
-        set(|n| *n = new.clone());
+        set(|n| *n = Some(new.clone()));
         new
     });
 
-    read
+    Memo(read)
+}
+
+impl<T> Clone for Memo<T>
+where
+    T: 'static,
+{
+    fn clone(&self) -> Self {
+        Self(self.0)
+    }
+}
+
+impl<T> Copy for Memo<T> {}
+
+impl<T> Memo<T>
+where
+    T: Debug,
+{
+    pub fn get(&self) -> T
+    where
+        T: Clone,
+    {
+        self.with(T::clone)
+    }
+
+    pub fn with<U>(&self, f: impl Fn(&T) -> U) -> U {
+        // okay to unwrap here, because the value will *always* have initially
+        // been set by the effect, synchronously
+        self.0.with(|n| f(n.as_ref().unwrap()))
+    }
+}
+
+impl<T> FnOnce<()> for Memo<T>
+where
+    T: Debug + Clone,
+{
+    type Output = T;
+
+    extern "rust-call" fn call_once(self, _args: ()) -> Self::Output {
+        self.get()
+    }
+}
+
+impl<T> FnMut<()> for Memo<T>
+where
+    T: Debug + Clone,
+{
+    extern "rust-call" fn call_mut(&mut self, _args: ()) -> Self::Output {
+        self.get()
+    }
+}
+
+impl<T> Fn<()> for Memo<T>
+where
+    T: Debug + Clone,
+{
+    extern "rust-call" fn call(&self, _args: ()) -> Self::Output {
+        self.get()
+    }
 }
