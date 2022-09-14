9bea0a85aed5281dbd746ae15d319adda99a947b
Greg Johnston <greg.johnston@gmail.com>
2022-09-14 10:14:18 -0400

    Improved flexibility in <Link href/> acceptable types

--- router/src/components/link.rs
+++ router/src/components/link.rs
@@ -7,15 +7,43 @@ use wasm_bindgen::JsCast;
 
 use crate::{use_location, use_resolved_path, State};
 
+pub trait ToHref {
+    fn to_href(&self) -> Box<dyn Fn() -> String + '_>;
+}
+
+impl ToHref for &str {
+    fn to_href(&self) -> Box<dyn Fn() -> String> {
+        let s = self.to_string();
+        Box::new(move || s.clone())
+    }
+}
+
+impl ToHref for String {
+    fn to_href(&self) -> Box<dyn Fn() -> String> {
+        let s = self.clone();
+        Box::new(move || s.clone())
+    }
+}
+
+impl<F> ToHref for F
+where
+    F: Fn() -> String + 'static,
+{
+    fn to_href(&self) -> Box<dyn Fn() -> String + '_> {
+        Box::new(self)
+    }
+}
+
 #[derive(TypedBuilder)]
-pub struct LinkProps<C>
+pub struct LinkProps<C, H>
 where
     C: IntoChild,
+    H: ToHref + 'static,
 {
     // <Link/> props
     /// Used to calculate the link's `href` attribute. Will be resolved relative
     /// to the current route.
-    to: String,
+    to: H,
     /// An object of any type that will be pushed to router state
     #[builder(default, setter(strip_option))]
     state: Option<State>,
@@ -28,11 +56,12 @@ where
 }
 
 #[allow(non_snake_case)]
-pub fn Link<C>(cx: Scope, mut props: LinkProps<C>) -> Element
+pub fn Link<C, H>(cx: Scope, mut props: LinkProps<C, H>) -> Element
 where
     C: IntoChild,
+    H: ToHref + 'static,
 {
-    let href = use_resolved_path(cx, move || props.to.clone());
+    let href = use_resolved_path(cx, move || props.to.to_href()());
 
     if props.children.len() != 1 {
         debug_warn!("[Link] Pass exactly one child to <Link/>. If you want to pass more than one child, next them within an element.");
@@ -51,14 +80,15 @@ where
 }
 
 #[derive(TypedBuilder)]
-pub struct NavLinkProps<C>
+pub struct NavLinkProps<C, H>
 where
     C: IntoChild,
+    H: ToHref + 'static,
 {
     // <Link/> props
     /// Used to calculate the link's `href` attribute. Will be resolved relative
     /// to the current route.
-    to: String,
+    to: H,
     /// If `true`, the link is marked active when the location matches exactly;
     /// if false, link is marked active if the current route starts with it.
     #[builder(default)]
@@ -75,12 +105,13 @@ where
 }
 
 #[allow(non_snake_case)]
-pub fn NavLink<C>(cx: Scope, mut props: NavLinkProps<C>) -> Element
+pub fn NavLink<C, H>(cx: Scope, mut props: NavLinkProps<C, H>) -> Element
 where
     C: IntoChild,
+    H: ToHref + 'static,
 {
     let location = use_location(cx);
-    let href = use_resolved_path(cx, move || props.to.clone());
+    let href = use_resolved_path(cx, move || props.to.to_href()());
     let is_active = create_memo(cx, move |_| match href() {
         None => false,
 
