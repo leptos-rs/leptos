d59147e3551df46ee1ec1560c422c0131ed1993f
Greg Johnston <greg.johnston@gmail.com>
2022-09-14 10:15:45 -0400

    use_query, use_query_map, use_navigate hooks

--- router/src/hooks.rs
+++ router/src/hooks.rs
@@ -1,6 +1,11 @@
+use std::rc::Rc;
+
 use leptos_reactive::{create_memo, use_context, Memo, Scope};
 
-use crate::{Location, Params, ParamsMap, RouteContext, RouterContext, RouterError};
+use crate::{
+    Location, NavigateOptions, NavigationError, Params, ParamsMap, RouteContext, RouterContext,
+    RouterError,
+};
 
 pub fn use_router(cx: Scope) -> RouterContext {
     if let Some(router) = use_context::<RouterContext>(cx) {
@@ -32,8 +37,27 @@ pub fn use_params_map(cx: Scope) -> ParamsMap {
     route.params()
 }
 
+pub fn use_query<T: Params>(cx: Scope) -> Memo<Result<T, RouterError>>
+where
+    T: PartialEq + std::fmt::Debug + Clone,
+{
+    let router = use_router(cx);
+    create_memo(cx, move |_| {
+        router.inner.location.query.with(|m| T::from_map(m))
+    })
+}
+
+pub fn use_query_map(cx: Scope) -> Memo<ParamsMap> {
+    use_router(cx).inner.location.query
+}
+
 pub fn use_resolved_path(cx: Scope, path: impl Fn() -> String + 'static) -> Memo<Option<String>> {
     let route = use_route(cx);
 
     create_memo(cx, move |_| route.resolve_path(&path()).map(String::from))
+}
+
+pub fn use_navigate(cx: Scope) -> impl Fn(&str, NavigateOptions) -> Result<(), NavigationError> {
+    let router = use_router(cx);
+    move |to, options| Rc::clone(&router.inner).navigate_from_route(to, &options)
 }
