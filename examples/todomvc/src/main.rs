fd3443b129e7a6c17c6b5d80dbd46b38afc03f0c
Greg Johnston <greg.johnston@gmail.com>
2022-11-05 23:27:36 -0400

    Fix TodoMVC example

--- examples/todomvc/src/main.rs
+++ examples/todomvc/src/main.rs
@@ -4,5 +4,5 @@ fn main() {
     console_error_panic_hook::set_once();
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
-    mount_to_body(|cx| view! { cx,  <TodoMVC todos=Todos::new(cx)/> })
+    mount_to_body(|cx| view! { cx,  <TodoMVC/> })
 }
