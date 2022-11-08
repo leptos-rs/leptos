acd20a24aca251b56e1fbbb1858b93edecbe7a67
Greg Johnston <greg.johnston@gmail.com>
2022-11-07 21:49:34 -0500

    Add `leptos_core` docs

--- leptos_core/src/suspense.rs
+++ leptos_core/src/suspense.rs
@@ -3,6 +3,8 @@ use leptos_dom::{Child, IntoChild};
 use leptos_macro::Props;
 use leptos_reactive::{provide_context, Scope, SuspenseContext};
 
+/// Props for the [Suspense](crate::Suspense) component, which shows a fallback
+/// while [Resource](leptos_reactive::Resource)s are being read.
 #[derive(Props)]
 pub struct SuspenseProps<F, E, G>
 where
@@ -10,10 +12,54 @@ where
     E: IntoChild,
     G: Fn() -> E,
 {
-    fallback: F,
-    children: Box<dyn Fn() -> Vec<G>>,
+    /// Will be displayed while resources are pending.
+    pub fallback: F,
+    /// Will be displayed once all resources have resolved.
+    pub children: Box<dyn Fn() -> Vec<G>>,
 }
 
+/// If any [Resource](leptos_reactive::Resource)s are read in the `children` of this
+/// component, it will show the `fallback` while they are loading. Once all are resolved,
+/// it will render the `children`.
+///
+/// Note that the `children` will be rendered initially (in order to capture the fact that
+/// those resources are read under the suspense), so you cannot assume that resources have
+/// `Some` value in `children`.
+///
+/// ```
+/// # use leptos_reactive::*;
+/// # use leptos_core::*;
+/// # use leptos_macro::*;
+/// # use leptos_dom::*;
+/// # run_scope(|cx| {
+/// async fn fetch_cats(how_many: u32) -> Result<Vec<String>, ()> { Ok(vec![]) }
+/// let (cat_count, set_cat_count) = create_signal::<u32>(cx, 1);
+/// let cats = create_resource(cx, cat_count, |count| fetch_cats(count));
+/// view! { cx,
+///   <div>
+///     <Suspense fallback={"Loading (Suspense Fallback)...".to_string()}>
+///       {move || {
+///           cats.read().map(|data| match data {
+///             Err(_) => view! { cx,  <pre>"Error"</pre> },
+///             Ok(cats) => view! { cx,
+///               <div>{
+///                 cats.iter()
+///                   .map(|src| {
+///                     view! { cx,
+///                       <img src={src}/>
+///                     }
+///                   })
+///                   .collect::<Vec<_>>()
+///               }</div>
+///             },
+///           })
+///         }
+///       }
+///     </Suspense>
+///   </div>
+/// };
+/// # });
+/// ```
 #[allow(non_snake_case)]
 pub fn Suspense<F, E, G>(cx: Scope, props: SuspenseProps<F, E, G>) -> impl Fn() -> Child
 where
