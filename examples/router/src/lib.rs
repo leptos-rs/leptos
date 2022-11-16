96238c553e414664e0a688cf6e5804af4fc9dfe8
Greg Johnston <greg.johnston@gmail.com>
2022-11-16 07:24:21 -0500

    Fix `router` example rendering

--- examples/router/src/lib.rs
+++ examples/router/src/lib.rs
@@ -121,19 +121,19 @@ pub fn Contact(cx: Scope) -> Element {
 }
 
 #[component]
-pub fn About(_cx: Scope) -> Vec<Element> {
+pub fn About(_cx: Scope) -> Element {
     view! { cx,
-        <>
+        <div>
             <h1>"About"</h1>
             <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-        </>
+        </div>
     }
 }
 
 #[component]
-pub fn Settings(_cx: Scope) -> Vec<Element> {
+pub fn Settings(_cx: Scope) -> Element {
     view! { cx,
-        <>
+        <div>
             <h1>"Settings"</h1>
             <form>
                 <fieldset>
@@ -143,6 +143,6 @@ pub fn Settings(_cx: Scope) -> Vec<Element> {
                 </fieldset>
                 <pre>"This page is just a placeholder."</pre>
             </form>
-        </>
+        </div>
     }
 }
