c17c6549cf724e74efcc55934fc30aaaba969933
Greg Johnston <greg.johnston@gmail.com>
2022-11-16 20:15:29 -0500

    Resolve ambiguous main import error

--- examples/counter-isomorphic/src/lib.rs
+++ examples/counter-isomorphic/src/lib.rs
@@ -9,7 +9,7 @@ cfg_if! {
         use crate::counters::*;
 
         #[wasm_bindgen]
-        pub fn main() {
+        pub fn hydrate() {
             console_error_panic_hook::set_once();
             _ = console_log::init_with_level(log::Level::Debug);
             console_error_panic_hook::set_once();
