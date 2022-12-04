cc538f8427ad0d312774bda10b707274ecef98b3
Jose Quesada <jquesada2016@fau.edu>
2022-12-04 17:22:03 -0600

    added more events

--- leptos_dom/src/events/typed.rs
+++ leptos_dom/src/events/typed.rs
@@ -3,6 +3,8 @@
 use std::borrow::Cow;
 use wasm_bindgen::convert::FromWasmAbi;
 
+use crate::IntoElement;
+
 /// A trait for converting types into [web_sys events](web_sys).
 pub trait IntoEvent {
   /// The [`web_sys`] event type, such as [`web_sys::MouseEvent`].
@@ -18,6 +20,22 @@ pub trait IntoEvent {
   /// otherwise, event listeners will be directly attached to the element.
   fn bubbles(&self) -> bool {
     true
+  }
+}
+
+/// Overrides the [`IntoEvent::bubbles`] method to always return
+/// `false`, which forces the event to not be globally delegated.
+pub struct Undelegated<Ev: IntoEvent>(Ev);
+
+impl<Ev: IntoEvent> IntoEvent for Undelegated<Ev> {
+  type EventType = Ev::EventType;
+
+  fn name(&self) -> Cow<'static, str> {
+    self.0.name()
+  }
+
+  fn bubbles(&self) -> bool {
+    false
   }
 }
 
@@ -46,5 +64,89 @@ macro_rules! generate_event_types {
 
 generate_event_types![
   // ClipboardEvent is unstable
-  [Event, [Copy, Cut, Paste]]
+  [Event, [Copy, Cut, Paste]],
+  [
+    CompositionEvent,
+    [CompositionEnd, CompositionStart, CompositionUpdate]
+  ],
+  [KeyboardEvent, [KeyDown, Keypress, Keyup]],
+  [FocusEvent, [Focus, FocusOut, FocusIn, Blur]],
+  [FormEvent, [Change, Input, Invalid, Reset, Submit]],
+  [
+    MouseEvent,
+    [
+      Click,
+      ContextMenu,
+      DoubleClick,
+      DblClick,
+      Drag,
+      DragEnd,
+      DragEnter,
+      DragExit,
+      DragLeave,
+      DragOver,
+      DragStart,
+      Drop,
+      MouseDown,
+      MouseEnter,
+      MouseLeave,
+      MouseMove,
+      MouseOut,
+      MouseOver,
+      MouseUp,
+    ]
+  ],
+  [ScrollEvent, [Scroll]],
+  [
+    PointerEvent,
+    [
+      PointerDown,
+      PointerMove,
+      PointerUp,
+      PointerCancel,
+      GotPointerCapture,
+      LostPointerCapture,
+      PointerEnter,
+      PointerLeave,
+      PointerOver,
+      PointerOut,
+    ]
+  ],
+  [SelectionEvent, [Select]],
+  [TouchEvent, [TouchCancel, TouchEnd, TouchMove, TouchStart]],
+  [WheelEvent, [Wheel]],
+  [
+    MediaEvent,
+    [
+      Abort,
+      CanPlay,
+      CanPlayThrough,
+      DurationChange,
+      Emptied,
+      Encrypted,
+      Ended,
+      Error,
+      LoadedData,
+      LoadedMetadata,
+      LoadStart,
+      Pause,
+      Play,
+      Playing,
+      Progress,
+      RateChange,
+      Seeked,
+      Seeking,
+      Stalled,
+      Suspend,
+      TimeUpdate,
+      VolumeChange,
+      Waiting,
+    ]
+  ],
+  [
+    AnimationEvent,
+    [AnimationStart, AnimationEnd, AnimationIteration,]
+  ],
+  [TransitionEvent, [TransitionEnd]],
+  [ToggleEvent, [Toggle]]
 ];
