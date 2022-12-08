2694d2e93ce1ad8c2cc0782077f3ca20c5f2aea2
hakesson <henrik@akesson.mobi>
2022-12-08 08:10:56 +0100

    Add missing init param

--- integrations/actix/src/lib.rs
+++ integrations/actix/src/lib.rs
@@ -201,7 +201,7 @@ pub fn render_app_to_stream(
                     <head>
                         <meta charset="utf-8"/>
                         <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                        <script type="module">import init, {{ hydrate }} from '{pkg_path}.js'; init().then(hydrate);</script>
+                        <script type="module">import init, {{ hydrate }} from '{pkg_path}.js'; init('{pkg_path}.wasm').then(hydrate);</script>
                         {leptos_autoreload}
                         "#
             );
