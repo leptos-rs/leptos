4360a73392edcd882304ef31e84d5f542486d7a7
Greg Johnston <greg.johnston@gmail.com>
2022-12-09 14:57:58 -0500

    Fix `SimpleCounter` example

--- examples/counter/src/lib.rs
+++ examples/counter/src/lib.rs
@@ -9,11 +9,11 @@ use leptos::*;
 /// - **step** [`i32`] - The change that should be applied on each step.
 #[component]
 pub fn SimpleCounter(cx: Scope, initial_value: i32, step: i32) -> web_sys::Element {
-    let (value, set_value) = create_signal(cx, 0);
+    let (value, set_value) = create_signal(cx, initial_value);
 
     view! { cx,
         <div>
-            <button on:click=move |_| set_value(0)>"Clear"</button>
+            <button on:click=move |_| set_value(initial_value)>"Clear"</button>
             <button on:click=move |_| set_value.update(|value| *value -= step)>"-1"</button>
             <span>"Value: " {move || value().to_string()} "!"</span>
             <button on:click=move |_| set_value.update(|value| *value += step)>"+1"</button>
