b8559d4335f948a1a0e12a80c960f9ba0794c765
Greg Johnston <greg.johnston@gmail.com>
2022-12-08 19:33:01 -0500

    Transition `counter` example to use `View`

--- examples/counter/src/lib.rs
+++ examples/counter/src/lib.rs
@@ -1,6 +1,6 @@
 use leptos::*;
 
-pub fn simple_counter(cx: Scope) -> web_sys::Element {
+pub fn simple_counter(cx: Scope) -> View {
     let (value, set_value) = create_signal(cx, 0);
 
     view! { cx,
