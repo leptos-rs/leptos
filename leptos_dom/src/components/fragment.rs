e2ef293d19dfd00731b708418558102890d92fb0
Jose Quesada <jquesada2016@fau.edu>
2022-12-12 13:39:41 -0600

    added `IntoFragment` trait for all `IntoIterator` and also impl `FromIterator` for `Fragment`

--- leptos_dom/src/components/fragment.rs
+++ leptos_dom/src/components/fragment.rs
@@ -1,8 +1,38 @@
+use leptos_reactive::Scope;
+
 use crate::{ComponentRepr, IntoView, View};
+
+/// Trait for converting any iterable into a [`Fragment`].
+pub trait IntoFragment {
+  /// Consumes this type, returning [`Fragment`].
+  fn into_fragment(self, cx: Scope) -> Fragment;
+}
+
+impl<I, V> IntoFragment for I
+where
+  I: IntoIterator<Item = V>,
+  V: IntoView,
+{
+  fn into_fragment(self, cx: Scope) -> Fragment {
+    self.into_iter().map(|v| v.into_view(cx)).collect()
+  }
+}
 
 /// Represents a group of [`views`](View).
 #[derive(Debug, Clone)]
 pub struct Fragment(Vec<View>);
+
+impl FromIterator<View> for Fragment {
+  fn from_iter<T: IntoIterator<Item = View>>(iter: T) -> Self {
+    Fragment::new(iter.into_iter().collect())
+  }
+}
+
+impl From<View> for Fragment {
+  fn from(view: View) -> Self {
+    Fragment::new(vec![view])
+  }
+}
 
 impl Fragment {
   /// Creates a new [`Fragment`] from a [`Vec<Node>`].
