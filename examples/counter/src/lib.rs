89aa02af198a2b698ddb07d1bd6eeb9cd166192c
Greg Johnston <greg.johnston@gmail.com>
2022-12-22 21:21:17 -0500

    Update `counter` example

--- examples/counter/src/lib.rs
+++ examples/counter/src/lib.rs
@@ -1,14 +1,24 @@
 use leptos::*;
 
-pub fn simple_counter(cx: Scope) -> HtmlElement<Div> {
-    let (value, set_value) = create_signal(cx, 0);
+/// A simple counter component.
+/// 
+/// You can use doc comments like this to document your component.
+#[component]
+pub fn SimpleCounter(
+    cx: Scope,
+    /// The starting value for the counter
+    initial_value: i32,
+    /// The change that should be applied each time the button is clicked.
+    step: i32
+) -> impl IntoView {
+    let (value, set_value) = create_signal(cx, initial_value);
 
     view! { cx,
         <div>
             <button on:click=move |_| set_value(0)>"Clear"</button>
-            <button on:click=move |_| set_value.update(|value| *value -= 1)>"-1"</button>
+            <button on:click=move |_| set_value.update(|value| *value -= step)>"-1"</button>
             <span>"Value: " {move || value().to_string()} "!"</span>
-            <button on:click=move |_| set_value.update(|value| *value += 1)>"+1"</button>
+            <button on:click=move |_| set_value.update(|value| *value += step)>"+1"</button>
         </div>
     }
 }
