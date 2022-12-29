15b5f7545ad7534e1edee007596d575980ab2c4a
Greg Johnston <greg.johnston@gmail.com>
2022-12-29 17:35:46 -0500

    Use `<Transition/>` here for that silky-smooth optimistic UI update

--- examples/todo-app-sqlite-axum/src/todo.rs
+++ examples/todo-app-sqlite-axum/src/todo.rs
@@ -151,7 +151,7 @@ pub fn Todos(cx: Scope) -> impl IntoView {
                 </label>
                 <input type="submit" value="Add"/>
             </MultiActionForm>
-            <Suspense fallback=move || view! {cx, <p>"Loading..."</p> }>
+            <Transition fallback=move || view! {cx, <p>"Loading..."</p> }>
                 {
                     let delete_todo = delete_todo.clone();
                     move || {
@@ -221,7 +221,7 @@ pub fn Todos(cx: Scope) -> impl IntoView {
                     }
                 }
             }
-            </Suspense>
+            </Transition>
         </div>
     }
 }
