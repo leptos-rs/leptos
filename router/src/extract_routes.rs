065e7f34afb9029a0af4491f1ef0438805b846ee
Greg Johnston <greg.johnston@gmail.com>
2023-01-06 14:14:04 -0500

    Create `generate_route_list`

---
+++ router/src/extract_routes.rs
@@ -0,0 +1,33 @@
+use leptos::*;
+use std::{cell::RefCell, rc::Rc};
+
+use crate::{Branch, RouterIntegrationContext, ServerIntegration};
+
+/// Context to contain all possible routes.
+#[derive(Clone, Default, Debug)]
+pub struct PossibleBranchContext(pub(crate) Rc<RefCell<Vec<Branch>>>);
+
+/// Generates a list of all routes this application could possibly serve.
+pub fn generate_route_list<IV>(app_fn: impl FnOnce(Scope) -> IV + 'static) -> Vec<String>
+where
+    IV: IntoView + 'static,
+{
+    let runtime = create_runtime();
+    run_scope(runtime, move |cx| {
+        let integration = ServerIntegration {
+            path: "http://leptos.rs/".to_string(),
+        };
+
+        provide_context(cx, RouterIntegrationContext::new(integration));
+        let branches = PossibleBranchContext::default();
+        provide_context(cx, branches.clone());
+
+        let _ = app_fn(cx).into_view(cx).render_to_string(cx);
+
+        let branches = branches.0.borrow();
+        branches
+            .iter()
+            .flat_map(|branch| branch.routes.last().map(|route| route.pattern.clone()))
+            .collect()
+    })
+}
