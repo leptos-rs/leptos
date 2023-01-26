d4da935a0c36cbd1195722b6dd4b8108d11e3689
IcosaHedron <110272232+Indrazar@users.noreply.github.com>
2023-01-25 20:15:12 -0500

    Fix CSR with Trunk on hackernews example, remove CSR option from isomorphic example (#369)
    * Fix CSR with Trunk on hackernews example
    
    * Update isomorphic example to remove CSR from Readme

--- examples/hackernews/src/main.rs
+++ examples/hackernews/src/main.rs
@@ -46,7 +46,11 @@ cfg_if! {
         }
     } else {
         fn main() {
-            // no client-side main function
+            use hackernews::{App, AppProps};
+
+            _ = console_log::init_with_level(log::Level::Debug);
+            console_error_panic_hook::set_once();
+            mount_to_body(|cx| view! { cx, <App/> })
         }
     }
 }
