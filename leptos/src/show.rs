4c26dc597d4cf8ba8ff7557052ea818da636bd89
Greg Johnston <greg.johnston@gmail.com>
2023-01-28 08:07:23 -0500

    Docs for `<Show/>` component

--- leptos/src/show.rs
+++ leptos/src/show.rs
@@ -3,8 +3,27 @@ use leptos_dom::{Fragment, IntoView};
 use leptos_reactive::Scope;
 use once_cell::sync::Lazy;
 
-/// A component that will show it's children when the passed in closure is True, and show the fallback
-/// when the closure is false
+/// A component that will show its children when the `when` condition is `true`,
+/// and show the fallback when it is `false`, without rerendering every time
+/// the condition changes.
+///
+/// ```rust
+/// # use leptos_reactive::*;
+/// # use leptos_macro::*;
+/// # use leptos_dom::*; use leptos::*;
+/// # run_scope(create_runtime(), |cx| {
+/// let (value, set_value) = create_signal(cx, 0);
+///
+/// view! { cx,
+///   <Show
+///     when=move || value() < 5
+///     fallback=|cx| view! { cx, "Big number!" }
+///   >
+///     "Small number!"
+///   </Show>
+/// }
+/// # });
+/// ```
 #[component]
 pub fn Show<F, W, IV>(
     /// The scope the component is running in
