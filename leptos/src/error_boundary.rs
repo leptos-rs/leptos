2863d49a1c9f4f13f7c635fe76d9f6f97f867278
Greg Johnston <greg.johnston@gmail.com>
2023-01-28 07:54:13 -0500

    Docs for `<ErrorBoundary/>`

--- leptos/src/error_boundary.rs
+++ leptos/src/error_boundary.rs
@@ -2,6 +2,30 @@ use leptos_dom::{Errors, Fragment, IntoView};
 use leptos_macro::component;
 use leptos_reactive::{create_rw_signal, provide_context, RwSignal, Scope};
 
+/// When you render a `Result<_, _>` in your view, in the `Err` case it will
+/// render nothing, and search up through the view tree for an `<ErrorBoundary/>`.
+/// This component lets you define a fallback that should be rendered in that
+/// error case, allowing you to handle errors within a section of the interface.
+///
+/// ```
+/// # use leptos_reactive::*;
+/// # use leptos_macro::*;
+/// # use leptos_dom::*; use leptos::*;
+/// # if false {
+/// # run_scope(create_runtime(), |cx| {
+/// let (value, set_value) = create_signal(cx, Ok(0));
+/// let on_input = move |ev| set_value(event_target_value(&ev).parse::<i32>());
+///
+/// view! { cx,
+///   <input type="text" on:input=on_input/>
+///   <ErrorBoundary
+///     fallback=move |_, _| view! { cx, <p class="error">"Enter a valid number."</p>}
+///   >
+///     <p>"Value is: " {value}</p>
+///   </ErrorBoundary>
+/// }
+/// # });
+/// ```
 #[component(transparent)]
 pub fn ErrorBoundary<F, IV>(
     cx: Scope,
