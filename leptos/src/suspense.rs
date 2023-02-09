d906381644632b026ffd5312198fe599e61ddee4
Jose Quesada <jquesada2016@fau.edu>
2023-02-09 08:25:14 -0600

    fixed missing import and removed accidental addition of `rustfmt.toml`

--- leptos/src/suspense.rs
+++ leptos/src/suspense.rs
@@ -2,7 +2,7 @@ use cfg_if::cfg_if;
 use leptos_dom::HydrationCtx;
 use leptos_dom::{DynChild, Fragment, IntoView};
 use leptos_macro::component;
-use leptos_reactive::{provide_context, Scope, SuspenseContext};
+use leptos_reactive::{provide_context, signal_prelude::*, Scope, SuspenseContext};
 use std::rc::Rc;
 
 /// If any [Resources](leptos_reactive::Resource) are read in the `children` of this
