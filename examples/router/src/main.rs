0fa59bd89dae37a1154d1d7493cdefd8a6838fea
Greg Johnston <greg.johnston@gmail.com>
2023-02-10 17:29:57 -0500

    Restore `router` example

--- examples/router/src/main.rs
+++ examples/router/src/main.rs
@@ -5,5 +5,5 @@ pub fn main() {
     console_error_panic_hook::set_once();
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
-    mount_to_body(|cx| view! { cx, <App/> })
+    mount_to_body(|cx| view! { cx, <RouterExample/> })
 }
