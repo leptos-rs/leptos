00a796d2041f466caeb96be6d01b6b2cabc12075
Greg Johnston <greg.johnston@gmail.com>
2023-02-15 14:03:16 -0500

    change: tweak API of `Errors` and implement `IntoIter` (#522)

--- examples/todo_app_sqlite_axum/src/error_template.rs
+++ examples/todo_app_sqlite_axum/src/error_template.rs
@@ -1,8 +1,6 @@
 use crate::errors::TodoAppError;
 use cfg_if::cfg_if;
-use leptos::Errors;
-use leptos::*;
-
+use leptos::{Errors, *};
 #[cfg(feature = "ssr")]
 use leptos_axum::ResponseOptions;
 
@@ -23,14 +21,12 @@ pub fn ErrorTemplate(
     };
 
     // Get Errors from Signal
-    let errors = errors.get().0;
-
     // Downcast lets us take a type that implements `std::error::Error`
     let errors: Vec<TodoAppError> = errors
+        .get()
         .into_iter()
-        .filter_map(|(_k, v)| v.downcast_ref::<TodoAppError>().cloned())
+        .filter_map(|(_, v)| v.downcast_ref::<TodoAppError>().cloned())
         .collect();
-    println!("Errors: {errors:#?}");
 
     // Only the response code for the first error is actually sent from the server
     // this may be customized by the specific application
