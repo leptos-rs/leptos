bf06b63779c7c8cd9e6726c330abb9c3c0011e1f
Markus Kohlhase <markus.kohlhase@slowtec.de>
2023-02-24 23:11:58 +0100

    example: Login with API token (CSR only) (#523)

---
+++ examples/login_with_token_csr_only/client/src/pages/register.rs
@@ -0,0 +1,77 @@
+use leptos::*;
+use leptos_router::*;
+
+use api_boundary::*;
+
+use crate::{
+    api::{self, UnauthorizedApi},
+    components::credentials::*,
+    Page,
+};
+
+#[component]
+pub fn Register(cx: Scope, api: UnauthorizedApi) -> impl IntoView {
+    let (register_response, set_register_response) =
+        create_signal(cx, None::<()>);
+    let (register_error, set_register_error) =
+        create_signal(cx, None::<String>);
+    let (wait_for_response, set_wait_for_response) = create_signal(cx, false);
+
+    let register_action =
+        create_action(cx, move |(email, password): &(String, String)| {
+            let email = email.to_string();
+            let password = password.to_string();
+            let credentials = Credentials { email, password };
+            log!("Try to register new account for {}", credentials.email);
+            async move {
+                set_wait_for_response.update(|w| *w = true);
+                let result = api.register(&credentials).await;
+                set_wait_for_response.update(|w| *w = false);
+                match result {
+                    Ok(res) => {
+                        set_register_response.update(|v| *v = Some(res));
+                        set_register_error.update(|e| *e = None);
+                    }
+                    Err(err) => {
+                        let msg = match err {
+                            api::Error::Fetch(js_err) => {
+                                format!("{js_err:?}")
+                            }
+                            api::Error::Api(err) => err.message,
+                        };
+                        log::warn!(
+                            "Unable to register new account for {}: {msg}",
+                            credentials.email
+                        );
+                        set_register_error.update(|e| *e = Some(msg));
+                    }
+                }
+            }
+        });
+
+    let disabled = Signal::derive(cx, move || wait_for_response.get());
+
+    view! { cx,
+      <Show
+        when = move || register_response.get().is_some()
+        fallback = move |_| view!{ cx,
+          <CredentialsForm
+            title = "Please enter the desired credentials"
+            action_label = "Register"
+            action = register_action
+            error = register_error.into()
+            disabled
+          />
+          <p>"Your already have an account?"</p>
+          <A href=Page::Login.path()>"Login"</A>
+        }
+      >
+        <p>"You have successfully registered."</p>
+        <p>
+         "You can now "
+         <A href=Page::Login.path()>"login"</A>
+         " with your new account."
+        </p>
+      </Show>
+    }
+}
---
+++ examples/login_with_token_csr_only/server/src/application.rs
@@ -0,0 +1,159 @@
+use mailparse::addrparse;
+use pwhash::bcrypt;
+use std::{collections::HashMap, str::FromStr, sync::RwLock};
+use thiserror::Error;
+use uuid::Uuid;
+
+#[derive(Default)]
+pub struct AppState {
+    users: RwLock<HashMap<EmailAddress, Password>>,
+    tokens: RwLock<HashMap<Uuid, EmailAddress>>,
+}
+
+impl AppState {
+    pub fn create_user(
+        &self,
+        credentials: Credentials,
+    ) -> Result<(), CreateUserError> {
+        let Credentials { email, password } = credentials;
+        let user_exists = self.users.read().unwrap().get(&email).is_some();
+        if user_exists {
+            return Err(CreateUserError::UserExists);
+        }
+        self.users.write().unwrap().insert(email, password);
+        Ok(())
+    }
+
+    pub fn login(
+        &self,
+        email: EmailAddress,
+        password: &str,
+    ) -> Result<Uuid, LoginError> {
+        let valid_credentials = self
+            .users
+            .read()
+            .unwrap()
+            .get(&email)
+            .map(|hashed_password| hashed_password.verify(password))
+            .unwrap_or(false);
+        if !valid_credentials {
+            Err(LoginError::InvalidEmailOrPassword)
+        } else {
+            let token = Uuid::new_v4();
+            self.tokens.write().unwrap().insert(token, email);
+            Ok(token)
+        }
+    }
+
+    pub fn logout(&self, token: &str) -> Result<(), LogoutError> {
+        let token = token
+            .parse::<Uuid>()
+            .map_err(|_| LogoutError::NotLoggedIn)?;
+        self.tokens.write().unwrap().remove(&token);
+        Ok(())
+    }
+
+    pub fn authorize_user(
+        &self,
+        token: &str,
+    ) -> Result<CurrentUser, AuthError> {
+        token
+            .parse::<Uuid>()
+            .map_err(|_| AuthError::NotAuthorized)
+            .and_then(|token| {
+                self.tokens
+                    .read()
+                    .unwrap()
+                    .get(&token)
+                    .cloned()
+                    .map(|email| CurrentUser { email, token })
+                    .ok_or(AuthError::NotAuthorized)
+            })
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum CreateUserError {
+    #[error("The user already exists")]
+    UserExists,
+}
+
+#[derive(Debug, Error)]
+pub enum LoginError {
+    #[error("Invalid email or password")]
+    InvalidEmailOrPassword,
+}
+
+#[derive(Debug, Error)]
+pub enum LogoutError {
+    #[error("You are not logged in")]
+    NotLoggedIn,
+}
+
+#[derive(Debug, Error)]
+pub enum AuthError {
+    #[error("You are not authorized")]
+    NotAuthorized,
+}
+
+pub struct Credentials {
+    pub email: EmailAddress,
+    pub password: Password,
+}
+
+#[derive(Clone, Eq, PartialEq, Hash)]
+pub struct EmailAddress(String);
+
+#[derive(Debug, Error)]
+#[error("The given email address is invalid")]
+pub struct InvalidEmailAddress;
+
+impl FromStr for EmailAddress {
+    type Err = InvalidEmailAddress;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        addrparse(s)
+            .ok()
+            .and_then(|parsed| parsed.extract_single_info())
+            .map(|single_info| Self(single_info.addr))
+            .ok_or(InvalidEmailAddress)
+    }
+}
+
+impl EmailAddress {
+    pub fn into_string(self) -> String {
+        self.0
+    }
+}
+
+#[derive(Clone)]
+pub struct CurrentUser {
+    pub email: EmailAddress,
+    pub token: Uuid,
+}
+
+const MIN_PASSWORD_LEN: usize = 3;
+
+pub struct Password(String);
+
+impl Password {
+    pub fn verify(&self, password: &str) -> bool {
+        bcrypt::verify(password, &self.0)
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum InvalidPassword {
+    #[error("Password is too short (min. length is {0})")]
+    TooShort(usize),
+}
+
+impl TryFrom<String> for Password {
+    type Error = InvalidPassword;
+    fn try_from(p: String) -> Result<Self, Self::Error> {
+        if p.len() < MIN_PASSWORD_LEN {
+            return Err(InvalidPassword::TooShort(MIN_PASSWORD_LEN));
+        }
+        let hashed = bcrypt::hash(&p).unwrap();
+        Ok(Self(hashed))
+    }
+}
