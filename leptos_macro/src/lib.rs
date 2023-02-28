19d2a1dfa7814a3614ec60d773f1453fd9b641bc
Greg Johnston <greg.johnston@gmail.com>
2023-02-28 15:36:52 -0500

    fix: restore SSR fast-path support

--- leptos_macro/src/lib.rs
+++ leptos_macro/src/lib.rs
@@ -327,7 +327,7 @@ pub fn view(tokens: TokenStream) -> TokenStream {
                 Ok(nodes) => render_view(
                     &proc_macro2::Ident::new(&cx.to_string(), cx.span()),
                     &nodes,
-                    Mode::Client, //Mode::default(),
+                    Mode::default(),
                     global_class.as_ref(),
                 ),
                 Err(error) => error.to_compile_error(),
