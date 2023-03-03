2ee323135f1e057ad1e8a9a637d93c64de77838b
Roland Fredenhagen <dev@modprog.de>
2023-03-03 01:15:45 +0100

    feat: support expressions in #[prop(default=...)] (#611)

---
+++ leptos_macro/tests/component.rs
@@ -0,0 +1,23 @@
+use core::num::NonZeroUsize;
+use leptos::*;
+
+#[component]
+fn Component(
+    _cx: Scope,
+    #[prop(optional)] optional: bool,
+    #[prop(optional_no_strip)] optional_no_strip: Option<String>,
+    #[prop(strip_option)] strip_option: Option<u8>,
+    #[prop(default = NonZeroUsize::new(10).unwrap())] default: NonZeroUsize,
+    #[prop(into)] into: String,
+) -> impl IntoView {
+}
+
+#[test]
+fn component() {
+    let cp = ComponentProps::builder().into("").strip_option(9).build();
+    assert_eq!(cp.optional, false);
+    assert_eq!(cp.optional_no_strip, None);
+    assert_eq!(cp.strip_option, Some(9));
+    assert_eq!(cp.default, NonZeroUsize::new(10).unwrap());
+    assert_eq!(cp.into, "");
+}
