89ca047f2f9229597ce7f27d09bb3683f8b82cef
Greg Johnston <greg.johnston@gmail.com>
2023-03-27 12:50:01 -0400

    examples: improve `counter_without_macros` (#751)

--- examples/counter_without_macros/src/main.rs
+++ examples/counter_without_macros/src/main.rs
@@ -1,16 +1,8 @@
-use counter_without_macros as counter;
+use counter_without_macros::counter;
 use leptos::*;
 
 pub fn main() {
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
-    mount_to_body(|cx| {
-        counter::view(
-            cx,
-            counter::Props {
-                initial_value: 0,
-                step: 1,
-            },
-        )
-    })
+    mount_to_body(|cx| counter(cx, 0, 1))
 }
