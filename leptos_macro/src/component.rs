33ab2ee1d48457403e6aa56c15b40068870e82b1
Lukas Potthast <privat@lukas-potthast.de>
2023-03-27 08:12:09 +0200

    Support `leptos::` types; Extract checks to functions

--- leptos_macro/src/component.rs
+++ leptos_macro/src/component.rs
@@ -4,6 +4,7 @@ use convert_case::{
     Casing,
 };
 use itertools::Itertools;
+use once_cell::sync::Lazy;
 use proc_macro2::{Ident, TokenStream};
 use quote::{format_ident, ToTokens, TokenStreamExt};
 use syn::{
@@ -43,7 +44,7 @@ impl Parse for Model {
                 "this method requires a `Scope` parameter";
                 help = "try `fn {}(cx: Scope, /* ... */)`", item.sig.ident
             );
-        } else if props[0].ty != parse_quote!(Scope) && props[0].ty != parse_quote!(::leptos::Scope) {
+        } else if !is_valid_scope_type_decl(&props[0].ty) {
             abort!(
                 item.sig.inputs,
                 "this method requires a `Scope` parameter";
@@ -68,7 +69,7 @@ impl Parse for Model {
         });
 
         // Make sure return type is correct
-        if item.sig.output != parse_quote!(-> impl IntoView) && item.sig.output != parse_quote!(-> impl ::leptos::IntoView) {
+        if !is_valid_into_view_return_decl(&item.sig.output) {
             abort!(
                 item.sig,
                 "return type is incorrect";
@@ -436,8 +437,7 @@ impl ToTokens for TypedBuilderOpts {
 fn prop_builder_fields(vis: &Visibility, props: &[Prop]) -> TokenStream {
     props
         .iter()
-        .filter(|Prop { ty, .. }| *ty != parse_quote!(Scope))
-        .filter(|Prop { ty, .. }| *ty != parse_quote!(::leptos::Scope))
+        .filter(|Prop { ty, .. }| !is_valid_scope_type_decl(ty))
         .map(|prop| {
             let Prop {
                 docs,
@@ -464,8 +464,7 @@ fn prop_builder_fields(vis: &Visibility, props: &[Prop]) -> TokenStream {
 fn prop_names(props: &[Prop]) -> TokenStream {
     props
         .iter()
-        .filter(|Prop { ty, .. }| *ty != parse_quote!(Scope))
-        .filter(|Prop { ty, .. }| *ty != parse_quote!(::leptos::Scope))
+        .filter(|Prop { ty, .. }| !is_valid_scope_type_decl(ty))
         .map(|Prop { name, .. }| quote! { #name, })
         .collect()
 }
@@ -643,4 +642,28 @@ fn prop_to_doc(
             }
         }
     }
+}
+
+const VALID_SCOPE_TYPE_DECL: Lazy<Vec<Type>> = Lazy::new(|| {
+    vec![
+        parse_quote!(Scope),
+        parse_quote!(leptos::Scope),
+        parse_quote!(::leptos::Scope),
+    ]
+});
+
+fn is_valid_scope_type_decl(ty: &Type) -> bool {
+    VALID_SCOPE_TYPE_DECL.iter().any(|test| ty == test)
+}
+
+const VALID_INTO_VIEW_RETURN_DECL: Lazy<Vec<ReturnType>> = Lazy::new(|| {
+    vec![
+        parse_quote!(-> impl IntoView),
+        parse_quote!(-> impl leptos::IntoView),
+        parse_quote!(-> impl ::leptos::IntoView),
+    ]
+});
+
+fn is_valid_into_view_return_decl(ty: &ReturnType) -> bool {
+    VALID_INTO_VIEW_RETURN_DECL.iter().any(|test| ty == test)
 }
