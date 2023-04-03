9309c24be80c9d24df8ded1db26f1e1a1a0c3419
Greg Johnston <greg.johnston@gmail.com>
2023-04-03 14:24:17 -0400

    cargo fmt

--- meta/src/html.rs
+++ meta/src/html.rs
@@ -100,7 +100,7 @@ pub fn Html(
 ) -> impl IntoView {
     #[cfg(debug_assertions)]
     crate::feature_warning();
-    
+
     cfg_if! {
         if #[cfg(any(feature = "csr", feature = "hydrate"))] {
             let el = document().document_element().expect("there to be a <html> element");
