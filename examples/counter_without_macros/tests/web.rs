5e462d6b9048e9befccb68871b8d12e75af92e3a
Joseph Cruz <agilarity@gmail.com>
2023-04-16 19:37:20 -0400

    test: split counter tests

--- examples/counter_without_macros/tests/web.rs
+++ examples/counter_without_macros/tests/web.rs
@@ -1,5 +1,6 @@
 use counter_without_macros::counter;
 use leptos::*;
+use pretty_assertions::assert_eq;
 use wasm_bindgen::JsCast;
 use wasm_bindgen_test::*;
 use web_sys::HtmlElement;
@@ -7,24 +8,48 @@ use web_sys::HtmlElement;
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn inc() {
-    mount_to_body(move |cx| counter(cx, 0, 1));
+fn should_increment_counter() {
+    mount_counter();
 
     click_increment();
     click_increment();
 
     assert_eq!(see_text(), Some("Value: 2!".to_string()));
+}
 
-    click_decrement();
-    click_decrement();
+#[wasm_bindgen_test]
+fn should_decrement_counter() {
+    mount_counter();
+
     click_decrement();
     click_decrement();
 
     assert_eq!(see_text(), Some("Value: -2!".to_string()));
+}
+
+#[wasm_bindgen_test]
+fn should_clear_counter() {
+    mount_counter();
+
+    click_increment();
+    click_increment();
 
     click_clear();
 
     assert_eq!(see_text(), Some("Value: 0!".to_string()));
+}
+
+fn mount_counter() {
+    remove_existing_counter();
+    mount_to_body(move |cx| counter(cx, 0, 1));
+}
+
+fn remove_existing_counter() {
+    if let Some(counter) =
+        leptos::document().query_selector("body div").unwrap()
+    {
+        counter.remove();
+    }
 }
 
 fn click_clear() {
