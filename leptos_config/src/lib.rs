6e5be15735a0b0e5fe605472fe43efbb6787fd38
benwis <ben@benw.is>
2023-04-19 10:50:32 -0700

    Remove need for LEPTOS_OUTPUT_NAME env var after compilation by defaulting to value set during compilation for cargo-leptos builds

--- leptos_config/src/lib.rs
+++ leptos_config/src/lib.rs
@@ -54,11 +54,7 @@ pub struct LeptosOptions {
 impl LeptosOptions {
     fn try_from_env() -> Result<Self, LeptosConfigError> {
         Ok(LeptosOptions {
-            output_name: std::env::var("LEPTOS_OUTPUT_NAME").map_err(|e| {
-                LeptosConfigError::EnvVarError(format!(
-                    "LEPTOS_OUTPUT_NAME: {e}"
-                ))
-            })?,
+            output_name: env_w_default("LEPTOS_OUTPUT_NAME", std::env!("LEPTOS_OUTPUT_NAME"))?,
             site_root: env_w_default("LEPTOS_SITE_ROOT", "target/site")?,
             site_pkg_dir: env_w_default("LEPTOS_SITE_PKG_DIR", "pkg")?,
             env: Env::default(),
