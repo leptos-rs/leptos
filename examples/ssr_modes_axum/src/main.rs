a1b915a87892d771d48fe85b863f538afccfa415
benwis <ben@celcyon.com>
2023-04-22 18:39:38 -0700

    Will it ever end?

--- examples/ssr_modes_axum/src/main.rs
+++ examples/ssr_modes_axum/src/main.rs
@@ -1,25 +1,32 @@
 #[cfg(feature = "ssr")]
 #[tokio::main]
-async fn main(){
+async fn main() {
+    use axum::{
+        extract::{Extension, Path},
+        routing::{get, post},
+        Router,
+    };
     use leptos::*;
     use leptos_axum::{generate_route_list, LeptosRoutes};
-    use axum::{extract::{Extension, Path}, Router, routing::{get, post}};
+    use ssr_modes_axum::{app::*, fallback::file_and_error_handler};
     use std::sync::Arc;
-    use ssr_modes_axum::fallback::file_and_error_handler;
-    use ssr_modes_axum::app::*;
 
     let conf = get_configuration(None).await.unwrap();
     let addr = conf.leptos_options.site_addr;
     let leptos_options = conf.leptos_options;
     // Generate the list of routes in your Leptos App
-    let routes = generate_route_list(|cx| view! { cx, <App/> },None).await;
+    let routes = generate_route_list(|cx| view! { cx, <App/> }, None).await;
 
     GetPost::register();
     ListPostMetadata::register();
 
     let app = Router::new()
         .route("/api/*fn_name", post(leptos_axum::handle_server_fns))
-        .leptos_routes(leptos_options.clone(), routes, |cx| view! { cx, <App/> })
+        .leptos_routes(
+            leptos_options.clone(),
+            routes,
+            |cx| view! { cx, <App/> },
+        )
         .fallback(file_and_error_handler)
         .layer(Extension(Arc::new(leptos_options)));
 
