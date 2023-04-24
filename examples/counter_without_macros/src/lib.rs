e397c76878c494f6d8c3fd76c9398369a7d68c44
Joseph Cruz <agilarity@gmail.com>
2023-04-24 08:18:28 -0400

    test: use the count logic

--- examples/counter_without_macros/src/lib.rs
+++ examples/counter_without_macros/src/lib.rs
@@ -3,8 +3,7 @@ use leptos::{ev, html::*, *};
 /// A simple counter view.
 // A component is really just a function call: it runs once to create the DOM and reactive system
 pub fn counter(cx: Scope, initial_value: i32, step: u32) -> impl IntoView {
-    let step = step as i32;
-    let (value, set_value) = create_signal(cx, initial_value);
+    let (count, set_count) = create_signal(cx, Count::new(initial_value, step));
 
     // elements are created by calling a function with a Scope argument
     // the function name is the same as the HTML tag name
@@ -17,13 +16,13 @@ pub fn counter(cx: Scope, initial_value: i32, step: u32) -> impl IntoView {
                 // typed events found in leptos::ev
                 // 1) prevent typos in event names
                 // 2) allow for correct type inference in callbacks
-                .on(ev::click, move |_| set_value.update(|value| *value = 0))
+                .on(ev::click, move |_| set_count.update(|count| count.clear()))
                 .child("Clear"),
         )
         .child(
             button(cx)
                 .on(ev::click, move |_| {
-                    set_value.update(|value| *value -= step)
+                    set_count.update(|count| count.decrease())
                 })
                 .child("-1"),
         )
@@ -32,13 +31,13 @@ pub fn counter(cx: Scope, initial_value: i32, step: u32) -> impl IntoView {
                 .child("Value: ")
                 // reactive values are passed to .child() as a tuple
                 // (Scope, [child function]) so an effect can be created
-                .child((cx, move || value.get()))
+                .child(move || count.get().value())
                 .child("!"),
         )
         .child(
             button(cx)
                 .on(ev::click, move |_| {
-                    set_value.update(|value| *value += step)
+                    set_count.update(|count| count.increase())
                 })
                 .child("+1"),
         )
