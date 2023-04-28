a4e242c9640d1828d46cb350356066cdf9512f5e
Joseph Cruz <agilarity@gmail.com>
2023-04-28 18:52:46 -0400

    test:  resolve check-style issues

--- examples/counter/src/main.rs
+++ examples/counter/src/main.rs
@@ -4,10 +4,12 @@ use leptos::*;
 pub fn main() {
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
-    mount_to_body(|cx| view! { cx,
-        <SimpleCounter
-            initial_value=0
-            step=1
-        />
+    mount_to_body(|cx| {
+        view! { cx,
+            <SimpleCounter
+                initial_value=0
+                step=1
+            />
+        }
     })
 }
