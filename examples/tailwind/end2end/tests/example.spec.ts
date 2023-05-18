209e51c932c1fff036cb26eec00189f9a7fa997d
Joseph Cruz <agilarity@gmail.com>
2023-05-18 14:30:33 -0400

    test: should see the welcome message

--- examples/tailwind/end2end/tests/example.spec.ts
+++ examples/tailwind/end2end/tests/example.spec.ts
@@ -1,9 +1,7 @@
 import { test, expect } from "@playwright/test";
 
-test("homepage has title and links to intro page", async ({ page }) => {
+test("should see the welcome message", async ({ page }) => {
   await page.goto("http://localhost:3000/");
 
-  await expect(page).toHaveTitle("Cargo Leptos");
-
-  await expect(page.locator("h1")).toHaveText("Hi from your Leptos WASM!");
+  await expect(page.locator("h2")).toHaveText("Welcome to Leptos with Tailwind");
 });
