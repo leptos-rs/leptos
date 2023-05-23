807b18ee590b92a4aeb1f6cdb35ab2ed4d2aa3df
benwis <ben@celcyon.com>
2023-05-22 18:47:04 -0700

    Whee

--- examples/errors_axum/src/fallback.rs
+++ examples/errors_axum/src/fallback.rs
@@ -14,7 +14,6 @@ cfg_if! { if #[cfg(feature = "ssr")] {
     use crate::landing::App;
 
     pub async fn file_and_error_handler(uri: Uri, State(options): State<LeptosOptions>, req: Request<Body>) -> AxumResponse {
-        let options = options.leptos_options;
         let root = options.site_root.clone();
         let res = get_static_file(uri.clone(), &root).await.unwrap();
 
