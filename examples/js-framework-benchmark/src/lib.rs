e5cad650916e7b0d1ee1b5f870e1ccc46bec01f8
Vladimir Motylenko <me@vldm.cc>
2023-05-31 13:49:29 +0300

    chore: Remove EXPANSION.rs from js-framework-benchmark

--- examples/js-framework-benchmark/src/lib.rs
+++ examples/js-framework-benchmark/src/lib.rs
@@ -154,9 +154,7 @@ pub fn App(cx: Scope) -> impl IntoView {
                     <div class="col-md-6"><h1>"Leptos"</h1></div>
                     <div class="col-md-6">
                         <div class="row">
-                            // TODO: Remove this close tag for button.
-                            // Used to show diff in component expansion behaviour
-                            <Button id="run" text="Create 1,000 rows" on:click=run> </Button>
+                            <Button id="run" text="Create 1,000 rows" on:click=run />
                             <Button id="runlots" text="Create 10,000 rows" on:click=run_lots />
                             <Button id="add" text="Append 1,000 rows" on:click=add />
                             <Button id="update" text="Update every 10th row" on:click=update />
