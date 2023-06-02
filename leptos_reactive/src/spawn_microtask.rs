de1e317c9f91e1e436d80895d732ab36c3e9d703
tempbottle <1203820+tempbottle@users.noreply.github.com>
2023-06-02 10:02:32 +0800

    remove unsafe code comment, add #![forbid(unsafe_code)] guard.  And use `unchecked_into` instead of `dyn_into`

--- leptos_reactive/src/spawn_microtask.rs
+++ leptos_reactive/src/spawn_microtask.rs
@@ -1,8 +1,4 @@
-// `queue_microtask` needs to be in its own module, which is the only thing
-// in this entire framework that requires "unsafe" code (because Rust seems to
-// that a `wasm_bindgen` imported function like this is unsafe)
-// this is stupid, and one day hopefully web_sys will add queue_microtask itself
-
+#![forbid(unsafe_code)]
 use cfg_if::cfg_if;
 
 cfg_if! {
@@ -19,7 +15,7 @@ cfg_if! {
             use wasm_bindgen::prelude::*;
             let window = web_sys::window().expect("window not available");
             let queue_microtask = Reflect::get(&window, &JsValue::from_str("queueMicrotask")).expect("queueMicrotask not available");
-            let queue_microtask = queue_microtask.dyn_into::<Function>().expect("queueMicrotask not a function");
+            let queue_microtask = queue_microtask.unchecked_into::<Function>();
             let _ = queue_microtask.call0(&task);
         }
     } else {
