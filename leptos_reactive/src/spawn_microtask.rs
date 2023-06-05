6b982fb042ca177c3878aedc6fe187c7d1536842
Daniel Santana <danielgsantana@gmail.com>
2023-06-05 17:22:02 +0100

    Added MDN documentation.

--- leptos_reactive/src/spawn_microtask.rs
+++ leptos_reactive/src/spawn_microtask.rs
@@ -1,5 +1,13 @@
 #![forbid(unsafe_code)]
 
+/// The microtask is a short function which will run after the current task has
+/// completed its work and when there is no other code waiting to be run before
+/// control of the execution context is returned to the browser's event loop.
+///
+/// Microtasks are especially useful for libraries and frameworks that need
+/// to perform final cleanup or other just-before-rendering tasks.
+///
+/// [MDN queueMicrotask](https://developer.mozilla.org/en-US/docs/Web/API/queueMicrotask)
 pub fn queue_microtask(task: impl FnOnce() + 'static) {
     #[cfg(not(any(feature = "hydrate", feature = "csr")))]
     {
