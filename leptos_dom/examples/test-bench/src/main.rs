da77cc567ffd434520187a9a7c7b8aea247d1d17
Jose Quesada <jquesada2016@fau.edu>
2023-06-07 09:23:41 -0500

    removed `&` from test cases

--- leptos_dom/examples/test-bench/src/main.rs
+++ leptos_dom/examples/test-bench/src/main.rs
@@ -31,52 +31,53 @@ fn view_fn(cx: Scope) -> impl IntoView {
         <ul>
           /* These work! */
           <li><strong>"should be []"</strong></li>
-          <Test from=&[1] to=&[]/>
-          <Test from=&[1, 2] to=&[]/>
-          <Test from=&[1, 2, 3] to=&[]/>
+          <Test from=[1] to=[]/>
+          <Test from=[1, 2] to=[]/>
+          <Test from=[1, 2, 3] to=[]/>
           <hr/>
           <li><strong>"should be [1]"</strong></li>
-          <Test from=&[] to=&[1]/>
-          <Test from=&[1, 2] to=&[1]/>
-          <Test from=&[2, 1] to=&[1]/>
+          <Test from=[] to=[1]/>
+          <Test from=[1, 2] to=[1]/>
+          <Test from=[2, 1] to=[1]/>
           <hr/>
           <li><strong>"should be [1, 2]"</strong></li>
-          <Test from=&[1, 2, 3] to=&[1, 2]/>
-          <Test from=&[2] to=&[1, 2]/>
-          <Test from=&[1] to=&[1, 2]/>
+          <Test from=[1, 2, 3] to=[1, 2]/>
+          <Test from=[2] to=[1, 2]/>
+          <Test from=[1] to=[1, 2]/>
           <li><strong>"should be [1, 2, 3]"</strong></li>
-          <Test from=&[] to=&[1, 2, 3]/>
-          <Test from=&[2] to=&[1, 2, 3]/>
-          <Test from=&[1] to=&[1, 2, 3]/>
-          <Test from=&[1, 3, 2] to=&[1, 2, 3]/>
-          <Test from=&[2, 1, 3] to=&[1, 2, 3]/>
+          <Test from=[] to=[1, 2, 3]/>
+          <Test from=[2] to=[1, 2, 3]/>
+          <Test from=[1] to=[1, 2, 3]/>
+          <Test from=[1, 3, 2] to=[1, 2, 3]/>
+          <Test from=[2, 1, 3] to=[1, 2, 3]/>
         </ul>
         <h2>"Broken Tests"</h2>
         <ul>
           <li><strong>"should be [1, 2, 3]"</strong></li>
-          <Test from=&[3] to=&[1, 2, 3]/>
-          <Test from=&[3, 1] to=&[1, 2, 3]/>
-          <Test from=&[3, 2, 1] to=&[1, 2, 3]/> 
+          <Test from=[3] to=[1, 2, 3]/>
+          <Test from=[3, 1] to=[1, 2, 3]/>
+          <Test from=[3, 2, 1] to=[1, 2, 3]/>
          <hr/>
           <li><strong>"should be [1, 2, 3, 4]"</strong></li>
-          <Test from=&[1, 4, 2, 3] to=&[1, 2, 3, 4]/> 
+          <Test from=[1, 4, 2, 3] to=[1, 2, 3, 4]/>
           <hr/>
           <li><strong>"should be [1, 2, 3, 4, 5]"</strong></li>
-          <Test from=&[1, 4, 3, 2, 5] to=&[1, 2, 3, 4, 5]/> 
-          <Test from=&[4, 5, 3, 1, 2] to=&[1, 2, 3, 4, 5]/> 
+          <Test from=[1, 4, 3, 2, 5] to=[1, 2, 3, 4, 5]/>
+          <Test from=[4, 5, 3, 1, 2] to=[1, 2, 3, 4, 5]/>
         </ul>
     }
 }
 
 #[component]
-fn Test(
-    cx: Scope,
-    from: &'static [usize],
-    to: &'static [usize],
-) -> impl IntoView {
-    let (list, set_list) = create_signal(cx, from.to_vec());
+fn Test<From, To>(cx: Scope, from: From, to: To) -> impl IntoView
+where
+    From: IntoIterator<Item = usize>,
+    To: IntoIterator<Item = usize> + 'static,
+{
+    let (list, set_list) =
+        create_signal(cx, from.into_iter().collect::<Vec<_>>());
     request_animation_frame(move || {
-        set_list(to.to_vec());
+        set_list(to.into_iter().collect::<Vec<_>>());
     });
 
     view! { cx,
