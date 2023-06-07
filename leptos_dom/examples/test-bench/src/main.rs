69d12ba8a306ae2f330fd431f352c32527f3bc49
Greg Johnston <greg.johnston@gmail.com>
2023-06-06 21:31:11 -0400

    each of the commented examples panics

--- leptos_dom/examples/test-bench/src/main.rs
+++ leptos_dom/examples/test-bench/src/main.rs
@@ -44,29 +44,30 @@ fn view_fn(cx: Scope) -> impl IntoView {
         <hr/>
         <li><strong>"should be [1, 2]"</strong></li>
         <Test from=&[1, 2, 3] to=&[1, 2]/>
+        <li><strong>"should be [1, 2, 3]"</strong></li>
+        <Test from=&[] to=&[1, 2, 3]/>
       </ul>
       <h2>"Broken Tests"</h2>
       <ul>
         <li><strong>"should be [1, 2]"</strong></li>
-        <Test from=&[2] to=&[1, 2]/>
-        <Test from=&[1] to=&[1, 2]/>
-        <li><strong>"should be [1, 2, 3]"</strong></li>
-        <Test from=&[] to=&[1, 2, 3]/>
-        <Test from=&[2] to=&[1, 2, 3]/>
-        <Test from=&[1] to=&[1, 2, 3]/>
-        <Test from=&[3] to=&[1, 2, 3]/>
-        <Test from=&[3, 1] to=&[1, 2, 3]/>
-        <Test from=&[1, 3, 2] to=&[1, 2, 3]/>
-        <Test from=&[2, 1, 3] to=&[1, 2, 3]/>
-        <Test from=&[2, 1, 3] to=&[1, 2, 3]/>
-        <Test from=&[3, 2, 1] to=&[1, 2, 3]/>  
+        //<Test from=&[2] to=&[1, 2]/>
+        //<Test from=&[1] to=&[1, 2]/>
         <hr/>
+        <li><strong>"should be [1, 2, 3]"</strong></li>
+        //<Test from=&[2] to=&[1, 2, 3]/>
+        //<Test from=&[1] to=&[1, 2, 3]/>
+        //<Test from=&[3] to=&[1, 2, 3]/>
+        //<Test from=&[3, 1] to=&[1, 2, 3]/>
+        //<Test from=&[1, 3, 2] to=&[1, 2, 3]/>
+        //<Test from=&[2, 1, 3] to=&[1, 2, 3]/>
+        //<Test from=&[3, 2, 1] to=&[1, 2, 3]/>  
+       <hr/>
         <li><strong>"should be [1, 2, 3, 4]"</strong></li>
-        <Test from=&[1, 4, 2, 3] to=&[1, 2, 3, 4]/>  
+        //<Test from=&[1, 4, 2, 3] to=&[1, 2, 3, 4]/>  
         <hr/>
         <li><strong>"should be [1, 2, 3, 4, 5]"</strong></li>
-        <Test from=&[1, 4, 3, 2, 5] to=&[1, 2, 3, 4, 5]/> 
-        <Test from=&[4, 5, 3, 1, 2] to=&[1, 2, 3, 4, 5]/>           
+        //<Test from=&[1, 4, 3, 2, 5] to=&[1, 2, 3, 4, 5]/> 
+        //<Test from=&[4, 5, 3, 1, 2] to=&[1, 2, 3, 4, 5]/>          
       </ul>
   }
 }
