c55067ab7ca04540344a48d788b2ba9e7805e732
Greg Johnston <greg.johnston@gmail.com>
2023-06-23 11:10:59 -0400

    feat: improved error handling and version tracking for pending actions/`<ActionForm/>` (closes #1205) (#1225)

--- leptos_server/src/action.rs
+++ leptos_server/src/action.rs
@@ -3,7 +3,7 @@ use leptos_reactive::{
     create_rw_signal, signal_prelude::*, spawn_local, store_value, ReadSignal,
     RwSignal, Scope, StoredValue,
 };
-use std::{future::Future, pin::Pin, rc::Rc};
+use std::{cell::Cell, future::Future, pin::Pin, rc::Rc};
 
 /// An action synchronizes an imperative `async` call to the synchronous reactive system.
 ///
@@ -106,13 +106,30 @@ where
         self.0.with_value(|a| a.pending.read_only())
     }
 
-    /// Updates whether the action is currently pending.
+    /// Updates whether the action is currently pending. If the action has been dispatched
+    /// multiple times, and some of them are still pending, it will *not* update the `pending`
+    /// signal.
     #[cfg_attr(
         any(debug_assertions, feature = "ssr"),
         tracing::instrument(level = "trace", skip_all,)
     )]
     pub fn set_pending(&self, pending: bool) {
-        self.0.try_with_value(|a| a.pending.set(pending));
+        self.0.try_with_value(|a| {
+            let pending_dispatches = &a.pending_dispatches;
+            let still_pending = {
+                pending_dispatches.set(if pending {
+                    pending_dispatches.get().wrapping_add(1)
+                } else {
+                    pending_dispatches.get().saturating_sub(1)
+                });
+                pending_dispatches.get()
+            };
+            if still_pending == 0 {
+                a.pending.set(false);
+            } else {
+                a.pending.set(true);
+            }
+        });
     }
 
     /// The URL associated with the action (typically as part of a server function.)
@@ -186,6 +203,7 @@ where
     I: 'static,
     O: 'static,
 {
+    cx: Scope,
     /// How many times the action has successfully resolved.
     pub version: RwSignal<usize>,
     /// The current argument that was dispatched to the `async` function.
@@ -195,6 +213,8 @@ where
     pub value: RwSignal<Option<O>>,
     pending: RwSignal<bool>,
     url: Option<String>,
+    /// How many dispatched actions are still pending.
+    pending_dispatches: Rc<Cell<usize>>,
     #[allow(clippy::complexity)]
     action_fn: Rc<dyn Fn(&I) -> Pin<Box<dyn Future<Output = O>>>>,
 }
@@ -215,14 +235,23 @@ where
         let input = self.input;
         let version = self.version;
         let pending = self.pending;
+        let pending_dispatches = Rc::clone(&self.pending_dispatches);
         let value = self.value;
+        let cx = self.cx;
         pending.set(true);
+        pending_dispatches.set(pending_dispatches.get().saturating_sub(1));
         spawn_local(async move {
             let new_value = fut.await;
-            value.set(Some(new_value));
-            input.set(None);
-            pending.set(false);
-            version.update(|n| *n += 1);
+            cx.batch(move || {
+                value.set(Some(new_value));
+                input.set(None);
+                version.update(|n| *n += 1);
+                pending_dispatches
+                    .set(pending_dispatches.get().saturating_sub(1));
+                if pending_dispatches.get() == 0 {
+                    pending.set(false);
+                }
+            });
         })
     }
 }
@@ -316,6 +345,7 @@ where
     let input = create_rw_signal(cx, None);
     let value = create_rw_signal(cx, None);
     let pending = create_rw_signal(cx, false);
+    let pending_dispatches = Rc::new(Cell::new(0));
     let action_fn = Rc::new(move |input: &I| {
         let fut = action_fn(input);
         Box::pin(fut) as Pin<Box<dyn Future<Output = O>>>
@@ -324,11 +354,13 @@ where
     Action(store_value(
         cx,
         ActionState {
+            cx,
             version,
             url: None,
             input,
             value,
             pending,
+            pending_dispatches,
             action_fn,
         },
     ))
