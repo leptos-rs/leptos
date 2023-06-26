ac84ff79bddee79004e9e4d3c42fbe9eb75989f6
Joseph Cruz <agilarity@gmail.com>
2023-06-26 10:03:02 -0400

    test(counters_stable): increase retries

--- examples/counters_stable/e2e/playwright.config.ts
+++ examples/counters_stable/e2e/playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 5 : 5,
+  retries: process.env.CI ? 10 : 10,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
