91b61fba08c666df3086458e603be62fc216e3a3
Joseph Cruz <agilarity@gmail.com>
2023-06-27 10:08:44 -0400

    test(counters_stable): configure html reporter

--- examples/counters_stable/e2e/playwright.config.ts
+++ examples/counters_stable/e2e/playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.DEV ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "list",
+  reporter: [["html", { open: "never" }], ["list"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
