b5ef59f8f703a8563abddc9bce81d1d0ffd69648
Joseph Cruz <agilarity@gmail.com>
2023-06-27 10:06:41 -0400

    test(counters_stable): replace CI variable

--- examples/counters_stable/e2e/playwright.config.ts
+++ examples/counters_stable/e2e/playwright.config.ts
@@ -14,11 +14,11 @@ export default defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  forbidOnly: !process.env.DEV,
   /* Retry on CI only */
-  retries: process.env.CI ? 10 : 10,
+  retries: process.env.DEV ? 0 : 10,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
+  workers: process.env.DEV ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "list",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
