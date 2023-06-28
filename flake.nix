5bec43eab41bf803ae097168507e04c7652b86a2
Jay Querie <jay@querie.cc>
2023-06-27 17:24:23 -0700

    nix-flake: use follows for `rust-overlay` (..)
    This removes the extra nixpkgs dependency by re-using the nixpkgs
    already defined.
    
    https://nixos.wiki/wiki/Flakes
    https://web.archive.org/web/20230621091703/https://nixos.wiki/wiki/Flakes
    
    > # The `follows` keyword in inputs is used for inheritance.
    > # Here, `inputs.nixpkgs` of sops-nix is kept consistent with the `inputs.nixpkgs` of
    > # the current flake, to avoid problems caused by different versions of nixpkgs.

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url  = "github:numtide/flake-utils";
   };
 
