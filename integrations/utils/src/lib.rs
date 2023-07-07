9a0707d8f5cb8653fb4d291fed6a29cbe7556495
snapiz <jonathan.lapiquonne@gmail.com>
2023-07-08 00:01:01 +0200

    fix: https live reload

--- integrations/utils/src/lib.rs
+++ integrations/utils/src/lib.rs
@@ -14,7 +14,8 @@ fn autoreload(options: &LeptosOptions) -> String {
             r#"
                 <script crossorigin="">(function () {{
                     {}
-                    var ws = new WebSocket('ws://{site_ip}:{reload_port}/live_reload');
+                    //var ws = new WebSocket('ws://{site_ip}:{reload_port}/live_reload');
+                    var ws = new WebSocket('ws://timada.localhost/starter-reload/live_reload');
                     ws.onmessage = (ev) => {{
                         let msg = JSON.parse(ev.data);
                         if (msg.all) window.location.reload();
