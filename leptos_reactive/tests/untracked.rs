4130ecb782188a24ab169458b68df581ab3658f0
Greg Johnston <greg.johnston@gmail.com>
2023-07-20 08:39:20 -0400

    cleaning up tests

--- leptos_reactive/tests/untracked.rs
+++ leptos_reactive/tests/untracked.rs
@@ -1,79 +1,78 @@
 use leptos_reactive::{
-    create_isomorphic_effect, create_runtime, create_scope, create_signal,
-    signal_prelude::*, SignalGetUntracked, SignalSetUntracked,
+    create_isomorphic_effect, create_runtime, create_signal, signal_prelude::*,
+    SignalGetUntracked, SignalSetUntracked,
 };
 
 #[test]
 fn untracked_set_doesnt_trigger_effect() {
     use std::{cell::RefCell, rc::Rc};
 
-    create_scope(create_runtime(), |cx| {
-        let (a, set_a) = create_signal(cx, -1);
+    let runtime = create_runtime();
 
-        // simulate an arbitrary side effect
-        let b = Rc::new(RefCell::new(String::new()));
+    let (a, set_a) = create_signal(-1);
 
-        create_isomorphic_effect(cx, {
-            let b = b.clone();
-            move |_| {
-                let formatted = format!("Value is {}", a.get());
-                *b.borrow_mut() = formatted;
-            }
-        });
+    // simulate an arbitrary side effect
+    let b = Rc::new(RefCell::new(String::new()));
 
-        assert_eq!(b.borrow().as_str(), "Value is -1");
+    create_isomorphic_effect({
+        let b = b.clone();
+        move |_| {
+            let formatted = format!("Value is {}", a.get());
+            *b.borrow_mut() = formatted;
+        }
+    });
 
-        set_a.set(1);
+    assert_eq!(b.borrow().as_str(), "Value is -1");
 
-        assert_eq!(b.borrow().as_str(), "Value is 1");
+    set_a.set(1);
 
-        set_a.set_untracked(-1);
+    assert_eq!(b.borrow().as_str(), "Value is 1");
 
-        assert_eq!(b.borrow().as_str(), "Value is 1");
-    })
-    .dispose()
+    set_a.set_untracked(-1);
+
+    assert_eq!(b.borrow().as_str(), "Value is 1");
+
+    runtime.dispose();
 }
 
 #[test]
 fn untracked_get_doesnt_trigger_effect() {
     use std::{cell::RefCell, rc::Rc};
 
-    create_scope(create_runtime(), |cx| {
-        let (a, set_a) = create_signal(cx, -1);
-        let (a2, set_a2) = create_signal(cx, 1);
+    let runtime = create_runtime();
 
-        // simulate an arbitrary side effect
-        let b = Rc::new(RefCell::new(String::new()));
+    let (a, set_a) = create_signal(-1);
+    let (a2, set_a2) = create_signal(1);
 
-        create_isomorphic_effect(cx, {
-            let b = b.clone();
-            move |_| {
-                let formatted = format!(
-                    "Values are {} and {}",
-                    a.get(),
-                    a2.get_untracked()
-                );
-                *b.borrow_mut() = formatted;
-            }
-        });
+    // simulate an arbitrary side effect
+    let b = Rc::new(RefCell::new(String::new()));
 
-        assert_eq!(b.borrow().as_str(), "Values are -1 and 1");
+    create_isomorphic_effect({
+        let b = b.clone();
+        move |_| {
+            let formatted =
+                format!("Values are {} and {}", a.get(), a2.get_untracked());
+            *b.borrow_mut() = formatted;
+        }
+    });
 
-        set_a.set(1);
+    assert_eq!(b.borrow().as_str(), "Values are -1 and 1");
 
-        assert_eq!(b.borrow().as_str(), "Values are 1 and 1");
+    set_a.set(1);
 
-        set_a.set_untracked(-1);
+    assert_eq!(b.borrow().as_str(), "Values are 1 and 1");
 
-        assert_eq!(b.borrow().as_str(), "Values are 1 and 1");
+    set_a.set_untracked(-1);
 
-        set_a2.set(-1);
+    assert_eq!(b.borrow().as_str(), "Values are 1 and 1");
 
-        assert_eq!(b.borrow().as_str(), "Values are 1 and 1");
+    set_a2.set(-1);
 
-        set_a.set(-1);
+    assert_eq!(b.borrow().as_str(), "Values are 1 and 1");
 
-        assert_eq!(b.borrow().as_str(), "Values are -1 and -1");
-    })
-    .dispose()
+    set_a.set(-1);
+
+    assert_eq!(b.borrow().as_str(), "Values are -1 and -1");
+
+    runtime.dispose();
 }
