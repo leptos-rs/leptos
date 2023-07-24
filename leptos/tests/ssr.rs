fd69248cd57e29e475936cad907d2c4f764ec6fa
Greg Johnston <greg.johnston@gmail.com>
2023-07-24 14:33:35 -0400

    ssr tests

--- leptos/tests/ssr.rs
+++ leptos/tests/ssr.rs
@@ -3,25 +3,25 @@
 fn simple_ssr_test() {
     use leptos::*;
 
-    _ = create_scope(create_runtime(), |cx| {
-        let (value, set_value) = create_signal(0);
-        let rendered = view! {
+    let runtime = create_runtime();
+    let (value, set_value) = create_signal(0);
+    let rendered = view! {
 
-            <div>
-                <button on:click=move |_| set_value.update(|value| *value -= 1)>"-1"</button>
-                <span>"Value: " {move || value.get().to_string()} "!"</span>
-                <button on:click=move |_| set_value.update(|value| *value += 1)>"+1"</button>
-            </div>
-        };
+        <div>
+            <button on:click=move |_| set_value.update(|value| *value -= 1)>"-1"</button>
+            <span>"Value: " {move || value.get().to_string()} "!"</span>
+            <button on:click=move |_| set_value.update(|value| *value += 1)>"+1"</button>
+        </div>
+    };
 
-        assert!(rendered.into_view(cx).render_to_string(cx).contains(
-            "<div id=\"_0-1\"><button id=\"_0-2\">-1</button><span \
-             id=\"_0-3\">Value: \
-             <!--hk=_0-4o|leptos-dyn-child-start-->0<!\
-             --hk=_0-4c|leptos-dyn-child-end-->!</span><button \
-             id=\"_0-5\">+1</button></div>"
-        ));
-    });
+    assert!(rendered.into_view().render_to_string().contains(
+        "<div id=\"_0-1\"><button id=\"_0-2\">-1</button><span \
+         id=\"_0-3\">Value: \
+         <!--hk=_0-4o|leptos-dyn-child-start-->0<!\
+         --hk=_0-4c|leptos-dyn-child-end-->!</span><button \
+         id=\"_0-5\">+1</button></div>"
+    ));
+    runtime.dispose();
 }
 
 #[cfg(not(any(feature = "csr", feature = "hydrate")))]
@@ -42,23 +42,23 @@ fn ssr_test_with_components() {
         }
     }
 
-    _ = create_scope(create_runtime(), |cx| {
-        let rendered = view! {
+    let runtime = create_runtime();
+    let rendered = view! {
 
-            <div class="counters">
-                <Counter initial_value=1/>
-                <Counter initial_value=2/>
-            </div>
-        };
+        <div class="counters">
+            <Counter initial_value=1/>
+            <Counter initial_value=2/>
+        </div>
+    };
 
-        assert!(rendered.into_view(cx).render_to_string(cx).contains(
-            "<div id=\"_0-3\"><button id=\"_0-4\">-1</button><span \
-             id=\"_0-5\">Value: \
-             <!--hk=_0-6o|leptos-dyn-child-start-->1<!\
-             --hk=_0-6c|leptos-dyn-child-end-->!</span><button \
-             id=\"_0-7\">+1</button></div>"
-        ));
-    });
+    assert!(rendered.into_view().render_to_string().contains(
+        "<div id=\"_0-3\"><button id=\"_0-4\">-1</button><span \
+         id=\"_0-5\">Value: \
+         <!--hk=_0-6o|leptos-dyn-child-start-->1<!\
+         --hk=_0-6c|leptos-dyn-child-end-->!</span><button \
+         id=\"_0-7\">+1</button></div>"
+    ));
+    runtime.dispose();
 }
 
 #[cfg(not(any(feature = "csr", feature = "hydrate")))]
@@ -79,23 +79,24 @@ fn ssr_test_with_snake_case_components() {
         }
     }
 
-    _ = create_scope(create_runtime(), |cx| {
-        let rendered = view! {
+    let runtime = create_runtime();
+    let rendered = view! {
 
-            <div class="counters">
-                <SnakeCaseCounter initial_value=1/>
-                <SnakeCaseCounter initial_value=2/>
-            </div>
-        };
+        <div class="counters">
+            <SnakeCaseCounter initial_value=1/>
+            <SnakeCaseCounter initial_value=2/>
+        </div>
+    };
 
-        assert!(rendered.into_view(cx).render_to_string(cx).contains(
-            "<div id=\"_0-3\"><button id=\"_0-4\">-1</button><span \
-             id=\"_0-5\">Value: \
-             <!--hk=_0-6o|leptos-dyn-child-start-->1<!\
-             --hk=_0-6c|leptos-dyn-child-end-->!</span><button \
-             id=\"_0-7\">+1</button></div>"
-        ));
-    });
+    assert!(rendered.into_view().render_to_string().contains(
+        "<div id=\"_0-3\"><button id=\"_0-4\">-1</button><span \
+         id=\"_0-5\">Value: \
+         <!--hk=_0-6o|leptos-dyn-child-start-->1<!\
+         --hk=_0-6c|leptos-dyn-child-end-->!</span><button \
+         id=\"_0-7\">+1</button></div>"
+    ));
+
+    runtime.dispose();
 }
 
 #[cfg(not(any(feature = "csr", feature = "hydrate")))]
@@ -103,18 +104,18 @@ fn ssr_test_with_snake_case_components() {
 fn test_classes() {
     use leptos::*;
 
-    _ = create_scope(create_runtime(), |cx| {
-        let (value, _set_value) = create_signal(5);
-        let rendered = view! {
+    let runtime = create_runtime();
+    let (value, _set_value) = create_signal(5);
+    let rendered = view! {
 
-            <div class="my big" class:a={move || value.get() > 10} class:red=true class:car={move || value.get() > 1}></div>
-        };
+        <div class="my big" class:a={move || value.get() > 10} class:red=true class:car={move || value.get() > 1}></div>
+    };
 
-        assert!(rendered
-            .into_view(cx)
-            .render_to_string(cx)
-            .contains("<div id=\"_0-1\" class=\"my big  red car\"></div>"));
-    });
+    assert!(rendered
+        .into_view()
+        .render_to_string()
+        .contains("<div id=\"_0-1\" class=\"my big  red car\"></div>"));
+    runtime.dispose();
 }
 
 #[cfg(not(any(feature = "csr", feature = "hydrate")))]
@@ -122,21 +123,21 @@ fn test_classes() {
 fn ssr_with_styles() {
     use leptos::*;
 
-    _ = create_scope(create_runtime(), |cx| {
-        let (_, set_value) = create_signal(0);
-        let styles = "myclass";
-        let rendered = view! {
-             class = styles,
-            <div>
-                <button class="btn" on:click=move |_| set_value.update(|value| *value -= 1)>"-1"</button>
-            </div>
-        };
+    let runtime = create_runtime();
+    let (_, set_value) = create_signal(0);
+    let styles = "myclass";
+    let rendered = view! {
+         class = styles,
+        <div>
+            <button class="btn" on:click=move |_| set_value.update(|value| *value -= 1)>"-1"</button>
+        </div>
+    };
 
-        assert!(rendered.into_view(cx).render_to_string(cx).contains(
-            "<div id=\"_0-1\" class=\" myclass\"><button id=\"_0-2\" \
-             class=\"btn myclass\">-1</button></div>"
-        ));
-    });
+    assert!(rendered.into_view().render_to_string().contains(
+        "<div id=\"_0-1\" class=\" myclass\"><button id=\"_0-2\" class=\"btn \
+         myclass\">-1</button></div>"
+    ));
+    runtime.dispose();
 }
 
 #[cfg(not(any(feature = "csr", feature = "hydrate")))]
@@ -144,16 +145,17 @@ fn ssr_with_styles() {
 fn ssr_option() {
     use leptos::*;
 
-    _ = create_scope(create_runtime(), |cx| {
-        let (_, _) = create_signal(0);
-        let rendered = view! {
+    let runtime = create_runtime();
+    let (_, _) = create_signal(0);
+    let rendered = view! {
 
-            <option/>
-        };
+        <option/>
+    };
 
-        assert!(rendered
-            .into_view(cx)
-            .render_to_string(cx)
-            .contains("<option id=\"_0-1\"></option>"));
-    });
+    assert!(rendered
+        .into_view()
+        .render_to_string()
+        .contains("<option id=\"_0-1\"></option>"));
+
+    runtime.dispose();
 }
