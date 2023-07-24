b3bbde596d3423baec9a3da20c437aef520bb47c
Bechma <brunochenoll@hotmail.com>
2023-07-24 21:22:45 +0200

    add disclaimer to spawn_local for ssr feature

--- leptos_reactive/src/spawn.rs
+++ leptos_reactive/src/spawn.rs
@@ -5,6 +5,65 @@ use std::future::Future;
 /// Spawns and runs a thread-local [`Future`] in a platform-independent way.
 ///
 /// This can be used to interface with any `async` code.
+///
+/// When calling this function in SSR mode(like in a component) remember
+/// to not use it to synchronise signals, as it can lead to
+/// race conditions or memory leaks.
+///
+/// ❌❌❌
+/// ```
+/// use leptos::*;
+///
+/// #[server(GetUser)]
+/// pub async fn get_user(user: String) -> Result<String, ServerFnError> {
+///     Ok(format!("this user is {user}"))
+/// }
+///
+/// #[component]
+/// pub fn User(cx: Scope) -> impl IntoView {
+///     let user = create_rw_signal(cx, String::new());
+///
+///     // DON'T
+///     spawn_local(async move {
+///         let user_res = get_user("user".into()).await.unwrap_or_default();
+///         user.set(user_res);
+///     });
+///     view!{cx, <p>"This will be empty(hopefully the client will render it) -> "{move || user.get()}</p>}
+/// }
+/// ```
+///
+/// ✅✅✅
+/// ```
+/// use leptos::*;
+///
+/// #[server(GetUser)]
+/// pub async fn get_user(user: String) -> Result<String, ServerFnError> {
+///     Ok(format!("this user is {user}"))
+/// }
+///
+/// #[component]
+/// pub fn User(cx: Scope) -> impl IntoView {
+///     // new resource with no dependencies(it will only called once)
+///     let user = create_resource(cx, || (), |_| async { get_user("john".into()).await });
+///     view!{cx,
+///         // handles the loading
+///         <Suspense fallback=move || view! {cx, <p>"Loading User"</p> }>
+///             // handles the error from the resource
+///             <ErrorBoundary fallback=|cx, _| {view! {cx, <p>"Something went wrong"</p>}}>
+///                 {move || {
+///                     user.read(cx).map(move |x| {
+///                         // the resource has a result
+///                         x.map(move |y| {
+///                             // successful call from the server fn
+///                             view!{cx, <p>"User result filled in server and client: "{y}</p>}
+///                         })
+///                     })
+///                 }}
+///             </ErrorBoundary>
+///         </Suspense>
+///     }
+/// }
+/// ```
 pub fn spawn_local<F>(fut: F)
 where
     F: Future<Output = ()> + 'static,
