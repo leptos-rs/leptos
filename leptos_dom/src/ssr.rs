b2bb978c303a37858f5a5ef49b5c6cfd9a7483e2
Greg Johnston <greg.johnston@gmail.com>
2023-07-28 10:09:44 -0400

    fix release build SSR

--- leptos_dom/src/ssr.rs
+++ leptos_dom/src/ssr.rs
@@ -530,7 +530,7 @@ impl View {
                                                     "{}<!--hk={}-->",
                                                     content(),
                                                     HydrationCtx::to_string(&id, true)
-                                                )
+                                                ).into()
                                              }
                                         }
                                     })
