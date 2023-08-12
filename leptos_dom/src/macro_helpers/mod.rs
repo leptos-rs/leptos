abfa2c1421c9c944ae9a82f939ceba709d3fe271
luoxiao <luoxiaozero@163.com>
2023-08-13 01:40:46 +0800

    feat: tracing property

--- leptos_dom/src/macro_helpers/mod.rs
+++ leptos_dom/src/macro_helpers/mod.rs
@@ -2,6 +2,9 @@ mod into_attribute;
 mod into_class;
 mod into_property;
 mod into_style;
+#[cfg(debug_assertions)]
+#[doc(hidden)]
+pub mod tracing_property;
 pub use into_attribute::*;
 pub use into_class::*;
 pub use into_property::*;
---
+++ leptos_dom/src/macro_helpers/tracing_property.rs
@@ -0,0 +1,70 @@
+use wasm_bindgen::UnwrapThrowExt;
+
+#[cfg_attr(debug_assertions, macro_export)]
+/// Use for tracing property
+macro_rules! tracing_props {
+    () => {
+        ::leptos::leptos_dom::tracing::span!(
+            ::leptos::leptos_dom::tracing::Level::INFO,
+            "leptos_dom::tracing_props",
+            props = String::from("[]")
+        );
+    };
+    ($($prop:tt),+ $(,)?) => {
+        {
+            use ::leptos::leptos_dom::tracing_property::{Match, SerializeMatch, DefaultMatch};
+            let mut props = String::from('[');
+            $(
+                let prop = (&&Match {
+                    name: stringify!{$prop},
+                    value: std::cell::Cell::new(Some(&$prop))
+                }).spez();
+                props.push_str(&format!("{prop},"));
+            )*
+            props.pop();
+            props.push(']');
+            ::leptos::leptos_dom::tracing::span!(
+                ::leptos::leptos_dom::tracing::Level::INFO,
+                "leptos_dom::tracing_props",
+                props
+            );
+        }
+    };
+}
+
+// Implementation based on spez
+// see https://github.com/m-ou-se/spez
+
+pub struct Match<T> {
+    pub name: &'static str,
+    pub value: std::cell::Cell<Option<T>>,
+}
+
+pub trait SerializeMatch {
+    type Return;
+    fn spez(&self) -> Self::Return;
+}
+impl<T: serde::Serialize> SerializeMatch for &Match<&T> {
+    type Return = String;
+    fn spez(&self) -> Self::Return {
+        let name = self.name;
+        serde_json::to_string(self.value.get().unwrap_throw()).map_or_else(
+            |err| format!(r#"{{"name": "{name}", "error": "{err}"}}"#),
+            |value| format!(r#"{{"name": "{name}", "value": {value}}}"#),
+        )
+    }
+}
+
+pub trait DefaultMatch {
+    type Return;
+    fn spez(&self) -> Self::Return;
+}
+impl<T> DefaultMatch for Match<&T> {
+    type Return = String;
+    fn spez(&self) -> Self::Return {
+        let name = self.name;
+        format!(
+            r#"{{"name": "{name}", "error": "The trait `serde::Serialize` is not implemented"}}"#
+        )
+    }
+}
