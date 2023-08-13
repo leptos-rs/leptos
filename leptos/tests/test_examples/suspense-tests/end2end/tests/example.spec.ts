415e92fb4f7b1951993f26164ff9a9b1c6a9d696
Joseph Cruz <agilarity@gmail.com>
2023-08-13 18:42:33 -0400

    chore(test_examples): remove obsolete directory

--- leptos/tests/test_examples/suspense-tests/end2end/tests/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("homepage has title and links to intro page", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
-
-  await expect(page).toHaveTitle("Welcome to Leptos");
-
-  await expect(page.locator("h1")).toHaveText("Welcome to Leptos!");
-});
