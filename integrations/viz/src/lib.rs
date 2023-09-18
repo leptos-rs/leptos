25eabe0543678267e2480a3fbd4809294a1b3041
Greg Johnston <greg.johnston@gmail.com>
2023-09-18 15:46:11 -0400

    make same change to viz integration

--- integrations/viz/src/lib.rs
+++ integrations/viz/src/lib.rs
@@ -988,7 +988,7 @@ where
 /// Generates a list of all routes defined in Leptos's Router in your app. We can then use this to automatically
 /// create routes in Viz's Router without having to use wildcard matching or fallbacks. Takes in your root app Element
 /// as an argument so it can walk you app tree. This version is tailored to generate Viz compatible paths.
-pub async fn generate_route_list<IV>(
+pub fn generate_route_list<IV>(
     app_fn: impl Fn() -> IV + 'static + Clone,
 ) -> Vec<RouteListing>
 where
@@ -1008,13 +1008,13 @@ pub async fn generate_route_list_with_ssg<IV>(
 where
     IV: IntoView + 'static,
 {
-    generate_route_list_with_exclusions_and_ssg(app_fn, None).await
+    generate_route_list_with_exclusions_and_ssg(app_fn, None)
 }
 
 /// Generates a list of all routes defined in Leptos's Router in your app. We can then use this to automatically
 /// create routes in Viz's Router without having to use wildcard matching or fallbacks. Takes in your root app Element
 /// as an argument so it can walk you app tree. This version is tailored to generate Viz compatible paths.
-pub async fn generate_route_list_with_exclusions<IV>(
+pub fn generate_route_list_with_exclusions<IV>(
     app_fn: impl Fn() -> IV + 'static + Clone,
     excluded_routes: Option<Vec<String>>,
 ) -> Vec<RouteListing>
