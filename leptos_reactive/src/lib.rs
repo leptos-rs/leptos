efd5b606f92594d8e042aba6f566dea0d758cb0b
Greg Johnston <greg.johnston@gmail.com>
2023-09-27 19:32:22 -0400

    unused features, as far as I can tell

--- leptos_reactive/src/lib.rs
+++ leptos_reactive/src/lib.rs
@@ -2,8 +2,6 @@
 #![deny(missing_docs)]
 #![cfg_attr(feature = "nightly", feature(fn_traits))]
 #![cfg_attr(feature = "nightly", feature(unboxed_closures))]
-#![cfg_attr(feature = "nightly", feature(negative_impls))]
-#![cfg_attr(feature = "nightly", feature(auto_traits))]
 #![cfg_attr(feature = "nightly", feature(type_name_of_val))]
 
 //! The reactive system for the [Leptos](https://docs.rs/leptos/latest/leptos/) Web framework.
