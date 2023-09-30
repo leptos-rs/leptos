9940252c9d6f40b7fcee921d5ef27fd7de7b630f
Maccesch <maccesch@synphonyte.com>
2023-09-30 19:33:31 +0100

    made show fallback optional

--- leptos/src/show.rs
+++ leptos/src/show.rs
@@ -1,15 +1,13 @@
 use leptos::{component, ChildrenFn};
-use leptos_dom::IntoView;
+use leptos_dom::{IntoView, View};
 use leptos_reactive::{create_memo, signal_prelude::*};
+use std::rc::Rc;
 
 /// A component that will show its children when the `when` condition is `true`,
 /// and show the fallback when it is `false`, without rerendering every time
 /// the condition changes.
 ///
-/// *Note*: Because of the nature of generic arguments, it’s not really possible
-/// to make the `fallback` optional. If you want an empty fallback state—in other
-/// words, if you want to show the children if `when` is true and noting otherwise—use
-/// `fallback=|_| ()` (i.e., a fallback function that returns the unit type `()`).
+/// The fallback prop is optional and defaults to rendering nothing.
 ///
 /// ```rust
 /// # use leptos_reactive::*;
@@ -34,23 +32,55 @@ use leptos_reactive::{create_memo, signal_prelude::*};
     tracing::instrument(level = "info", skip_all)
 )]
 #[component]
-pub fn Show<F, W, IV>(
+pub fn Show<W>(
     /// The children will be shown whenever the condition in the `when` closure returns `true`.
     children: ChildrenFn,
     /// A closure that returns a bool that determines whether this thing runs
     when: W,
-    /// A closure that returns what gets rendered if the when statement is false
-    fallback: F,
+    /// A closure that returns what gets rendered if the when statement is false. By default this is the empty view.
+    #[prop(optional, into)]
+    fallback: Fallback,
 ) -> impl IntoView
 where
     W: Fn() -> bool + 'static,
-    F: Fn() -> IV + 'static,
-    IV: IntoView,
 {
     let memoized_when = create_memo(move |_| when());
 
     move || match memoized_when.get() {
         true => children().into_view(),
-        false => fallback().into_view(),
+        false => fallback.run(),
+    }
+}
+
+/// New-type wrapper for the fallback view function to enable `#[prop(into, optional)]`.
+#[derive(Clone)]
+pub struct Fallback {
+    function: Rc<dyn Fn() -> View>,
+}
+
+impl Default for Fallback {
+    fn default() -> Self {
+        Self {
+            function: Rc::new(|| ().into_view()),
+        }
+    }
+}
+
+impl<F, IV> From<F> for Fallback
+where
+    F: Fn() -> IV + 'static,
+    IV: IntoView,
+{
+    fn from(value: F) -> Self {
+        Self {
+            function: Rc::new(move || value().into_view()),
+        }
+    }
+}
+
+impl Fallback {
+    /// Execute the wrapped function
+    pub fn run(&self) -> View {
+        (self.function)()
     }
 }
