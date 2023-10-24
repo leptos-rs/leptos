9f75869c1d714cd86e934fb8dfe6b0f969ab0d88
Greg Johnston <greg.johnston@gmail.com>
2023-10-24 14:34:58 -0400

    chore: remove `wee_alloc` to make Dependabot happy

--- examples/hackernews_islands_axum/src/lib.rs
+++ examples/hackernews_islands_axum/src/lib.rs
@@ -36,12 +36,6 @@ cfg_if! {
     if #[cfg(feature = "hydrate")] {
         use wasm_bindgen::prelude::wasm_bindgen;
 
-        extern crate wee_alloc;
-
-        // Use `wee_alloc` as the global allocator.
-        #[global_allocator]
-        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
         #[wasm_bindgen]
         pub fn hydrate() {
             #[cfg(debug_assertions)]
