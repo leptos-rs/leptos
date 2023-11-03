21020b0481ead60af6e691e87dcc968e60a75d3c
Saikat Das <saikatdas0790@gmail.com>
2023-11-03 14:27:05 +0530

    build: bump nix flake and add rust toolchain particulars

--- flake.nix
+++ flake.nix
@@ -2,32 +2,29 @@
   description = "A basic Rust devshell for NixOS users developing Leptos";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
-          inherit system overlays;
+          inherit system;
         };
       in
       with pkgs;
       {
         devShells.default = mkShell {
           buildInputs = [
-            openssl
-            pkg-config
             cacert
             cargo-make
+            nodejs_21
+            openssl
+            pkg-config
+            rustup
             trunk
-            (rust-bin.selectLatestNightlyWith( toolchain: toolchain.default.override {
-              extensions= [ "rust-src" "rust-analyzer" ];
-              targets = [ "wasm32-unknown-unknown" ];
-            }))
+            wasm-pack
           ];
 
           shellHook = ''
