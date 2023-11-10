1e4c1660a08f5ae507220f4e94f3fbf561506ad1
Greg Johnston <greg.johnston@gmail.com>
2023-11-10 14:33:24 -0500

    removing unrelated change to example

--- examples/tailwind_axum/src/app.rs
+++ examples/tailwind_axum/src/app.rs
@@ -8,7 +8,7 @@ pub fn App() -> impl IntoView {
 
     view! {
 
-        <Stylesheet id="leptos" href="/blah/tailwind.css"/>
+        <Stylesheet id="leptos" href="/pkg/tailwind.css"/>
         <Link rel="shortcut icon" type_="image/ico" href="/favicon.ico"/>
         <Router>
             <Routes>
