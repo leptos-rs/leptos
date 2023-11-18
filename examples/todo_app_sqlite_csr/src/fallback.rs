e47a6195569fdcc3b2de36ffa38dcc63fa49e83e
Greg Johnston <greg.johnston@gmail.com>
2023-11-18 08:24:15 -0500

    examples: add CSR with server functions example (closes #1975) (#2031)

---
+++ examples/todo_app_sqlite_csr/src/fallback.rs
@@ -0,0 +1,45 @@
+use axum::{
+    body::{boxed, Body, BoxBody},
+    extract::State,
+    http::{Request, Response, StatusCode, Uri},
+    response::{Html, IntoResponse, Response as AxumResponse},
+};
+use leptos::LeptosOptions;
+use leptos_integration_utils::html_parts_separated;
+use tower::ServiceExt;
+use tower_http::services::ServeDir;
+
+pub async fn file_or_index_handler(
+    uri: Uri,
+    State(options): State<LeptosOptions>,
+) -> AxumResponse {
+    let root = options.site_root.clone();
+    let res = get_static_file(uri.clone(), &root).await.unwrap();
+
+    if res.status() == StatusCode::OK {
+        res.into_response()
+    } else {
+        let (head, tail) = html_parts_separated(&options, None);
+
+        Html(format!("{head}</head><body>{tail}")).into_response()
+    }
+}
+
+async fn get_static_file(
+    uri: Uri,
+    root: &str,
+) -> Result<Response<BoxBody>, (StatusCode, String)> {
+    let req = Request::builder()
+        .uri(uri.clone())
+        .body(Body::empty())
+        .unwrap();
+    // `ServeDir` implements `tower::Service` so we can call it with `tower::ServiceExt::oneshot`
+    // This path is relative to the cargo root
+    match ServeDir::new(root).oneshot(req).await {
+        Ok(res) => Ok(res.map(boxed)),
+        Err(err) => Err((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {err}"),
+        )),
+    }
+}
