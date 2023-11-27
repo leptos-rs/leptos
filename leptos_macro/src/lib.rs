f9a84f562480e22f907a7d2e16044f19f17b67ce
Greg Johnston <greg.johnston@gmail.com>
2023-11-26 20:11:39 -0500

    remove unused suppressions

--- leptos_macro/src/lib.rs
+++ leptos_macro/src/lib.rs
@@ -621,7 +621,7 @@ pub fn component(args: proc_macro::TokenStream, s: TokenStream) -> TokenStream {
             mod #module_name {
                 use super::*;
 
-                #[allow(non_snake_case, dead_code, clippy::too_many_arguments, clippy::needless_lifetimes, unused_variables)]
+                #[allow(non_snake_case, dead_code, clippy::too_many_arguments)]
                 #unexpanded
             }
         }
@@ -633,7 +633,7 @@ pub fn component(args: proc_macro::TokenStream, s: TokenStream) -> TokenStream {
             mod #module_name {
                 use super::*;
 
-                #[allow(non_snake_case, dead_code, clippy::too_many_arguments, clippy::needless_lifetimes, unused_variables)]
+                #[allow(non_snake_case, dead_code, clippy::too_many_arguments)]
                 #dummy
             }
         }
