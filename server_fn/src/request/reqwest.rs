a5ce625c68d5c82b56c5121be8c2ff6290dfb290
Greg Johnston <greg.johnston@gmail.com>
2024-01-05 17:50:02 -0500

    actually use server functions in `ActionForm`

--- server_fn/src/request/reqwest.rs
+++ server_fn/src/request/reqwest.rs
@@ -88,4 +88,19 @@ impl<CustErr> ClientReq<CustErr> for Request {
             .build()
             .map_err(|e| ServerFnError::Request(e.to_string()))
     }
+
+    fn try_new_post_form_data(
+        path: &str,
+        accepts: &str,
+        content_type: &str,
+        body: Self::FormData,
+    ) -> Result<Self, ServerFnError<CustErr>> {
+        /*CLIENT
+        .post(path)
+        .header(ACCEPT, accepts)
+        .multipart(body)
+        .build()
+        .map_err(|e| ServerFnError::Request(e.to_string()))*/
+        todo!()
+    }
 }
