45dfb96bbe9de229fc1f9447df13954d8778b880
Greg Johnston <greg.johnston@gmail.com>
2024-01-10 20:15:30 -0500

    remove viz integration (see #2177)

--- router/src/components/redirect.rs
+++ router/src/components/redirect.rs
@@ -11,13 +11,12 @@ use std::rc::Rc;
 /// an absolute path, prefix it with `/`).
 ///
 /// **Note**: Support for server-side redirects is provided by the server framework
-/// integrations ([`leptos_actix`], [`leptos_axum`], and [`leptos_viz`]). If you’re not using one of those
+/// integrations ([`leptos_actix`] and [`leptos_axum`]. If you’re not using one of those
 /// integrations, you should manually provide a way of redirecting on the server
 /// using [`provide_server_redirect`].
 ///
 /// [`leptos_actix`]: <https://docs.rs/leptos_actix/>
 /// [`leptos_axum`]: <https://docs.rs/leptos_axum/>
-/// [`leptos_viz`]: <https://docs.rs/leptos_viz/>
 #[cfg_attr(
     any(debug_assertions, feature = "ssr"),
     tracing::instrument(level = "trace", skip_all,)
