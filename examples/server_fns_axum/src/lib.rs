d87997955d5008e643731b6caad8042e38574d7e
Greg Johnston <greg.johnston@gmail.com>
2024-01-14 16:17:06 -0500

    working on server fn example

---
+++ examples/server_fns_axum/src/lib.rs
@@ -0,0 +1,15 @@
+pub mod app;
+pub mod error_template;
+pub mod errors;
+#[cfg(feature = "ssr")]
+pub mod fallback;
+
+#[wasm_bindgen::prelude::wasm_bindgen]
+pub fn hydrate() {
+    use crate::app::TodoApp;
+
+    _ = console_log::init_with_level(log::Level::Error);
+    console_error_panic_hook::set_once();
+
+    leptos::mount_to_body(TodoApp);
+}
