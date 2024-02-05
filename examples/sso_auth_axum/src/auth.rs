06721c5fcd5bc0c8977f2c27c8b706f938355a6b
Steffen <99717310+ngutech21@users.noreply.github.com>
2024-02-05 11:20:53 +0100

    examples: fix typos in examples (#2260)

--- examples/sso_auth_axum/src/auth.rs
+++ examples/sso_auth_axum/src/auth.rs
@@ -47,7 +47,7 @@ pub mod ssr_imports {
             .await
             .ok()?;
 
-            //lets just get all the tokens the user can use, we will only use the full permissions if modifing them.
+            //lets just get all the tokens the user can use, we will only use the full permissions if modifying them.
             let sql_user_perms = sqlx::query_as::<_, SqlPermissionTokens>(
                 "SELECT token FROM user_permissions WHERE user_id = ?;",
             )
@@ -71,7 +71,7 @@ pub mod ssr_imports {
             .await
             .ok()?;
 
-            //lets just get all the tokens the user can use, we will only use the full permissions if modifing them.
+            //lets just get all the tokens the user can use, we will only use the full permissions if modifying them.
             let sql_user_perms = sqlx::query_as::<_, SqlPermissionTokens>(
                 "SELECT token FROM user_permissions WHERE user_id = ?;",
             )
