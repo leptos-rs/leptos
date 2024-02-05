ee0288c92084d398324f0d51e990c4eff580e7de
Greg Johnston <greg.johnston@gmail.com>
2024-02-04 20:07:33 -0500

    feat: modular, trait-based, `Send`/`Sync` reactive system

---
+++ reactive_graph/src/effect/effect.rs
@@ -0,0 +1,120 @@
+use crate::{
+    channel::{channel, Receiver},
+    effect::inner::EffectInner,
+    executor::Executor,
+    graph::{AnySubscriber, SourceSet, Subscriber, ToAnySubscriber},
+    owner::Owner,
+    OrPoisoned,
+};
+use futures::StreamExt;
+use std::{
+    mem,
+    sync::{Arc, RwLock},
+};
+
+pub struct Effect<T>
+where
+    T: 'static,
+{
+    value: Arc<RwLock<Option<T>>>,
+    inner: Arc<RwLock<EffectInner>>,
+}
+
+impl<T> Clone for Effect<T> {
+    fn clone(&self) -> Self {
+        Self {
+            value: Arc::clone(&self.value),
+            inner: Arc::clone(&self.inner),
+        }
+    }
+}
+
+fn effect_base() -> (Receiver, Owner, Arc<RwLock<EffectInner>>) {
+    let (mut observer, rx) = channel();
+
+    // spawn the effect asynchronously
+    // we'll notify once so it runs on the next tick,
+    // to register observed values
+    observer.notify();
+
+    let owner = Owner::new();
+    let inner = Arc::new(RwLock::new(EffectInner {
+        observer,
+        sources: SourceSet::new(),
+    }));
+
+    (rx, owner, inner)
+}
+
+impl<T> Effect<T>
+where
+    T: 'static,
+{
+    pub fn with_value_mut<U>(
+        &self,
+        fun: impl FnOnce(&mut T) -> U,
+    ) -> Option<U> {
+        self.value.write().or_poisoned().as_mut().map(fun)
+    }
+
+    pub fn new(mut fun: impl FnMut(Option<T>) -> T + 'static) -> Self {
+        let (mut rx, owner, inner) = effect_base();
+        let value = Arc::new(RwLock::new(None));
+
+        Executor::spawn_local({
+            let value = Arc::clone(&value);
+            let subscriber = inner.to_any_subscriber();
+
+            async move {
+                while rx.next().await.is_some() {
+                    subscriber.clear_sources(&subscriber);
+
+                    let old_value =
+                        mem::take(&mut *value.write().or_poisoned());
+                    let new_value = owner.with_cleanup(|| {
+                        subscriber.with_observer(|| fun(old_value))
+                    });
+                    *value.write().or_poisoned() = Some(new_value);
+                }
+            }
+        });
+        Self { value, inner }
+    }
+}
+
+impl<T> Effect<T>
+where
+    T: Send + Sync + 'static,
+{
+    pub fn new_sync(
+        mut fun: impl FnMut(Option<T>) -> T + Send + Sync + 'static,
+    ) -> Self {
+        let (mut rx, owner, inner) = effect_base();
+        let value = Arc::new(RwLock::new(None));
+
+        Executor::spawn({
+            let value = Arc::clone(&value);
+            let subscriber = inner.to_any_subscriber();
+
+            async move {
+                while rx.next().await.is_some() {
+                    subscriber.clear_sources(&subscriber);
+
+                    let old_value =
+                        mem::take(&mut *value.write().or_poisoned());
+                    let new_value = owner.with_cleanup(|| {
+                        subscriber.with_observer(|| fun(old_value))
+                    });
+                    *value.write().or_poisoned() = Some(new_value);
+                }
+            }
+        });
+        Self { value, inner }
+    }
+}
+
+impl<T> ToAnySubscriber for Effect<T> {
+    fn to_any_subscriber(&self) -> AnySubscriber {
+        self.inner.to_any_subscriber()
+    }
+}
---
+++ reactive_graph/src/signal.rs
@@ -0,0 +1,28 @@
+mod arc_read;
+mod arc_rw;
+mod arc_write;
+mod guards;
+mod read;
+mod rw;
+mod subscriber_traits;
+mod write;
+
+pub use arc_read::*;
+pub use arc_rw::*;
+pub use arc_write::*;
+pub use guards::*;
+pub use read::*;
+pub use rw::*;
+pub use write::*;
+
+#[inline(always)]
+#[track_caller]
+pub fn arc_signal<T>(value: T) -> (ArcReadSignal<T>, ArcWriteSignal<T>) {
+    ArcRwSignal::new(value).split()
+}
+
+#[inline(always)]
+#[track_caller]
+pub fn signal<T: Send + Sync>(value: T) -> (ReadSignal<T>, WriteSignal<T>) {
+    RwSignal::new(value).split()
+}
