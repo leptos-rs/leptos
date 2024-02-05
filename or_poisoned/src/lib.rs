f56023bb2561eb1bebe62dfe9a37037443dc763b
Greg Johnston <greg.johnston@gmail.com>
2024-02-04 20:55:48 -0500

    chore: split `OrPoisoned` trait into its own crate for reuse

---
+++ or_poisoned/src/lib.rs
@@ -0,0 +1,54 @@
+//! Provides a simple trait that unwraps the locks provide by [`std::sync::RwLock`].
+//!
+//! In every case, this is the same as calling `.expect("lock poisoned")`. However, it
+//! does not use `.unwrap()` or `.expect()`, which makes it easier to distinguish from
+//! other forms of unwrapping when reading code.
+//!
+//! ```rust
+//! use or_poisoned::OrPoisoned;
+//! use std::sync::RwLock;
+//!
+//! let lock = RwLock::new(String::from("Hello!"));
+//!
+//! let read = lock.read().or_poisoned();
+//! // this is identical to
+//! let read = lock.read().unwrap();
+//! ```
+
+#![forbid(unsafe_code)]
+#![deny(missing_docs)]
+
+use std::sync::{PoisonError, RwLockReadGuard, RwLockWriteGuard};
+
+/// Unwraps a lock.
+pub trait OrPoisoned {
+    /// The inner guard type.
+    type Inner;
+
+    /// Unwraps the lock.
+    ///
+    /// ## Panics
+    ///
+    /// Will panic if the lock is poisoned.
+    fn or_poisoned(self) -> Self::Inner;
+}
+
+impl<'a, T> OrPoisoned
+    for Result<RwLockReadGuard<'a, T>, PoisonError<RwLockReadGuard<'a, T>>>
+{
+    type Inner = RwLockReadGuard<'a, T>;
+
+    fn or_poisoned(self) -> Self::Inner {
+        self.expect("lock poisoned")
+    }
+}
+
+impl<'a, T> OrPoisoned
+    for Result<RwLockWriteGuard<'a, T>, PoisonError<RwLockWriteGuard<'a, T>>>
+{
+    type Inner = RwLockWriteGuard<'a, T>;
+
+    fn or_poisoned(self) -> Self::Inner {
+        self.expect("lock poisoned")
+    }
+}
