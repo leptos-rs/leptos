d70536ebb028421d284c64aa0676b5b1455625e0
Joseph Cruz <agilarity@gmail.com>
2024-02-05 19:07:58 -0500

    refactor(test-report): extract functions

--- examples/cargo-make/scripts/web-report.sh
+++ examples/cargo-make/scripts/web-report.sh
@@ -7,92 +7,118 @@ ITALIC="\e[3m"
 YELLOW="\e[0;33m"
 RESET="\e[0m"
 
-echo
-echo -e "${YELLOW}Web Test Technology${RESET}"
-echo
+function print_report {
+    print_header
+    print_crate_tags "$@"
+    print_footer
+}
 
-makefile_paths=$(find . -name Makefile.toml -not -path '*/target/*' -not -path '*/node_modules/*' |
-    sed 's%./%%' |
-    sed 's%/Makefile.toml%%' |
-    grep -v Makefile.toml |
-    sort -u)
+function print_header {
+    echo
+    echo -e "${YELLOW}Web Test Technology${RESET}"
+    echo
+}
 
-start_path=$(pwd)
+function print_crate_tags {
+    local makefile_paths
+    makefile_paths=$(find_makefile_lines)
 
-for path in $makefile_paths; do
-    cd "$path"
+    local start_path
+    start_path=$(pwd)
 
-    crate_symbols=
+    for path in $makefile_paths; do
+        cd "$path"
 
-    pw_count=$(find . -name playwright.config.ts | wc -l)
+        local crate_tags=
 
-    while read -r line; do
-        case $line in
-        *"cucumber"*)
-            crate_symbols=$crate_symbols"C"
-            ;;
-        *"fantoccini"*)
-            crate_symbols=$crate_symbols"D"
-            ;;
-        esac
-    done <"./Cargo.toml"
+        local pw_count
+        pw_count=$(find . -name playwright.config.ts | wc -l)
 
-    while read -r line; do
-        case $line in
-        *"cargo-make/wasm-test.toml"*)
-            crate_symbols=$crate_symbols"W"
-            ;;
-        *"cargo-make/playwright-test.toml"*)
-            crate_symbols=$crate_symbols"P"
-            crate_symbols=$crate_symbols"N"
-            ;;
-        *"cargo-make/playwright-trunk-test.toml"*)
-            crate_symbols=$crate_symbols"P"
-            crate_symbols=$crate_symbols"T"
-            ;;
-        *"cargo-make/trunk_server.toml"*)
-            crate_symbols=$crate_symbols"T"
-            ;;
-        *"cargo-make/cargo-leptos-webdriver-test.toml"*)
-            crate_symbols=$crate_symbols"L"
-            ;;
-        *"cargo-make/cargo-leptos-test.toml"*)
-            crate_symbols=$crate_symbols"L"
-            if [ "$pw_count" -gt 0 ]; then
-                crate_symbols=$crate_symbols"P"
+        while read -r line; do
+            case $line in
+            *"cucumber"*)
+                crate_tags=$crate_tags"C"
+                ;;
+            *"fantoccini"*)
+                crate_tags=$crate_tags"D"
+                ;;
+            esac
+        done <"./Cargo.toml"
+
+        while read -r line; do
+            case $line in
+            *"cargo-make/wasm-test.toml"*)
+                crate_tags=$crate_tags"W"
+                ;;
+            *"cargo-make/playwright-test.toml"*)
+                crate_tags=$crate_tags"P"
+                crate_tags=$crate_tags"N"
+                ;;
+            *"cargo-make/playwright-trunk-test.toml"*)
+                crate_tags=$crate_tags"P"
+                crate_tags=$crate_tags"T"
+                ;;
+            *"cargo-make/trunk_server.toml"*)
+                crate_tags=$crate_tags"T"
+                ;;
+            *"cargo-make/cargo-leptos-webdriver-test.toml"*)
+                crate_tags=$crate_tags"L"
+                ;;
+            *"cargo-make/cargo-leptos-test.toml"*)
+                crate_tags=$crate_tags"L"
+                if [ "$pw_count" -gt 0 ]; then
+                    crate_tags=$crate_tags"P"
+                fi
+                ;;
+            esac
+        done <"./Makefile.toml"
+
+        # Sort list of tools
+        local sorted_crate_symbols
+        sorted_crate_symbols=$(echo "$crate_tags" | grep -o . | sort | tr -d "\n")
+
+        local formatted_crate_symbols
+        formatted_crate_symbols=" ➤ ${BOLD}${YELLOW}$sorted_crate_symbols${RESET}"
+
+        local crate_line=$path
+
+        if [ "$#" -gt 0 ]; then
+            # Show all examples
+            if [ -n "$crate_tags" ]; then
+                crate_line=$crate_line$formatted_crate_symbols
             fi
-            ;;
-        esac
-    done <"./Makefile.toml"
-
-    # Sort list of tools
-    sorted_crate_symbols=$(echo ${crate_symbols} | grep -o . | sort | tr -d "\n")
-
-    formatted_crate_symbols=" ➤ ${BOLD}${YELLOW}${sorted_crate_symbols}${RESET}"
-    crate_line=$path
-    if [ ! -z ${1+x} ]; then
-        # Show all examples
-        if [ ! -z $crate_symbols ]; then
+            echo -e "$crate_line"
+        elif [ -n "$crate_tags" ]; then
+            # Show configured examples
             crate_line=$crate_line$formatted_crate_symbols
+            echo -e "$crate_line"
         fi
-        echo -e "$crate_line"
-    elif [ ! -z $crate_symbols ]; then
-        # Filter out examples that do not run tests in `ci`
-        crate_line=$crate_line$formatted_crate_symbols
-        echo -e "$crate_line"
-    fi
 
-    cd "$start_path"
-done
+        cd "$start_path"
+    done
+}
 
-c="${BOLD}${YELLOW}C${RESET} = Cucumber"
-d="${BOLD}${YELLOW}D${RESET} = WebDriver"
-l="${BOLD}${YELLOW}L${RESET} = Cargo Leptos"
-n="${BOLD}${YELLOW}N${RESET} = Node"
-p="${BOLD}${YELLOW}P${RESET} = Playwright"
-t="${BOLD}${YELLOW}T${RESET} = Trunk"
-w="${BOLD}${YELLOW}W${RESET} = WASM"
+function find_makefile_lines {
+    find . -name Makefile.toml -not -path '*/target/*' -not -path '*/node_modules/*' |
+        sed 's%./%%' |
+        sed 's%/Makefile.toml%%' |
+        grep -v Makefile.toml |
+        sort -u
+}
 
-echo
-echo -e "${ITALIC}Keys:${RESET} $c, $d, $l, $n, $p, $t, $w"
-echo
+function print_footer {
+    c="${BOLD}${YELLOW}C${RESET} = Cucumber"
+    d="${BOLD}${YELLOW}D${RESET} = WebDriver"
+    l="${BOLD}${YELLOW}L${RESET} = Cargo Leptos"
+    n="${BOLD}${YELLOW}N${RESET} = Node"
+    p="${BOLD}${YELLOW}P${RESET} = Playwright"
+    t="${BOLD}${YELLOW}T${RESET} = Trunk"
+    w="${BOLD}${YELLOW}W${RESET} = WASM"
+
+    echo
+    echo -e "${ITALIC}Keys:${RESET} $c, $d, $l, $n, $p, $t, $w"
+    echo
+}
+
+TIMEFORMAT="./web-report.sh completed in %3lR"
+time "${@:-print_report}" # Show the report by default
