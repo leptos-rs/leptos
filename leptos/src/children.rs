30cd69038b7cb2634eda2eccd769d97e6677b374
Greg Johnston <greg.johnston@gmail.com>
2024-02-10 14:18:56 -0500

    stash

--- leptos/src/children.rs
+++ leptos/src/children.rs
@@ -1,62 +1,100 @@
-use leptos_dom::Fragment;
-use std::rc::Rc;
+use std::sync::Arc;
+use tachys::{
+    renderer::dom::Dom,
+    view::{
+        any_view::{AnyView, IntoAny},
+        RenderHtml,
+    },
+};
 
 /// The most common type for the `children` property on components,
 /// which can only be called once.
-pub type Children = Box<dyn FnOnce() -> Fragment>;
+pub type Children = Box<dyn FnOnce() -> AnyView<Dom>>;
 
 /// A type for the `children` property on components that can be called
 /// more than once.
-pub type ChildrenFn = Rc<dyn Fn() -> Fragment>;
+pub type ChildrenFn = Arc<dyn Fn() -> AnyView<Dom>>;
 
 /// A type for the `children` property on components that can be called
 /// more than once, but may mutate the children.
-pub type ChildrenFnMut = Box<dyn FnMut() -> Fragment>;
+pub type ChildrenFnMut = Box<dyn FnMut() -> AnyView<Dom>>;
 
-// This is to still support components that accept `Box<dyn Fn() -> Fragment>` as a children.
-type BoxedChildrenFn = Box<dyn Fn() -> Fragment>;
+// This is to still support components that accept `Box<dyn Fn() -> AnyView>` as a children.
+type BoxedChildrenFn = Box<dyn Fn() -> AnyView<Dom>>;
 
 #[doc(hidden)]
 pub trait ToChildren<F> {
     fn to_children(f: F) -> Self;
 }
 
-impl<F> ToChildren<F> for Children
+impl<F, C> ToChildren<F> for Children
 where
-    F: FnOnce() -> Fragment + 'static,
+    F: FnOnce() -> C + 'static,
+    C: RenderHtml<Dom> + 'static,
 {
     #[inline]
     fn to_children(f: F) -> Self {
-        Box::new(f)
+        Box::new(move || f().into_any())
     }
 }
 
-impl<F> ToChildren<F> for ChildrenFn
+impl<F, C> ToChildren<F> for ChildrenFn
 where
-    F: Fn() -> Fragment + 'static,
+    F: Fn() -> C + 'static,
+    C: RenderHtml<Dom> + 'static,
 {
     #[inline]
     fn to_children(f: F) -> Self {
-        Rc::new(f)
+        Arc::new(move || f().into_any())
     }
 }
 
-impl<F> ToChildren<F> for ChildrenFnMut
+impl<F, C> ToChildren<F> for ChildrenFnMut
 where
-    F: FnMut() -> Fragment + 'static,
+    F: Fn() -> C + 'static,
+    C: RenderHtml<Dom> + 'static,
 {
     #[inline]
     fn to_children(f: F) -> Self {
-        Box::new(f)
+        Box::new(move || f().into_any())
     }
 }
 
-impl<F> ToChildren<F> for BoxedChildrenFn
+impl<F, C> ToChildren<F> for BoxedChildrenFn
 where
-    F: Fn() -> Fragment + 'static,
+    F: Fn() -> C + 'static,
+    C: RenderHtml<Dom> + 'static,
 {
     #[inline]
     fn to_children(f: F) -> Self {
-        Box::new(f)
+        Box::new(move || f().into_any())
+    }
+}
+
+/// New-type wrapper for the a function that returns a view with `From` and `Default` traits implemented
+/// to enable optional props in for example `<Show>` and `<Suspense>`.
+#[derive(Clone)]
+pub struct ViewFn(Arc<dyn Fn() -> AnyView<Dom>>);
+
+impl Default for ViewFn {
+    fn default() -> Self {
+        Self(Arc::new(|| ().into_any()))
+    }
+}
+
+impl<F, C> From<F> for ViewFn
+where
+    F: Fn() -> C + 'static,
+    C: RenderHtml<Dom> + 'static,
+{
+    fn from(value: F) -> Self {
+        Self(Arc::new(move || value().into_any()))
+    }
+}
+
+impl ViewFn {
+    /// Execute the wrapped function
+    pub fn run(&self) -> AnyView<Dom> {
+        (self.0)()
     }
 }
---
+++ leptos/src/into_view.rs
@@ -0,0 +1,120 @@
+use tachys::{
+    hydration::Cursor,
+    renderer::dom::Dom,
+    ssr::StreamBuilder,
+    view::{Mountable, Position, PositionState, Render, RenderHtml},
+};
+
+pub struct View<T>(T);
+
+pub trait IntoView: Sized {
+    fn into_view(self) -> View<Self>;
+}
+
+impl<T: Render<Dom> + RenderHtml<Dom>> IntoView for T {
+    fn into_view(self) -> View<Self> {
+        View(self)
+    }
+}
+
+impl<T: Render<Dom>> Render<Dom> for View<T> {
+    type State = T::State;
+
+    fn build(self) -> Self::State {
+        self.0.build()
+    }
+
+    fn rebuild(self, state: &mut Self::State) {
+        self.0.rebuild(state)
+    }
+}
+
+impl<T: RenderHtml<Dom>> RenderHtml<Dom> for View<T> {
+    const MIN_LENGTH: usize = <T as RenderHtml<Dom>>::MIN_LENGTH;
+
+    fn to_html_with_buf(self, buf: &mut String, position: &mut Position) {
+        self.0.to_html_with_buf(buf, position);
+    }
+
+    fn to_html_async_with_buf<const OUT_OF_ORDER: bool>(
+        self,
+        buf: &mut StreamBuilder,
+        position: &mut Position,
+    ) where
+        Self: Sized,
+    {
+        self.0.to_html_async_with_buf::<OUT_OF_ORDER>(buf, position)
+    }
+
+    fn hydrate<const FROM_SERVER: bool>(
+        self,
+        cursor: &Cursor<Dom>,
+        position: &PositionState,
+    ) -> Self::State {
+        self.0.hydrate::<FROM_SERVER>(cursor, position)
+    }
+}
+
+/*pub trait IntoView {
+    const MIN_HTML_LENGTH: usize;
+
+    type State: Mountable<Dom>;
+
+    fn build(self) -> Self::State;
+
+    fn rebuild(self, state: &mut Self::State);
+
+    fn to_html_with_buf(self, buf: &mut String, position: &mut Position);
+
+    fn to_html_async_with_buf<const OUT_OF_ORDER: bool>(
+        self,
+        buf: &mut StreamBuilder,
+        position: &mut Position,
+    );
+
+    fn hydrate<const FROM_SERVER: bool>(
+        self,
+        cursor: &Cursor<Dom>,
+        position: &PositionState,
+    ) -> Self::State;
+}
+
+impl<T: RenderHtml<Dom>> IntoView for T {}
+
+impl<T: IntoView> Render<Dom> for T {
+    type State = <Self as IntoView>::State;
+
+    fn build(self) -> Self::State {
+        IntoView::build(self)
+    }
+
+    fn rebuild(self, state: &mut Self::State) {
+        IntoView::rebuild(self, state);
+    }
+}
+
+impl<T: IntoView> RenderHtml<Dom> for T {
+    const MIN_LENGTH: usize = T::MIN_HTML_LENGTH;
+
+    fn to_html_with_buf(self, buf: &mut String, position: &mut Position) {
+        IntoView::to_html_with_buf(self, buf, position);
+    }
+
+    fn to_html_async_with_buf<const OUT_OF_ORDER: bool>(
+        self,
+        buf: &mut StreamBuilder,
+        position: &mut Position,
+    ) where
+        Self: Sized,
+    {
+        IntoView::to_html_async_with_buf::<OUT_OF_ORDER>(self, buf, position);
+    }
+
+    fn hydrate<const FROM_SERVER: bool>(
+        self,
+        cursor: &Cursor<Dom>,
+        position: &PositionState,
+    ) -> Self::State {
+        IntoView::hydrate::<FROM_SERVER>(self, cursor, position)
+    }
+}*/
