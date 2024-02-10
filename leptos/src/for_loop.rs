05a4d2b05e74c675f8df4c5c7044fe8834439450
Greg Johnston <greg.johnston@gmail.com>
2024-02-10 14:18:56 -0500

    stash

--- leptos/src/for_loop.rs
+++ leptos/src/for_loop.rs
@@ -1,113 +1,63 @@
-use leptos_dom::IntoView;
 use leptos_macro::component;
-use std::hash::Hash;
+use std::{hash::Hash, marker::PhantomData};
+use tachys::{
+    renderer::Renderer,
+    view::{keyed::keyed, RenderHtml},
+};
 
-/// Iterates over children and displays them, keyed by the `key` function given.
-///
-/// This is much more efficient than naively iterating over nodes with `.iter().map(|n| view! { ... })...`,
-/// as it avoids re-creating DOM nodes that are not being changed.
-///
-/// ```
-/// # use leptos::*;
-///
-/// #[derive(Copy, Clone, Debug, PartialEq, Eq)]
-/// struct Counter {
-///   id: usize,
-///   count: RwSignal<i32>
-/// }
-///
-/// #[component]
-/// fn Counters() -> impl IntoView {
-///   let (counters, set_counters) = create_signal::<Vec<Counter>>(vec![]);
-///
-///   view! {
-///     <div>
-///       <For
-///         // a function that returns the items we're iterating over; a signal is fine
-///         each=move || counters.get()
-///         // a unique key for each item
-///         key=|counter| counter.id
-///         // renders each item to a view
-///         children=move |counter: Counter| {
-///           view! {
-///             <button>"Value: " {move || counter.count.get()}</button>
-///           }
-///         }
-///       />
-///       <For
-///         // a function that returns the items we're iterating over; a signal is fine
-///         each=move || counters.get()
-///         // a unique key for each item
-///         key=|counter| counter.id
-///         // renders each item to a view
-///         children=move |counter: Counter| {
-///           view! {
-///             <button>"Value: " {move || counter.count.get()}</button>
-///           }
-///         }
-///       />
-///     </div>
-///   }
-/// }
-/// ```
-#[cfg_attr(
-    any(debug_assertions, feature = "ssr"),
-    tracing::instrument(level = "trace", skip_all)
-)]
-#[component(transparent)]
-pub fn For<IF, I, T, EF, N, KF, K>(
+#[component]
+pub fn For<Rndr, IF, I, T, EF, N, KF, K>(
     /// Items over which the component should iterate.
     each: IF,
     /// A key function that will be applied to each item.
     key: KF,
     /// A function that takes the item, and returns the view that will be displayed for each item.
-    ///
-    /// ## Syntax
-    /// This can be passed directly in the `view` children of the `<For/>` by using the
-    /// `let:` syntax to specify the name for the data variable passed in the argument.
-    ///
-    /// ```rust
-    /// # use leptos::*;
-    /// # if false {
-    /// let (data, set_data) = create_signal(vec![0, 1, 2]);
-    /// view! {
-    ///     <For
-    ///         each=move || data.get()
-    ///         key=|n| *n
-    ///         // stores the item in each row in a variable named `data`
-    ///         let:data
-    ///     >
-    ///         <p>{data}</p>
-    ///     </For>
-    /// }
-    /// # ;
-    /// # }
-    /// ```
-    /// is the same as
-    ///  ```rust
-    /// # use leptos::*;
-    /// # if false {
-    /// let (data, set_data) = create_signal(vec![0, 1, 2]);
-    /// view! {
-    ///     <For
-    ///         each=move || data.get()
-    ///         key=|n| *n
-    ///         children=|data| view! { <p>{data}</p> }
-    ///     />
-    /// }
-    /// # ;
-    /// # }
-    /// ```
     children: EF,
-) -> impl IntoView
+    #[prop(optional)] _rndr: PhantomData<Rndr>,
+) -> impl RenderHtml<Rndr>
 where
     IF: Fn() -> I + 'static,
     I: IntoIterator<Item = T>,
-    EF: Fn(T) -> N + 'static,
-    N: IntoView + 'static,
-    KF: Fn(&T) -> K + 'static,
+    EF: Fn(T) -> N + Clone + 'static,
+    N: RenderHtml<Rndr> + 'static,
+    KF: Fn(&T) -> K + Clone + 'static,
     K: Eq + Hash + 'static,
     T: 'static,
+    Rndr: Renderer + 'static,
+    Rndr::Node: Clone,
+    Rndr::Element: Clone,
 {
-    leptos_dom::Each::new(each, key, children).into_view()
+    move || keyed(each(), key.clone(), children.clone())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::For;
+    use leptos_macro::view;
+    use reactive_graph::{signal::RwSignal, signal_traits::SignalGet};
+    use tachys::{
+        html::element::HtmlElement, prelude::ElementChild,
+        renderer::mock_dom::MockDom, view::Render,
+    };
+
+    #[test]
+    fn creates_list() {
+        let values = RwSignal::new(vec![1, 2, 3, 4, 5]);
+        let list: HtmlElement<_, _, _, MockDom> = view! {
+            <ol>
+                <For
+                    each=move || values.get()
+                    key=|i| *i
+                    let:i
+                >
+                    <li>{i}</li>
+                </For>
+            </ol>
+        };
+        let list = list.build();
+        assert_eq!(
+            list.el.to_debug_html(),
+            "<ol><li>1</li><li>2</li><li>3</li><li>4</li><li>5</li></ol>"
+        );
+    }
 }
