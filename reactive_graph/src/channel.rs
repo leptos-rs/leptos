f91d0d98c085e20f0648ae184fbdf7fe47b050fd
Greg Johnston <greg.johnston@gmail.com>
2024-02-12 09:07:48 -0500

    fix: close memory leak in tasks waiting on channels

--- reactive_graph/src/channel.rs
+++ reactive_graph/src/channel.rs
@@ -20,6 +20,25 @@ struct Inner {
     set: AtomicBool,
 }
 
+impl Drop for Inner {
+    fn drop(&mut self) {
+        // Sender holds a strong reference to Inner, and Receiver holds a weak reference to Inner,
+        // so this will run when the Sender is dropped.
+        //
+        // The Receiver is usually owned by a spawned async task that is always waiting on the next
+        // value from its Stream. While it's waiting, it continues owning all the data it has
+        // captured. That data will not be dropped until the the stream ends.
+        //
+        // If we don't wake the waker a final time here, the spawned task will continue waiting for
+        // a final message from the Receiver that never arrives, because the waker never wakes it
+        // up again. So we wake the waker a final time, which tries to upgrade the Receiver, which
+        // fails, which causes the stream to yield Poll::Ready(None), ending the stream, and
+        // therefore ending the task, and therefore dropping all data that the stream has
+        // captured, avoiding a memory leak.
+        self.waker.wake();
+    }
+}
+
 pub fn channel() -> (Sender, Receiver) {
     let inner = Arc::new(Inner {
         waker: AtomicWaker::new(),
