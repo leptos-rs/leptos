b49a13f8c126002bec0d7ce5420c3a1b9215d046
Greg Johnston <greg.johnston@gmail.com>
2024-02-16 19:49:13 -0500

    work on async demo

---
+++ examples/async-demo/src/main.rs
@@ -0,0 +1,8 @@
+use fetch::async_example;
+use leptos::*;
+
+pub fn main() {
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+    mount_to_body(async_example)
+}
