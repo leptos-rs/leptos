b22226cae9f196fbf38a74e90511b30b3a19a037
Greg Johnston <greg.johnston@gmail.com>
2024-02-24 14:47:53 -0500

    work on routing

--- routing/src/matching/horizontal/mod.rs
+++ routing/src/matching/horizontal/mod.rs
@@ -1,6 +1,5 @@
 use crate::PathSegment;
 use alloc::vec::Vec;
-use core::str::Chars;
 
 mod param_segments;
 mod static_segment;
@@ -16,11 +15,7 @@ pub use static_segment::*;
 /// as subsequent segments of the URL and tries to match them all. For a "vertical"
 /// matching that sees a tuple as alternatives to one another, see [`RouteChild`](super::RouteChild).
 pub trait PossibleRouteMatch {
-    fn matches(&self, path: &str) -> bool {
-        self.matches_iter(&mut path.chars())
-    }
-
-    fn matches_iter(&self, path: &mut Chars) -> bool;
+    fn matches<'a>(&self, path: &'a str) -> Option<&'a str>;
 
     fn test<'a>(&self, path: &'a str) -> Option<PartialPathMatch<'a>>;
 
