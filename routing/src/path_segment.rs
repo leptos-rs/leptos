2ef1ae46a9a3919039b4b46211e6b5310399263c
Greg Johnston <greg.johnston@gmail.com>
2024-03-02 10:40:55 -0500

    stash

--- routing/src/path_segment.rs
+++ routing/src/path_segment.rs
@@ -2,6 +2,7 @@ use alloc::borrow::Cow;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum PathSegment {
+    Unit,
     Static(Cow<'static, str>),
     Param(Cow<'static, str>),
     Splat(Cow<'static, str>),
