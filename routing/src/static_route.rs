708ccea79d21b783743f8d7315361e77fe252ddd
Greg Johnston <greg.johnston@gmail.com>
2024-03-03 21:33:10 -0500

    working on nesting routing

---
+++ routing/src/static_route.rs
@@ -0,0 +1,20 @@
+/// The mode to use when rendering the route statically.
+/// On mode `Upfront`, the route will be built with the server is started using the provided static
+/// data. On mode `Incremental`, the route will be built on the first request to it and then cached
+/// and returned statically for subsequent requests.
+#[derive(Default, Copy, Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub enum StaticMode {
+    #[default]
+    Upfront,
+    Incremental,
+}
+
+// TODO
+#[derive(Debug)]
+pub struct StaticDataMap;
+
+impl StaticDataMap {
+    pub fn new() -> Self {
+        Self
+    }
+}
