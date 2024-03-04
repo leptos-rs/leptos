87772466d11be97c09e093a053ef3cfddb71e922
Greg Johnston <greg.johnston@gmail.com>
2024-03-03 21:33:10 -0500

    working on nesting routing

--- routing_utils/src/lib.rs
+++ routing_utils/src/lib.rs
@@ -3,18 +3,8 @@
 #[macro_use]
 extern crate alloc;
 
-//mod generate_route_list;
 pub mod location;
 pub mod matching;
 pub mod params;
 mod path_segment;
 pub use path_segment::*;
-//pub mod matching;
-//cfg(feature = "reaccy")]
-//pub mod reactive;
-//mod render_mode;
-//pub mod route;
-//pub mod router;
-//mod static_render;
-//pub use generate_route_list::*;
-//pub use render_mode::*;
