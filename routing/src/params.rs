1e0a3006cf0e9d7e2575fc5bea88c91b599b1c31
Greg Johnston <greg.johnston@gmail.com>
2024-03-09 20:29:41 -0500

    navigation between nested routes

--- routing/src/params.rs
+++ routing/src/params.rs
@@ -1,5 +1,7 @@
+use std::borrow::Cow;
+
 #[derive(Debug, Default, Clone, PartialEq, Eq, Hash)]
-pub struct Params(Vec<(String, String)>);
+pub struct Params(Vec<(Cow<'static, str>, String)>);
 
 impl Params {
     pub fn new() -> Self {
@@ -7,8 +9,16 @@ impl Params {
     }
 }
 
-impl FromIterator<(String, String)> for Params {
-    fn from_iter<T: IntoIterator<Item = (String, String)>>(iter: T) -> Self {
-        Self(iter.into_iter().collect())
+impl<K, V> FromIterator<(K, V)> for Params
+where
+    K: Into<Cow<'static, str>>,
+    V: Into<String>,
+{
+    fn from_iter<T: IntoIterator<Item = (K, V)>>(iter: T) -> Self {
+        Self(
+            iter.into_iter()
+                .map(|(k, v)| (k.into(), v.into()))
+                .collect(),
+        )
     }
 }
