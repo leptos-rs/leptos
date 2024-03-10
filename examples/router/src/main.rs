710450543cce5535b01f36480c48c31231e1b70f
Greg Johnston <greg.johnston@gmail.com>
2024-03-09 20:29:41 -0500

    navigation between nested routes

--- examples/router/src/main.rs
+++ examples/router/src/main.rs
@@ -1,16 +1,16 @@
 use leptos::*;
 use router::*;
-//use tracing_subscriber::fmt;
-//use tracing_subscriber_wasm::MakeConsoleWriter;
+use tracing_subscriber::fmt;
+use tracing_subscriber_wasm::MakeConsoleWriter;
 
 pub fn main() {
-    /*fmt()
+    fmt()
         .with_writer(
             MakeConsoleWriter::default()
                 .map_trace_level_to(tracing::Level::DEBUG),
         )
         .without_time()
         .init();
-    console_error_panic_hook::set_once();*/
-    mount_to_body(|| view! { <RouterExample/> })
+    console_error_panic_hook::set_once();
+    mount_to_body(RouterExample);
 }
