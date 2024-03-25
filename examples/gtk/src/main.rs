b8b248315bb515ab046afec2fe87a91c498377ad
Greg Johnston <greg.johnston@gmail.com>
2024-03-25 08:16:22 -0400

    GTK example for 0.7

--- examples/gtk/src/main.rs
+++ examples/gtk/src/main.rs
@@ -1,59 +1,80 @@
-use gtk::{prelude::*, Application, ApplicationWindow, Button};
-use leptos::*;
+use gtk::{prelude::*, Application, ApplicationWindow, Orientation};
+use leptos::{
+    prelude::*,
+    reactive_graph::{effect::Effect, owner::Owner, signal::RwSignal},
+    Executor, For, ForProps,
+};
+use leptos_gtk::{button, r#box, Box_, LeptosGtk};
+use std::{mem, thread, time::Duration};
+mod leptos_gtk;
 
 const APP_ID: &str = "dev.leptos.Counter";
 
 // Basic GTK app setup from https://gtk-rs.org/gtk4-rs/stable/latest/book/hello_world.html
 fn main() {
-    let _ = create_runtime();
-    // Create a new application
+    // use the glib event loop to power the reactive system
+    _ = Executor::init_glib();
+
     let app = Application::builder().application_id(APP_ID).build();
 
-    // Connect to "activate" signal of `app`
-    app.connect_activate(build_ui);
+    app.connect_activate(|app| {
+        let owner = Owner::new();
+        let view = owner.with(ui);
 
-    // Run the application
+        // Connect to "activate" signal of `app`
+        let state = view.build();
+
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .title("TachyGTK")
+            .child(&state.0 .0)
+            .build();
+        // Present window
+        window.present();
+
+        mem::forget((owner, state));
+    });
+
     app.run();
 }
 
-fn build_ui(app: &Application) {
-    let button = counter_button();
+fn ui() -> Box_<impl Render<LeptosGtk>> {
+    let value = RwSignal::new(0);
+    let rows = RwSignal::new(vec![1, 2, 3, 4, 5]);
 
-    // Create a window and set the title
-    let window = ApplicationWindow::builder()
-        .application(app)
-        .title("Leptos-GTK")
-        .child(&button)
-        .build();
+    Effect::new(move |_| {
+        println!("value = {}", value.get());
+    });
 
-    // Present window
-    window.present();
+    // just an example of multithreaded reactivity
+    thread::spawn(move || loop {
+        thread::sleep(Duration::from_millis(250));
+        value.update(|n| *n += 1);
+    });
+
+    vstack((
+        hstack((
+            button("-1", move |_| value.update(|n| *n -= 1)),
+            move || value.get().to_string(),
+            button("+1", move |_| value.update(|n| *n += 1)),
+        )),
+        button("Swap", move |_| {
+            rows.update(|items| {
+                items.swap(1, 3);
+            })
+        }),
+        vstack(For(ForProps::builder()
+            .each(move || rows.get())
+            .key(|k| *k)
+            .children(|v| v)
+            .build())),
+    ))
 }
 
-fn counter_button() -> Button {
-    let (value, set_value) = create_signal(0);
+fn vstack(children: impl Render<LeptosGtk>) -> Box_<impl Render<LeptosGtk>> {
+    r#box(Orientation::Vertical, 12, children)
+}
 
-    // Create a button with label and margins
-    let button = Button::builder()
-        .label("Count: ")
-        .margin_top(12)
-        .margin_bottom(12)
-        .margin_start(12)
-        .margin_end(12)
-        .build();
-
-    // Connect to "clicked" signal of `button`
-    button.connect_clicked(move |_| {
-        // Set the label to "Hello World!" after the button has been clicked on
-        set_value.update(|value| *value += 1);
-    });
-
-    create_effect({
-        let button = button.clone();
-        move |_| {
-            button.set_label(&format!("Count: {}", value()));
-        }
-    });
-
-    button
+fn hstack(children: impl Render<LeptosGtk>) -> impl Render<LeptosGtk> {
+    r#box(Orientation::Horizontal, 12, children)
 }
