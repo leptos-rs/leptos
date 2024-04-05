a82af6110fbfc9fe312edf9587026317f980e4dd
Adrian <60581773+solweo@users.noreply.github.com>
2024-04-05 06:09:55 +0300

    ex: counter_url_query; to stable (#2499)

--- examples/counter_url_query/src/lib.rs
+++ examples/counter_url_query/src/lib.rs
@@ -7,17 +7,17 @@ use leptos_router::*;
 #[component]
 pub fn SimpleQueryCounter() -> impl IntoView {
     let (count, set_count) = create_query_signal::<i32>("count");
-    let clear = move |_| set_count(None);
-    let decrement = move |_| set_count(Some(count().unwrap_or(0) - 1));
-    let increment = move |_| set_count(Some(count().unwrap_or(0) + 1));
+    let clear = move |_| set_count.set(None);
+    let decrement = move |_| set_count.set(Some(count.get().unwrap_or(0) - 1));
+    let increment = move |_| set_count.set(Some(count.get().unwrap_or(0) + 1));
 
     let (msg, set_msg) = create_query_signal::<String>("message");
     let update_msg = move |ev| {
         let new_msg = event_target_value(&ev);
         if new_msg.is_empty() {
-            set_msg(None);
+            set_msg.set(None);
         } else {
-            set_msg(Some(new_msg));
+            set_msg.set(Some(new_msg));
         }
     };
 
@@ -25,13 +25,13 @@ pub fn SimpleQueryCounter() -> impl IntoView {
         <div>
             <button on:click=clear>"Clear"</button>
             <button on:click=decrement>"-1"</button>
-            <span>"Value: " {move || count().unwrap_or(0)} "!"</span>
+            <span>"Value: " {move || count.get().unwrap_or(0)} "!"</span>
             <button on:click=increment>"+1"</button>
 
             <br />
 
             <input
-                prop:value=move || msg().unwrap_or_default()
+                prop:value=move || msg.get().unwrap_or_default()
                 on:input=update_msg
             />
         </div>
