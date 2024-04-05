ffe82dff7805ffea50dc27f6cea8f59357e3bcdd
Greg Johnston <greg.johnston@gmail.com>
2024-04-04 20:23:00 -0400

    fix ErrorBoundary/Suspense

--- examples/fetch/src/lib.rs
+++ examples/fetch/src/lib.rs
@@ -25,7 +25,7 @@ type CatCount = usize;
 
 async fn fetch_cats(count: CatCount) -> Result<Vec<String>> {
     if count > 0 {
-        gloo_timers::future::TimeoutFuture::new(1000).await;
+        gloo_timers::future::TimeoutFuture::new(250).await;
         // make the request
         let res = reqwasm::http::Request::get(&format!(
             "https://api.thecatapi.com/v1/images/search?limit={count}",
@@ -103,7 +103,7 @@ pub fn fetch_example() -> impl IntoView {
                 />
             </label>
             <ErrorBoundary fallback>
-                <Suspense fallback=|| view! { <div>"Loading..."</div> }>
+                <Transition fallback=|| view! { <div>"Loading..."</div> }>
                     <ul>
                     {
                         async move {
@@ -116,7 +116,7 @@ pub fn fetch_example() -> impl IntoView {
                         .suspend()
                     }
                     </ul>
-                </Suspense>
+                </Transition>
             </ErrorBoundary>
         </div>
     }
