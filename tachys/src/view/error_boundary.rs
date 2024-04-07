f78e6755069f0a5170252ad307d5b762027064b8
Greg Johnston <greg.johnston@gmail.com>
2024-04-07 14:08:29 -0400

    probably as far as I can go with the current SuspenseBoundary approach

--- tachys/src/view/error_boundary.rs
+++ tachys/src/view/error_boundary.rs
@@ -92,7 +92,7 @@ where
         // when the state is cleared, unregister this error; this item is being dropped and its
         // error should no longer be shown
         if let Err(e) = &self.state {
-            any_error::clear(e);
+            //any_error::clear(e);
         }
     }
 }
