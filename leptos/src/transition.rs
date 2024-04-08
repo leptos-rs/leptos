018f347673922d9b00037ef18bb4a4eae1a3ec39
Greg Johnston <greg.johnston@gmail.com>
2024-04-08 08:05:56 -0400

    add Transition

--- leptos/src/transition.rs
+++ leptos/src/transition.rs
@@ -1,28 +1,34 @@
 use crate::{
-    children::{TypedChildrenFn, ViewFn},
+    children::{TypedChildren, ViewFnOnce},
+    suspense_component::{SuspenseBoundary, SuspenseContext},
     IntoView,
 };
 use leptos_macro::component;
-use tachys::async_views::SuspenseBoundary;
+use reactive_graph::{
+    computed::ArcMemo, owner::provide_context, signal::ArcRwSignal,
+    traits::With,
+};
+use slotmap::{DefaultKey, SlotMap};
 
 /// TODO docs!
 #[component]
 pub fn Transition<Chil>(
-    #[prop(optional, into)] fallback: ViewFn,
-    children: TypedChildrenFn<Chil>,
+    #[prop(optional, into)] fallback: ViewFnOnce,
+    children: TypedChildren<Chil>,
 ) -> impl IntoView
 where
     Chil: IntoView + 'static,
 {
-    let children = children.into_inner();
-    let mut fallback = Some(fallback.run());
-    // TODO check this against islands
-    move || {
-        crate::logging::log!("running suspense again");
-        SuspenseBoundary::<true, _, _>::new(
-            fallback.take(),
-            (children.clone())(),
-        )
-        // TODO track
+    let fallback = fallback.run();
+    let children = children.into_inner()();
+    let tasks = ArcRwSignal::new(SlotMap::<DefaultKey, ()>::new());
+    provide_context(SuspenseContext {
+        tasks: tasks.clone(),
+    });
+    let none_pending = ArcMemo::new(move |_| tasks.with(SlotMap::is_empty));
+    SuspenseBoundary::<true, _, _> {
+        none_pending,
+        fallback,
+        children,
     }
 }
