ac73c32ce3fd7d391545a282266db12cfd9c72f1
Greg Johnston <greg.johnston@gmail.com>
2024-04-16 19:43:30 -0400

    Add description and readme

--- oco/src/lib.rs
+++ oco/src/lib.rs
@@ -4,6 +4,31 @@
 //!
 //! Imagine this as an alternative to [`Cow`] with an additional, reference-counted
 //! branch.
+//!
+//! ```rust
+//! use oco_ref::Oco;
+//! use std::rc::Rc;
+//!
+//! let static_str = "foo";
+//! let rc_str: Rc<str> = "bar".into();
+//! let owned_str: String = "baz".into();
+//!
+//! fn uses_oco(value: impl Into<Oco<'static, str>>) {
+//!     let mut value = value.into();
+//!
+//!     // ensures that the value is either a reference, or reference-counted
+//!     // O(n) at worst
+//!     let clone1 = value.clone_inplace();
+//!
+//!     // these subsequent clones are O(1)
+//!     let clone2 = value.clone();
+//!     let clone3 = value.clone();
+//! }
+//!
+//! uses_oco(static_str);
+//! uses_oco(rc_str);
+//! uses_oco(owned_str);
+//! ```
 
 #![forbid(unsafe_code)]
 #![deny(missing_docs)]
