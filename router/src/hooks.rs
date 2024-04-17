c1ebaf9d04e8a970998254d7dd0f1c82f5c30724
kryesh <28243483+kryesh@users.noreply.github.com>
2024-04-18 09:23:33 +1000

    feat: add `create_query_signal_with_options` to `leptos_router` (#2517)

--- router/src/hooks.rs
+++ router/src/hooks.rs
@@ -50,6 +50,17 @@ pub fn create_query_signal<T>(
 where
     T: FromStr + ToString + PartialEq,
 {
+    create_query_signal_with_options::<T>(key, NavigateOptions::default())
+}
+
+#[track_caller]
+pub fn create_query_signal_with_options<T>(
+    key: impl Into<Oco<'static, str>>,
+    nav_options: NavigateOptions,
+) -> (Memo<Option<T>>, SignalSetter<Option<T>>)
+where
+    T: FromStr + ToString + PartialEq,
+{
     let mut key: Oco<'static, str> = key.into();
     let query_map = use_query_map();
     let navigate = use_navigate();
@@ -77,7 +88,7 @@ where
         let path = location.pathname.get_untracked();
         let hash = location.hash.get_untracked();
         let new_url = format!("{path}{qs}{hash}");
-        navigate(&new_url, NavigateOptions::default());
+        navigate(&new_url, nav_options.clone());
     });
 
     (get, set)
