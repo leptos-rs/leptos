86f8e5e903b0217094e319435751f3dd6675ba6d
Greg Johnston <greg.johnston@gmail.com>
2024-04-20 13:33:22 -0400

    working on reconfiguring nested routing

--- routing/src/lib.rs
+++ routing/src/lib.rs
@@ -1,17 +1,19 @@
-pub mod components;
+//pub mod components;
 mod generate_route_list;
 pub mod location;
 mod matching;
 mod method;
+mod nested_router;
 mod params;
-mod router;
+//mod router;
 mod ssr_mode;
 mod static_route;
 
 pub use generate_route_list::*;
 pub use matching::*;
 pub use method::*;
+pub use nested_router::*;
 pub use params::*;
-pub use router::*;
+//pub use router::*;
 pub use ssr_mode::*;
 pub use static_route::*;
