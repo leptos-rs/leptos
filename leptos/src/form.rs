9c2c9c4ecaf879b969583bc082f2015ba4b4cb68
Greg Johnston <greg.johnston@gmail.com>
2024-04-28 19:39:28 -0400

    module restructuring for 0.7

--- leptos/src/form.rs
+++ leptos/src/form.rs
@@ -1,10 +1,6 @@
-use crate::{
-    children::Children, component, from_form_data::FromFormData, prelude::*,
-    IntoView,
-};
-use leptos_dom::{events::submit, helpers::window};
-use leptos_server::{ArcServerMultiAction, ServerAction, ServerMultiAction};
-use reactive_graph::actions::{ArcMultiAction, MultiAction};
+use crate::{children::Children, component, prelude::*, IntoView};
+use leptos_dom::helpers::window;
+use leptos_server::{ServerAction, ServerMultiAction};
 use serde::de::DeserializeOwned;
 use server_fn::{
     client::Client, codec::PostUrl, request::ClientReq, ServerFn, ServerFnError,
@@ -14,11 +10,17 @@ use tachys::{
     html::{
         attribute::any_attribute::AnyAttribute,
         element::{form, Form},
+        event::submit,
     },
     reactive_graph::node_ref::NodeRef,
     renderer::dom::Dom,
 };
-use web_sys::{FormData, SubmitEvent};
+use thiserror::Error;
+use wasm_bindgen::{JsCast, JsValue, UnwrapThrowExt};
+use web_sys::{
+    Event, FormData, HtmlButtonElement, HtmlFormElement, HtmlInputElement,
+    SubmitEvent,
+};
 
 /// Automatically turns a server [Action](leptos_server::Action) into an HTML
 /// [`form`](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/form)
@@ -249,6 +251,88 @@ pub(crate) fn resolve_redirect_url(loc: &str) -> Option<web_sys::Url> {
                 e.as_string().unwrap_or_default(),
             );
             None
+        }
+    }
+}
+
+/// Tries to deserialize a type from form data. This can be used for client-side
+/// validation during form submission.
+pub trait FromFormData
+where
+    Self: Sized + serde::de::DeserializeOwned,
+{
+    /// Tries to deserialize the data, given only the `submit` event.
+    fn from_event(ev: &web_sys::Event) -> Result<Self, FromFormDataError>;
+
+    /// Tries to deserialize the data, given the actual form data.
+    fn from_form_data(
+        form_data: &web_sys::FormData,
+    ) -> Result<Self, serde_qs::Error>;
+}
+
+#[derive(Error, Debug)]
+pub enum FromFormDataError {
+    #[error("Could not find <form> connected to event.")]
+    MissingForm(Event),
+    #[error("Could not create FormData from <form>: {0:?}")]
+    FormData(JsValue),
+    #[error("Deserialization error: {0:?}")]
+    Deserialization(serde_qs::Error),
+}
+
+impl<T> FromFormData for T
+where
+    T: serde::de::DeserializeOwned,
+{
+    fn from_event(ev: &Event) -> Result<Self, FromFormDataError> {
+        let submit_ev = ev.unchecked_ref();
+        let form_data = form_data_from_event(submit_ev)?;
+        Self::from_form_data(&form_data)
+            .map_err(FromFormDataError::Deserialization)
+    }
+
+    fn from_form_data(
+        form_data: &web_sys::FormData,
+    ) -> Result<Self, serde_qs::Error> {
+        let data =
+            web_sys::UrlSearchParams::new_with_str_sequence_sequence(form_data)
+                .unwrap_throw();
+        let data = data.to_string().as_string().unwrap_or_default();
+        serde_qs::Config::new(5, false).deserialize_str::<Self>(&data)
+    }
+}
+
+fn form_data_from_event(
+    ev: &SubmitEvent,
+) -> Result<FormData, FromFormDataError> {
+    let submitter = ev.submitter();
+    let mut submitter_name_value = None;
+    let opt_form = match &submitter {
+        Some(el) => {
+            if let Some(form) = el.dyn_ref::<HtmlFormElement>() {
+                Some(form.clone())
+            } else if let Some(input) = el.dyn_ref::<HtmlInputElement>() {
+                submitter_name_value = Some((input.name(), input.value()));
+                Some(ev.target().unwrap().unchecked_into())
+            } else if let Some(button) = el.dyn_ref::<HtmlButtonElement>() {
+                submitter_name_value = Some((button.name(), button.value()));
+                Some(ev.target().unwrap().unchecked_into())
+            } else {
+                None
+            }
+        }
+        None => ev.target().map(|form| form.unchecked_into()),
+    };
+    match opt_form.as_ref().map(FormData::new_with_form) {
+        None => Err(FromFormDataError::MissingForm(ev.clone().into())),
+        Some(Err(e)) => Err(FromFormDataError::FormData(e)),
+        Some(Ok(form_data)) => {
+            if let Some((name, value)) = submitter_name_value {
+                form_data
+                    .append_with_str(&name, &value)
+                    .map_err(FromFormDataError::FormData)?;
+            }
+            Ok(form_data)
         }
     }
 }
