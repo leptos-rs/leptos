b497bae6825875943b7726bba7bade28af3c4ced
Greg Johnston <greg.johnston@gmail.com>
2024-05-10 17:20:15 -0400

    examples: router in 0.7

--- examples/router/src/main.rs
+++ examples/router/src/main.rs
@@ -4,13 +4,13 @@ use tracing_subscriber::fmt;
 use tracing_subscriber_wasm::MakeConsoleWriter;
 
 pub fn main() {
-    /*fmt()
-    .with_writer(
-        MakeConsoleWriter::default()
-            .map_trace_level_to(tracing::Level::DEBUG),
-    )
-    .without_time()
-    .init();*/
+    fmt()
+        .with_writer(
+            MakeConsoleWriter::default()
+                .map_trace_level_to(tracing::Level::DEBUG),
+        )
+        .without_time()
+        .init();
     console_error_panic_hook::set_once();
     mount_to_body(RouterExample);
 }
