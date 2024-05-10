8bd4f2e82915234a77dd088a13558728e24b54aa
Greg Johnston <greg.johnston@gmail.com>
2024-05-10 17:19:22 -0400

    examples: timer in 0.7

--- examples/timer/src/main.rs
+++ examples/timer/src/main.rs
@@ -1,12 +1,7 @@
-use leptos::*;
+use leptos::prelude::*;
 use timer::TimerDemo;
 
 pub fn main() {
-    _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
-    mount_to_body(|| {
-        view! {
-            <TimerDemo />
-        }
-    })
+    mount_to_body(TimerDemo)
 }
