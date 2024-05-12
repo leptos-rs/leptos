f3fdc6c9fe1277ce0220b83b7b7f8199abf8647c
Greg Johnston <greg.johnston@gmail.com>
2024-05-11 20:38:14 -0400

    start working on porting over docs and tests and 0.7...

--- reactive_graph/src/actions/action.rs
+++ reactive_graph/src/actions/action.rs
@@ -1,14 +1,91 @@
 use crate::{
+    computed::ArcMemo,
     diagnostics::is_suppressing_resource_load,
     owner::{Owner, StoredValue},
     signal::{ArcRwSignal, RwSignal},
-    traits::{DefinedAt, Dispose, GetUntracked, Update},
+    traits::{DefinedAt, Dispose, Get, GetUntracked, Update},
     unwrap_signal,
 };
 use any_spawner::Executor;
 use futures::{channel::oneshot, select, FutureExt};
 use std::{future::Future, panic::Location, pin::Pin, sync::Arc};
 
+/// An action run some asynchronous code when you dispatch a new value to it, and gives you
+/// reactive access to the result.
+///
+/// Actions are intended for mutating or updating data, not for loading data. If you find yourself
+/// creating an action and immediately dispatching a value to it, this is probably the wrong
+/// primitive.
+///
+/// The arena-allocated, `Copy` version of an `ArcAction` is an [`Action`].
+///
+/// ```rust
+/// # use reactive_graph::actions::*;
+/// # use reactive_graph::prelude::*;
+/// # tokio_test::block_on(async move {
+/// # any_spawner::Executor::init_tokio();
+/// # let _guard = reactive_graph::diagnostics::SpecialNonReactiveZone::enter();
+/// async fn send_new_todo_to_api(task: String) -> usize {
+///     // do something...
+///     // return a task id
+///     42
+/// }
+/// let save_data = ArcAction::new(|task: &String| {
+///   // `task` is given as `&String` because its value is available in `input`
+///   send_new_todo_to_api(task.clone())
+/// });
+///
+/// // the argument currently running
+/// let input = save_data.input();
+/// // the most recent returned result
+/// let result_of_call = save_data.value();
+/// // whether the call is pending
+/// let pending = save_data.pending();
+/// // how many times the action has run
+/// // useful for reactively updating something else in response to a `dispatch` and response
+/// let version = save_data.version();
+///
+/// // before we do anything
+/// assert_eq!(input.get(), None); // no argument yet
+/// assert_eq!(pending.get(), false); // isn't pending a response
+/// assert_eq!(result_of_call.get(), None); // there's no "last value"
+/// assert_eq!(version.get(), 0);
+///
+/// // dispatch the action
+/// save_data.dispatch("My todo".to_string());
+///
+/// // when we're making the call
+/// assert_eq!(input.get(), Some("My todo".to_string()));
+/// assert_eq!(pending.get(), true); // is pending
+/// assert_eq!(result_of_call.get(), None); // has not yet gotten a response
+///
+/// # tokio::time::sleep(std::time::Duration::from_millis(25)).await;
+///
+/// // after call has resolved
+/// assert_eq!(input.get(), None); // input clears out after resolved
+/// assert_eq!(pending.get(), false); // no longer pending
+/// assert_eq!(result_of_call.get(), Some(42));
+/// assert_eq!(version.get(), 1);
+/// # });
+/// ```
+///
+/// The input to the `async` function should always be a single value,
+/// but it can be of any type. The argument is always passed by reference to the
+/// function, because it is stored in [Action::input] as well.
+///
+/// ```rust
+/// # use reactive_graph::actions::*;
+/// // if there's a single argument, just use that
+/// let action1 = ArcAction::new(|input: &String| {
+///     let input = input.clone();
+///     async move { todo!() }
+/// });
+///
+/// // if there are no arguments, use the unit type `()`
+/// let action2 = ArcAction::new(|input: &()| async { todo!() });
+///
+/// // if there are multiple arguments, use a tuple
+/// let action3 = ArcAction::new(|input: &(usize, String)| async { todo!() });
 pub struct ArcAction<I, O>
 where
     I: 'static,
@@ -49,6 +126,46 @@ where
     I: Send + Sync + 'static,
     O: Send + Sync + 'static,
 {
+    /// Creates a new action. Thi is lazy: it does not run the action function until some value
+    /// is dispatched.
+    ///
+    /// The constructor takes a function which will create a new `Future` from some input data.
+    /// When the action is dispatched, this `action_fn` will run, and the `Future` it returns will
+    /// be spawned.
+    ///
+    /// The `action_fn` must be `Send + Sync` so that the `ArcAction` is `Send + Sync`. The
+    /// `Future` must be `Send` so that it can be moved across threads by the async executor as
+    /// needed.
+    ///
+    /// ```rust
+    /// # use reactive_graph::actions::*;
+    /// # use reactive_graph::prelude::*;
+    /// # tokio_test::block_on(async move {
+    /// # any_spawner::Executor::init_tokio();
+    /// # let _guard = reactive_graph::diagnostics::SpecialNonReactiveZone::enter();
+    /// let act = ArcAction::new(|n: &u8| {
+    ///     let n = n.to_owned();
+    ///     async move { n * 2 }
+    /// });
+    ///
+    /// act.dispatch(3);
+    /// assert_eq!(act.input().get(), Some(3));
+    ///
+    /// // Remember that async functions already return a future if they are
+    /// // not `await`ed. You can save keystrokes by leaving out the `async move`
+    ///
+    /// let act2 = Action::new(|n: &String| yell(n.to_owned()));
+    /// act2.dispatch(String::from("i'm in a doctest"));
+    /// # tokio::time::sleep(std::time::Duration::from_millis(10)).await;
+    ///
+    /// // after it resolves
+    /// assert_eq!(act2.value().get(), Some("I'M IN A DOCTEST".to_string()));
+    ///
+    /// async fn yell(n: String) -> String {
+    ///     n.to_uppercase()
+    /// }
+    /// # });
+    /// ```
     #[track_caller]
     pub fn new<F, Fu>(action_fn: F) -> Self
     where
@@ -68,52 +185,52 @@ where
 
     #[track_caller]
     pub fn dispatch(&self, input: I) {
-        if !is_suppressing_resource_load() {
-            let mut fut = (self.action_fn)(&input).fuse();
+        //if !is_suppressing_resource_load() {
+        let mut fut = (self.action_fn)(&input).fuse();
 
-            // abort this task if the owner is cleaned up
-            let (abort_tx, mut abort_rx) = oneshot::channel();
-            Owner::on_cleanup(move || {
-                abort_tx.send(()).expect(
-                    "tried to cancel a future in ArcAction::dispatch(), but \
-                     the channel has already closed",
-                );
-            });
+        // abort this task if the owner is cleaned up
+        let (abort_tx, mut abort_rx) = oneshot::channel();
+        Owner::on_cleanup(move || {
+            abort_tx.send(()).expect(
+                "tried to cancel a future in ArcAction::dispatch(), but the \
+                 channel has already closed",
+            );
+        });
 
-            // Update the state before loading
-            self.in_flight.update(|n| *n += 1);
-            let current_version =
-                self.version.try_get_untracked().unwrap_or_default();
-            self.input.try_update(|inp| *inp = Some(input));
+        // Update the state before loading
+        self.in_flight.update(|n| *n += 1);
+        let current_version =
+            self.version.try_get_untracked().unwrap_or_default();
+        self.input.try_update(|inp| *inp = Some(input));
 
-            // Spawn the task
-            Executor::spawn({
-                let input = self.input.clone();
-                let version = self.version.clone();
-                let value = self.value.clone();
-                let in_flight = self.in_flight.clone();
-                async move {
-                    select! {
-                        // if the abort message has been sent, bail and do nothing
-                        _ = abort_rx => {
-                            in_flight.update(|n| *n = n.saturating_sub(1));
-                        },
-                        // otherwise, update the value
-                        result = fut => {
-                            in_flight.update(|n| *n = n.saturating_sub(1));
-                            let is_latest = version.get_untracked() <= current_version;
-                            if is_latest {
-                                version.update(|n| *n += 1);
-                                value.update(|n| *n = Some(result));
-                            }
-                            if in_flight.get_untracked() == 0 {
-                                input.update(|inp| *inp = None);
-                            }
+        // Spawn the task
+        Executor::spawn({
+            let input = self.input.clone();
+            let version = self.version.clone();
+            let value = self.value.clone();
+            let in_flight = self.in_flight.clone();
+            async move {
+                select! {
+                    // if the abort message has been sent, bail and do nothing
+                    _ = abort_rx => {
+                        in_flight.update(|n| *n = n.saturating_sub(1));
+                    },
+                    // otherwise, update the value
+                    result = fut => {
+                        in_flight.update(|n| *n = n.saturating_sub(1));
+                        let is_latest = version.get_untracked() <= current_version;
+                        if is_latest {
+                            version.update(|n| *n += 1);
+                            value.update(|n| *n = Some(result));
+                        }
+                        if in_flight.get_untracked() == 0 {
+                            input.update(|inp| *inp = None);
                         }
                     }
                 }
-            });
-        }
+            }
+        });
+        //}
     }
 }
 
@@ -131,6 +248,12 @@ impl<I, O> ArcAction<I, O> {
     #[track_caller]
     pub fn value(&self) -> ArcRwSignal<Option<O>> {
         self.value.clone()
+    }
+
+    #[track_caller]
+    pub fn pending(&self) -> ArcMemo<bool> {
+        let in_flight = self.in_flight.clone();
+        ArcMemo::new(move |_| in_flight.get() > 0)
     }
 }
 
