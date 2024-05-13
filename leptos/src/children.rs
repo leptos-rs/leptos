06b6209085740cbb4b6f609a50751eb7506a4a4a
Greg Johnston <greg.johnston@gmail.com>
2024-05-13 19:52:40 -0400

    fix: correct Send + Sync bounds for children

--- leptos/src/children.rs
+++ leptos/src/children.rs
@@ -13,18 +13,18 @@ use tachys::{
 
 /// The most common type for the `children` property on components,
 /// which can only be called once.
-pub type Children = Box<dyn FnOnce() -> AnyView<Dom>>;
+pub type Children = Box<dyn FnOnce() -> AnyView<Dom> + Send>;
 
 /// A type for the `children` property on components that can be called
 /// more than once.
-pub type ChildrenFn = Arc<dyn Fn() -> AnyView<Dom>>;
+pub type ChildrenFn = Arc<dyn Fn() -> AnyView<Dom> + Send + Sync>;
 
 /// A type for the `children` property on components that can be called
 /// more than once, but may mutate the children.
-pub type ChildrenFnMut = Box<dyn FnMut() -> AnyView<Dom>>;
+pub type ChildrenFnMut = Box<dyn FnMut() -> AnyView<Dom> + Send>;
 
 // This is to still support components that accept `Box<dyn Fn() -> AnyView>` as a children.
-type BoxedChildrenFn = Box<dyn Fn() -> AnyView<Dom>>;
+type BoxedChildrenFn = Box<dyn Fn() -> AnyView<Dom> + Send>;
 
 #[doc(hidden)]
 pub trait ToChildren<F> {
@@ -44,7 +44,7 @@ where
 
 impl<F, C> ToChildren<F> for ChildrenFn
 where
-    F: Fn() -> C + Send + 'static,
+    F: Fn() -> C + Send + Sync + 'static,
     C: RenderHtml<Dom> + Send + 'static,
 {
     #[inline]
@@ -66,7 +66,7 @@ where
 
 impl<F, C> ToChildren<F> for BoxedChildrenFn
 where
-    F: Fn() -> C + 'static,
+    F: Fn() -> C + Send + 'static,
     C: RenderHtml<Dom> + Send + 'static,
 {
     #[inline]
