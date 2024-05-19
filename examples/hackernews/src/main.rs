056456538e8a34c8eddeae29b6a2a5b5c49ab11a
Greg Johnston <greg.johnston@gmail.com>
2024-05-19 19:58:03 -0400

    upgrading hackernews example

--- examples/hackernews/src/main.rs
+++ examples/hackernews/src/main.rs
@@ -50,7 +50,20 @@ async fn main() -> std::io::Result<()> {
 fn main() {
     use hackernews::App;
 
-    _ = console_log::init_with_level(log::Level::Debug);
+    use tracing_subscriber::fmt;
+    use tracing_subscriber_wasm::MakeConsoleWriter;
+
+    fmt()
+        .with_writer(
+            // To avoide trace events in the browser from showing their
+            // JS backtrace, which is very annoying, in my opinion
+            MakeConsoleWriter::default()
+                .map_trace_level_to(tracing::Level::DEBUG),
+        )
+        // For some reason, if we don't do this in the browser, we get
+        // a runtime error.
+        .without_time()
+        .init();
     console_error_panic_hook::set_once();
-    leptos::mount_to_body(App)
+    leptos::mount::mount_to_body(App)
 }
