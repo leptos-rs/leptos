b32bcdb0c860576c0248108772ccb2ec3ef7de32
Greg Johnston <greg.johnston@gmail.com>
2024-05-20 13:37:52 -0400

    add async transitions that wait for any AsyncDerived created/triggered under them before resolving

---
+++ reactive_graph/src/transition.rs
@@ -0,0 +1,59 @@
+use futures::{channel::oneshot, future::join_all};
+use or_poisoned::OrPoisoned;
+use std::{
+    future::Future,
+    sync::{mpsc, OnceLock, RwLock},
+};
+
+static TRANSITION: OnceLock<RwLock<Option<TransitionInner>>> = OnceLock::new();
+
+fn global_transition() -> &'static RwLock<Option<TransitionInner>> {
+    TRANSITION.get_or_init(|| RwLock::new(None))
+}
+
+#[derive(Debug, Clone)]
+struct TransitionInner {
+    tx: mpsc::Sender<oneshot::Receiver<()>>,
+}
+
+#[derive(Debug)]
+pub struct AsyncTransition;
+
+impl AsyncTransition {
+    pub async fn run<T, U>(action: impl FnOnce() -> T) -> U
+    where
+        T: Future<Output = U>,
+    {
+        let (tx, rx) = mpsc::channel();
+        let global_transition = global_transition();
+        let inner = TransitionInner { tx };
+        let prev = std::mem::replace(
+            &mut *global_transition.write().or_poisoned(),
+            Some(inner.clone()),
+        );
+        let value = action().await;
+        _ = std::mem::replace(
+            &mut *global_transition.write().or_poisoned(),
+            prev,
+        );
+        let mut pending = Vec::new();
+        while let Ok(tx) = rx.try_recv() {
+            pending.push(tx);
+        }
+        join_all(pending).await;
+        value
+    }
+
+    pub(crate) fn register(rx: oneshot::Receiver<()>) {
+        if let Some(tx) = global_transition()
+            .read()
+            .or_poisoned()
+            .as_ref()
+            .map(|n| &n.tx)
+        {
+            tx.send(rx);
+        } else {
+            crate::log_warning(format_args!("no global transition"));
+        }
+    }
+}
