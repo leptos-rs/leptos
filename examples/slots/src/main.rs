b88cc84510a09ef433a5020ae870abe763c6ac32
Greg Johnston <greg.johnston@gmail.com>
2024-05-27 15:50:53 -0400

    feat: 0.7 slots

--- examples/slots/src/main.rs
+++ examples/slots/src/main.rs
@@ -1,12 +1,7 @@
-use leptos::*;
-use slots::*;
+use slots::App;
 
 pub fn main() {
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
-    mount_to_body(|| {
-        view! {
-            <App/>
-        }
-    })
+    leptos::mount::mount_to_body(App);
 }
