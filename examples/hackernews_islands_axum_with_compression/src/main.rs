ec9f0cc43268f9c34fdc01c7bb39a806b583fe59
David Karrick <the.white.wolf.is.1337@gmail.com>
2024-05-31 10:10:28 -0600

    Dynamically compress html

--- examples/hackernews_islands_axum_with_compression/src/main.rs
+++ examples/hackernews_islands_axum_with_compression/src/main.rs
@@ -6,16 +6,32 @@ async fn main() {
     use hackernews_islands::*;
     pub use leptos::get_configuration;
     pub use leptos_axum::{generate_route_list, LeptosRoutes};
+    use tower_http::compression::{
+        predicate::{NotForContentType, SizeAbove},
+        CompressionLayer, CompressionLevel, Predicate,
+    };
 
     let conf = get_configuration(Some("Cargo.toml")).await.unwrap();
     let leptos_options = conf.leptos_options;
     let addr = leptos_options.site_addr;
     let routes = generate_route_list(App);
 
+    let predicate = SizeAbove::new(8192)
+        .and(NotForContentType::GRPC)
+        .and(NotForContentType::IMAGES)
+        .and(NotForContentType::const_new("application/javascript"))
+        .and(NotForContentType::const_new("application/wasm"))
+        .and(NotForContentType::const_new("text/css"));
+
     // build our application with a route
     let app = Router::new()
         .route("/favicon.ico", get(file_and_error_handler))
         .leptos_routes(&leptos_options, routes, App)
+        .layer(
+            CompressionLayer::new()
+                .quality(CompressionLevel::Fastest)
+                .compress_when(predicate),
+        )
         .fallback(file_and_error_handler)
         .with_state(leptos_options);
 
