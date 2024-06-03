4636192fa3461d8c48b8de499554490f88e49953
Greg Johnston <greg.johnston@gmail.com>
2024-06-03 07:32:14 -0400

    feat: allow `.write()` on all writeable signals

--- examples/counter/src/lib.rs
+++ examples/counter/src/lib.rs
@@ -15,7 +15,7 @@ pub fn SimpleCounter(
     view! {
         <div>
             <button on:click=move |_| set_value.set(0)>"Clear"</button>
-            <button on:click=move |_| set_value.update(|value| *value -= step)>"-1"</button>
+            <button on:click=move |_| *set_value.write() -= step>"-1"</button>
             <span>"Value: " {value} "!"</span>
             <button on:click=move |_| set_value.update(|value| *value += step)>"+1"</button>
         </div>
