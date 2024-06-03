79191557722b7d534f7c0fabec7a9562272ca33e
David Karrick <the.white.wolf.is.1337@gmail.com>
2024-06-03 10:39:32 -0600

    Serve precompressed assets
    Code was taken from https://github.com/leptos-rs/cargo-leptos/pull/165#issuecomment-1647843037
    
    Co-authored-by: Sebastian Dobe <sebastiandobe@mailbox.org>

--- examples/hackernews_islands_axum/src/fallback.rs
+++ examples/hackernews_islands_axum/src/fallback.rs
@@ -2,20 +2,34 @@ use crate::error_template::error_template;
 use axum::{
     body::Body,
     extract::State,
-    http::{Request, Response, StatusCode, Uri},
+    http::{header, Request, Response, StatusCode, Uri},
     response::{IntoResponse, Response as AxumResponse},
 };
 use leptos::LeptosOptions;
-use tower::ServiceExt;
-use tower_http::services::ServeDir;
+use std::borrow::Cow;
+
+#[cfg(not(debug_assertions))]
+const DEV_MODE: bool = false;
+
+#[cfg(debug_assertions)]
+const DEV_MODE: bool = true;
+
+#[derive(rust_embed::RustEmbed)]
+#[folder = "target/site/"]
+struct Assets;
 
 pub async fn file_and_error_handler(
     uri: Uri,
     State(options): State<LeptosOptions>,
     req: Request<Body>,
 ) -> AxumResponse {
-    let root = options.site_root.clone();
-    let res = get_static_file(uri.clone(), &root).await.unwrap();
+    let accept_encoding = req
+        .headers()
+        .get("accept-encoding")
+        .map(|h| h.to_str().unwrap_or("none"))
+        .unwrap_or("none")
+        .to_string();
+    let res = get_static_file(uri.clone(), accept_encoding).await.unwrap();
 
     if res.status() == StatusCode::OK {
         res.into_response()
@@ -30,19 +44,56 @@ pub async fn file_and_error_handler(
 
 async fn get_static_file(
     uri: Uri,
-    root: &str,
+    accept_encoding: String,
 ) -> Result<Response<Body>, (StatusCode, String)> {
-    let req = Request::builder()
-        .uri(uri.clone())
-        .body(Body::empty())
-        .unwrap();
-    // `ServeDir` implements `tower::Service` so we can call it with `tower::ServiceExt::oneshot`
-    // This path is relative to the cargo root
-    match ServeDir::new(root).oneshot(req).await {
-        Ok(res) => Ok(res.into_response()),
-        Err(err) => Err((
-            StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Something went wrong: {}", err),
-        )),
+    let (_, path) = uri.path().split_at(1); // split off the first `/`
+    let mime = mime_guess::from_path(path);
+
+    let (path, encoding) = if DEV_MODE {
+        // during DEV, don't care about the precompression -> faster workflow
+        (Cow::from(path), "none")
+    } else if accept_encoding.contains("br") {
+        (Cow::from(format!("{}.br", path)), "br")
+    } else if accept_encoding.contains("gzip") {
+        (Cow::from(format!("{}.gz", path)), "gzip")
+    } else {
+        (Cow::from(path), "none")
+    };
+
+    match Assets::get(path.as_ref()) {
+        Some(content) => {
+            let body = Body::from(content.data);
+
+            let res = match DEV_MODE {
+                true => Response::builder()
+                    .header(
+                        header::CONTENT_TYPE,
+                        mime.first_or_octet_stream().as_ref(),
+                    )
+                    .header(header::CONTENT_ENCODING, encoding)
+                    .body(body)
+                    .unwrap(),
+                false => Response::builder()
+                    .header(header::CACHE_CONTROL, "max-age=86400")
+                    .header(
+                        header::CONTENT_TYPE,
+                        mime.first_or_octet_stream().as_ref(),
+                    )
+                    .header(header::CONTENT_ENCODING, encoding)
+                    .body(body)
+                    .unwrap(),
+            };
+
+            Ok(res.into_response())
+        }
+
+        None => {
+            eprintln!(">> Asset {} not found", path);
+            for a in Assets::iter() {
+                eprintln!("Available asset: {}", a);
+            }
+
+            Err((StatusCode::NOT_FOUND, format!("Not found")))
+        }
     }
 }
