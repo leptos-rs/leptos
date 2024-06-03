87ae6f25eed540752e692922986422e2f3090836
Greg Johnston <greg.johnston@gmail.com>
2024-06-03 07:32:14 -0400

    feat: allow `.write()` on all writeable signals

--- reactive_graph/src/signal/arc_rw.rs
+++ reactive_graph/src/signal/arc_rw.rs
@@ -11,6 +11,7 @@ use crate::{
 use core::fmt::{Debug, Formatter, Result};
 use std::{
     hash::Hash,
+    ops::DerefMut,
     panic::Location,
     sync::{Arc, RwLock},
 };
@@ -152,19 +153,19 @@ impl<T> Trigger for ArcRwSignal<T> {
     }
 }
 
-impl<T> Writeable for ArcRwSignal<T> {
+impl<T: 'static> Writeable for ArcRwSignal<T> {
     type Value = T;
 
-    fn try_write(&self) -> Option<WriteGuard<'_, Self, Self::Value>> {
+    fn try_write(
+        &self,
+    ) -> Option<WriteGuard<'_, Self, impl DerefMut<Target = Self::Value>>> {
         self.value
             .write()
             .ok()
             .map(|guard| WriteGuard::new(self, guard))
     }
 
-    fn try_write_untracked(
-        &self,
-    ) -> Option<UntrackedWriteGuard<'_, Self::Value>> {
-        self.value.write().ok().map(UntrackedWriteGuard::from)
+    fn try_write_untracked(&self) -> Option<UntrackedWriteGuard<Self::Value>> {
+        UntrackedWriteGuard::try_new(Arc::clone(&self.value))
     }
 }
