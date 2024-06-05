caf797dba0604d716bf53de5c7b229ca0ad65522
Greg Johnston <greg.johnston@gmail.com>
2024-06-05 08:15:29 -0400

    refactor integrations and add Actix integration

--- examples/ssr_modes_axum/src/fallback.rs
+++ examples/ssr_modes_axum/src/fallback.rs
@@ -20,11 +20,7 @@ pub async fn file_and_error_handler(
     if res.status() == StatusCode::OK {
         res.into_response()
     } else {
-        let handler = leptos_axum::render_app_to_stream(
-            options.to_owned(),
-            //move || view! { <App/> },
-            move || "404",
-        );
+        let handler = leptos_axum::render_app_to_stream(move || "404");
         handler(req).await.into_response()
     }
 }
