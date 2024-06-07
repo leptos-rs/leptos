cddad64ed26dc450f508c9e895d0df080c89c10a
Greg Johnston <greg.johnston@gmail.com>
2024-06-07 16:48:53 -0400

    Actix todo_app_sqlite

--- examples/todo_app_sqlite/src/main.rs
+++ examples/todo_app_sqlite/src/main.rs
@@ -5,8 +5,9 @@ mod ssr {
     pub use crate::todo::*;
     pub use actix_files::Files;
     pub use actix_web::*;
-    pub use leptos::*;
+    pub use leptos::prelude::*;
     pub use leptos_actix::{generate_route_list, LeptosRoutes};
+    pub use leptos_meta::MetaTags;
 
     #[get("/style.css")]
     pub async fn css() -> impl Responder {
@@ -25,37 +26,45 @@ async fn main() -> std::io::Result<()> {
         .await
         .expect("could not run SQLx migrations");
 
-    // Explicit server function registration is no longer required
-    // on the main branch. On 0.3.0 and earlier, uncomment the lines
-    // below to register the server functions.
-    // _ = GetTodos::register();
-    // _ = AddTodo::register();
-    // _ = DeleteTodo::register();
-
-    // Setting this to None means we'll be using cargo-leptos and its env vars.
+    // Generate the list of routes in your Leptos App
     let conf = get_configuration(None).await.unwrap();
-
     let addr = conf.leptos_options.site_addr;
 
-    // Generate the list of routes in your Leptos App
-    let routes = generate_route_list(TodoApp);
-
     HttpServer::new(move || {
+        // Generate the list of routes in your Leptos App
+        let routes = generate_route_list(TodoApp);
         let leptos_options = &conf.leptos_options;
         let site_root = &leptos_options.site_root;
-        let routes = &routes;
 
         App::new()
-            .service(css)
-            .leptos_routes(
-                leptos_options.to_owned(),
-                routes.to_owned(),
-                TodoApp,
-            )
+            .leptos_routes(routes, {
+                let leptos_options = leptos_options.clone();
+                move || {
+                    use leptos::prelude::*;
+
+                    view! {
+                        <!DOCTYPE html> 
+                        <html lang="en">
+                            <head>
+                                <meta charset="utf-8"/>
+                                <meta
+                                    name="viewport"
+                                    content="width=device-width, initial-scale=1"
+                                />
+                                <AutoReload options=leptos_options.clone()/>
+                                <HydrationScripts options=leptos_options.clone()/>
+                                <MetaTags/>
+                            </head>
+                            <body>
+                                <TodoApp/>
+                            </body>
+                        </html>
+                    }
+            }})
             .service(Files::new("/", site_root))
         //.wrap(middleware::Compress::default())
     })
-    .bind(addr)?
+    .bind(&addr)?
     .run()
     .await
 }
