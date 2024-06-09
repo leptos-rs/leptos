d343b373c726470011c7d08ec19f054a5f22cdc7
Greg Johnston <greg.johnston@gmail.com>
2024-06-08 20:18:54 -0400

    update counters_isomorphic

--- examples/counter_isomorphic/src/lib.rs
+++ examples/counter_isomorphic/src/lib.rs
@@ -3,11 +3,10 @@ pub mod counters;
 #[cfg(feature = "hydrate")]
 #[wasm_bindgen::prelude::wasm_bindgen]
 pub fn hydrate() {
-    use crate::counters::*;
-    use leptos::*;
+    use crate::counters::Counters;
 
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
 
-    mount_to_body(Counters);
+    leptos::mount::hydrate_body(Counters);
 }
