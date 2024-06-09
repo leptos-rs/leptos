947f3fcb2da34775fb8404aa6206270b31aa6bc9
Greg Johnston <greg.johnston@gmail.com>
2024-06-08 20:18:54 -0400

    update counters_isomorphic

--- examples/counter_isomorphic/src/main.rs
+++ examples/counter_isomorphic/src/main.rs
@@ -3,7 +3,6 @@ mod counters;
 use crate::counters::*;
 use actix_files::Files;
 use actix_web::*;
-use leptos::*;
 use leptos_actix::{generate_route_list, LeptosRoutes};
 
 #[get("/api/events")]
@@ -27,26 +26,44 @@ async fn counter_events() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    use leptos::prelude::*;
+
     // Setting this to None means we'll be using cargo-leptos and its env vars.
     // when not using cargo-leptos None must be replaced with Some("Cargo.toml")
     let conf = get_configuration(None).await.unwrap();
-
     let addr = conf.leptos_options.site_addr;
-    let routes = generate_route_list(Counters);
+    println!("listening on http://{}", &addr);
 
     HttpServer::new(move || {
+        // Generate the list of routes in your Leptos App
+        let routes = generate_route_list(Counters);
         let leptos_options = &conf.leptos_options;
         let site_root = &leptos_options.site_root;
 
         App::new()
             .service(counter_events)
-            .leptos_routes(
-                leptos_options.to_owned(),
-                routes.to_owned(),
-                Counters,
-            )
+            .leptos_routes(routes, {
+                let leptos_options = leptos_options.clone();
+                move || {
+                    view! {
+                        <!DOCTYPE html>
+                        <html lang="en">
+                            <head>
+                                <meta charset="utf-8"/>
+                                <meta
+                                    name="viewport"
+                                    content="width=device-width, initial-scale=1"
+                                />
+                                <AutoReload options=leptos_options.clone()/>
+                                <HydrationScripts options=leptos_options.clone()/>
+                            </head>
+                            <body>
+                                <Counters/>
+                            </body>
+                        </html>
+                    }
+            }})
             .service(Files::new("/", site_root))
-        //.wrap(middleware::Compress::default())
     })
     .bind(&addr)?
     .run()
