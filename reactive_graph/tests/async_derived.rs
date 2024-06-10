136f96b090b17782cb93bd1f1bf8258ab1db9c45
Greg Johnston <greg.johnston@gmail.com>
2024-06-09 21:15:49 -0400

    testing: provide tick() that can be called anywhere in tests

--- reactive_graph/tests/async_derived.rs
+++ reactive_graph/tests/async_derived.rs
@@ -1,6 +1,6 @@
 use any_spawner::Executor;
 use reactive_graph::{
-    computed::{ArcAsyncDerived, AsyncDerived, AsyncState},
+    computed::{ArcAsyncDerived, AsyncDerived},
     signal::RwSignal,
     traits::{Get, Read, Set, With, WithUntracked},
 };
@@ -8,13 +8,10 @@ use std::future::pending;
 
 #[tokio::test]
 async fn arc_async_derived_calculates_eagerly() {
-    use std::time::Duration;
-    use tokio::time::sleep;
-
     _ = Executor::init_tokio();
 
     let value = ArcAsyncDerived::new(|| async {
-        sleep(Duration::from_millis(25)).await;
+        Executor::tick().await;
         42
     });
 
@@ -23,35 +20,29 @@ async fn arc_async_derived_calculates_eagerly() {
 
 #[tokio::test]
 async fn arc_async_derived_tracks_signal_change() {
-    use std::time::Duration;
-    use tokio::time::sleep;
-
     _ = Executor::init_tokio();
 
     let signal = RwSignal::new(10);
     let value = ArcAsyncDerived::new(move || async move {
-        sleep(Duration::from_millis(25)).await;
+        Executor::tick().await;
         signal.get()
     });
 
     assert_eq!(value.clone().await, 10);
     signal.set(30);
-    sleep(Duration::from_millis(5)).await;
+    Executor::tick().await;
     assert_eq!(value.clone().await, 30);
     signal.set(50);
-    sleep(Duration::from_millis(5)).await;
+    Executor::tick().await;
     assert_eq!(value.clone().await, 50);
 }
 
 #[tokio::test]
 async fn async_derived_calculates_eagerly() {
-    use std::time::Duration;
-    use tokio::time::sleep;
-
     _ = Executor::init_tokio();
 
     let value = AsyncDerived::new(|| async {
-        sleep(Duration::from_millis(25)).await;
+        Executor::tick().await;
         42
     });
 
@@ -60,23 +51,20 @@ async fn async_derived_calculates_eagerly() {
 
 #[tokio::test]
 async fn async_derived_tracks_signal_change() {
-    use std::time::Duration;
-    use tokio::time::sleep;
-
     _ = Executor::init_tokio();
 
     let signal = RwSignal::new(10);
     let value = AsyncDerived::new(move || async move {
-        sleep(Duration::from_millis(25)).await;
+        Executor::tick().await;
         signal.get()
     });
 
     assert_eq!(value.await, 10);
     signal.set(30);
-    sleep(Duration::from_millis(5)).await;
+    Executor::tick().await;
     assert_eq!(value.await, 30);
     signal.set(50);
-    sleep(Duration::from_millis(5)).await;
+    Executor::tick().await;
     assert_eq!(value.await, 50);
 }
 
