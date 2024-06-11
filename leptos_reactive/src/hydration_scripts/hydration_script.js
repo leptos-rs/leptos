64a4e59266b0a774af6961a0d6cd67766e3fa608
Greg Johnston <greg.johnston@gmail.com>
2024-06-11 14:43:47 -0400

    remove leptos_reactive (moved into reactive_graph and leptos_server)

--- leptos_reactive/src/hydration_scripts/hydration_script.js
+++
@@ -1,8 +0,0 @@
-(function (pkg_path, output_name, wasm_output_name) {
-	import(`/${pkg_path}/${output_name}.js`)
-		.then(mod => {
-			mod.default(`/${pkg_path}/${wasm_output_name}.wasm`).then(() => {
-				mod.hydrate();
-			});
-		})
-})
