0ba53afa08e59ac24793ff210755f29f01797560
Greg Johnston <greg.johnston@gmail.com>
2024-06-12 07:52:07 -0400

    use `csr` feature so that reactivity runs

--- examples/counter_without_macros/src/lib.rs
+++ examples/counter_without_macros/src/lib.rs
@@ -8,6 +8,9 @@ use leptos::{
 // A component is really just a function call: it runs once to create the DOM and reactive system
 pub fn counter(initial_value: i32, step: u32) -> impl IntoView {
     let count = RwSignal::new(Count::new(initial_value, step));
+    Effect::new(move |_| {
+        leptos::logging::log!("count = {:?}", count.get());
+    });
 
     // the function name is the same as the HTML tag name
     div()
@@ -48,6 +51,7 @@ impl Count {
     }
 
     pub fn value(&self) -> i32 {
+        leptos::logging::log!("value = {}", self.value);
         self.value
     }
 
