e184438b2cf687dbd8749407af701dbb43258d78
Greg Johnston <greg.johnston@gmail.com>
2024-06-14 14:16:59 -0400

    initial work updating suspense tests

--- examples/suspense_tests/src/lib.rs
+++ examples/suspense_tests/src/lib.rs
@@ -1,12 +1,11 @@
 pub mod app;
 
+#[cfg(feature = "hydrate")]
 #[wasm_bindgen::prelude::wasm_bindgen]
 pub fn hydrate() {
     use app::*;
 
-    // initializes logging using the `log` crate
-    _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
 
-    leptos::mount_to_body(App);
+    leptos::mount::hydrate_body(App);
 }
