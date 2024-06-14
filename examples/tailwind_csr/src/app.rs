01c7c039c7436ac02aab2f5f84ccf6881483b374
Sam Mun <therealsam@berkeley.edu>
2024-06-13 18:24:01 -0700

    removed leptos_meta to simplify more

--- examples/tailwind_csr/src/app.rs
+++ examples/tailwind_csr/src/app.rs
@@ -1,13 +1,9 @@
 use leptos::*;
-use leptos_meta::*;
 use leptos_router::*;
 
 #[component]
 pub fn App() -> impl IntoView {
-    provide_meta_context();
-
     view! {
-        <Link rel="shortcut icon" type_="image/ico" href="/favicon.ico"/>
         <Router>
             <Routes>
                 <Route path="" view=  move || view! { <Home/> }/>
