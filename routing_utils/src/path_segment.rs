e322fb1b339e7d0752a1f369b96a612d4f46e6f1
Greg Johnston <greg.johnston@gmail.com>
2024-06-14 16:19:41 -0400

    remove old router files

--- routing_utils/src/path_segment.rs
+++
@@ -1,9 +0,0 @@
-use alloc::borrow::Cow;
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum PathSegment {
-    Unit,
-    Static(Cow<'static, str>),
-    Param(Cow<'static, str>),
-    Splat(Cow<'static, str>),
-}
