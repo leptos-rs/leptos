f6046fa8a755908979e21f3e6600573b929e8278
Greg Johnston <greg.johnston@gmail.com>
2024-06-22 10:13:52 -0400

    update islands example

--- examples/islands/src/app.rs
+++ examples/islands/src/app.rs
@@ -1,5 +1,24 @@
 use leptos::prelude::*;
 
+pub fn shell(options: &LeptosOptions) -> impl IntoView {
+    view! {
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <AutoReload options=options.clone() />
+                <HydrationScripts options=options.clone() islands=true/>
+                <link rel="stylesheet" id="leptos" href="/pkg/todo_app_sqlite_axum.css"/>
+                <link rel="shortcut icon" type="image/ico" href="/favicon.ico"/>
+            </head>
+            <body>
+                <App/>
+            </body>
+        </html>
+    }
+}
+
 #[component]
 pub fn App() -> impl IntoView {
     view! {
