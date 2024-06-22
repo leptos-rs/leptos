6097e3cc57663991e533fed67c63ac3f9382971d
Greg Johnston <greg.johnston@gmail.com>
2024-06-22 13:05:02 -0400

    fix js-framework-benchmark for stable

--- examples/js-framework-benchmark/src/lib.rs
+++ examples/js-framework-benchmark/src/lib.rs
@@ -171,7 +171,7 @@ pub fn App() -> impl IntoView {
                             ViewTemplate::new(view! {
                                 <tr class:danger={move || is_selected.selected(Some(row_id))}>
                                     <td class="col-md-1">{row_id.to_string()}</td>
-                                    <td class="col-md-4"><a on:click=move |_| set_selected.set(Some(row_id))>{label}</a></td>
+                                    <td class="col-md-4"><a on:click=move |_| set_selected.set(Some(row_id))>{move || label.get()}</a></td>
                                     <td class="col-md-1"><a on:click=move |_| remove(row_id)><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a></td>
                                     <td class="col-md-6"/>
                                 </tr>
