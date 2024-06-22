453eb7e318162ef64b35cf42dfe37e5306669def
Greg Johnston <greg.johnston@gmail.com>
2024-06-22 08:20:52 -0400

    examples: errors_axum

--- examples/errors_axum/src/error_template.rs
+++ examples/errors_axum/src/error_template.rs
@@ -7,25 +7,18 @@ use leptos_axum::ResponseOptions;
 // Feel free to do more complicated things here than just displaying them.
 #[component]
 pub fn ErrorTemplate(
-    #[prop(optional)] outside_errors: Option<Errors>,
-    #[prop(optional)] errors: Option<RwSignal<Errors>>,
+    #[prop(into)] errors: MaybeSignal<Errors>,
 ) -> impl IntoView {
-    let errors = match outside_errors {
-        Some(e) => RwSignal::new(e),
-        None => match errors {
-            Some(e) => e,
-            None => panic!("No Errors found and we expected errors!"),
-        },
-    };
-
     // Get Errors from Signal
     // Downcast lets us take a type that implements `std::error::Error`
-    let errors: Vec<AppError> = errors
-        .get_untracked()
-        .into_iter()
-        .filter_map(|(_, v)| v.downcast_ref::<AppError>().cloned())
-        .collect();
-    log!("Errors: {errors:#?}");
+    let errors = Memo::new(move |_| {
+        errors
+            .get_untracked()
+            .into_iter()
+            .filter_map(|(_, v)| v.downcast_ref::<AppError>().cloned())
+            .collect::<Vec<_>>()
+    });
+    log!("Errors: {:#?}", &*errors.read_untracked());
 
     // Only the response code for the first error is actually sent from the server
     // this may be customized by the specific application
@@ -33,26 +26,30 @@ pub fn ErrorTemplate(
     {
         let response = use_context::<ResponseOptions>();
         if let Some(response) = response {
-            response.set_status(errors[0].status_code());
+            response.set_status(errors.read_untracked()[0].status_code());
         }
     }
 
     view! {
-        <h1>{if errors.len() > 1 {"Errors"} else {"Error"}}</h1>
-        <For
-            // a function that returns the items we're iterating over; a signal is fine
-            each= move || {errors.clone().into_iter().enumerate()}
-            // a unique key for each item as a reference
-            key=|(index, _)| *index
-            // renders each item to a view
-            children=move |error| {
-                let error_string = error.1.to_string();
-                let error_code= error.1.status_code();
-                view! {
-                    <h2>{error_code.to_string()}</h2>
-                    <p>"Error: " {error_string}</p>
-                }
-            }
-        />
+        <h1>{move || {
+            if errors.read().len() > 1 {
+                "Errors"
+            } else {
+                "Error"
+            }}}
+        </h1>
+        {move || {
+            errors.get()
+                .into_iter()
+                .map(|error| {
+                    let error_string = error.to_string();
+                    let error_code= error.status_code();
+                    view! {
+                        <h2>{error_code.to_string()}</h2>
+                        <p>"Error: " {error_string}</p>
+                    }
+                })
+                .collect_view()
+        }}
     }
 }
