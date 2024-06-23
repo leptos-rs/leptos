5b7f5e3db33b5f07bc25c3b1d2f5b0f7f485f707
Greg Johnston <greg.johnston@gmail.com>
2024-06-23 17:55:16 -0400

    hackernews islands example

--- examples/hackernews_islands_axum/src/routes/nav.rs
+++ examples/hackernews_islands_axum/src/routes/nav.rs
@@ -1,5 +1,5 @@
-use leptos::{component, view, IntoView};
-use leptos_router::*;
+use leptos::prelude::*;
+use leptos_router::components::A;
 
 #[component]
 pub fn Nav() -> impl IntoView {
@@ -21,7 +21,7 @@ pub fn Nav() -> impl IntoView {
                 <A href="/job">
                     <strong>"Jobs"</strong>
                 </A>
-                <a class="github" href="http://github.com/gbj/leptos" target="_blank" rel="noreferrer">
+                <a class="github" href="http://github.com/leptos-rs/leptos" target="_blank" rel="noreferrer">
                     "Built with Leptos"
                 </a>
             </nav>
