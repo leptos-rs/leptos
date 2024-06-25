7c708de86a3d6993bb6b9947eb84a36cfd772add
David Karrick <the.white.wolf.is.1337@gmail.com>
2024-06-25 11:17:28 -0600

    Chore: Remove old build file

--- examples/hackernews_islands_axum/build-front.sh
+++
@@ -1,8 +0,0 @@
-wasm-pack build --target=web --features=hydrate --release
-cd pkg
-rm *.br
-cp hackernews.js hackernews.unmin.js
-cat hackernews.unmin.js | esbuild > hackernews.js
-brotli hackernews.js
-brotli hackernews_bg.wasm
-brotli style.css
