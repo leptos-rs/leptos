146cbb8fd006f942062e7a3e4872da91c9066d94
Greg Johnston <greg.johnston@gmail.com>
2024-07-19 08:43:43 -0400

    fix: add HTML global `on___` attributes

--- tachys/src/html/attribute/global.rs
+++ tachys/src/html/attribute/global.rs
@@ -387,3 +387,177 @@ where
     Rndr: Renderer,
 {
 }
+
+macro_rules! on_definitions {
+	($(#[$meta:meta] $key:ident $html:literal),* $(,)?) => {
+        paste::paste! {
+            $(
+                #[doc = concat!("Adds the HTML `", $html, "` attribute to the element.\n\n**Note**: This is the HTML attribute, which takes a JavaScript string, not an `on:` listener that takes application logic written in Rust.")]
+                #[track_caller]
+                fn $key(
+                    self,
+                    value: V,
+                ) -> <Self as AddAnyAttr<Rndr>>::Output<Attr<[<$key:camel>], V, Rndr>>
+                {
+                    self.add_any_attr($key(value))
+                }
+            )*
+		}
+    }
+}
+
+/// Provides methods for HTML event listener attributes.
+pub trait GlobalOnAttributes<Rndr, V>
+where
+    Self: Sized + AddAnyAttr<Rndr>,
+    V: AttributeValue<Rndr>,
+    Rndr: Renderer,
+{
+    on_definitions! {
+        /// The `onabort` attribute specifies the event handler for the abort event.
+        onabort "onabort",
+        /// The `onautocomplete` attribute specifies the event handler for the autocomplete event.
+        onautocomplete "onautocomplete",
+        /// The `onautocompleteerror` attribute specifies the event handler for the autocompleteerror event.
+        onautocompleteerror "onautocompleteerror",
+        /// The `onblur` attribute specifies the event handler for the blur event.
+        onblur "onblur",
+        /// The `oncancel` attribute specifies the event handler for the cancel event.
+        oncancel "oncancel",
+        /// The `oncanplay` attribute specifies the event handler for the canplay event.
+        oncanplay "oncanplay",
+        /// The `oncanplaythrough` attribute specifies the event handler for the canplaythrough event.
+        oncanplaythrough "oncanplaythrough",
+        /// The `onchange` attribute specifies the event handler for the change event.
+        onchange "onchange",
+        /// The `onclick` attribute specifies the event handler for the click event.
+        onclick "onclick",
+        /// The `onclose` attribute specifies the event handler for the close event.
+        onclose "onclose",
+        /// The `oncontextmenu` attribute specifies the event handler for the contextmenu event.
+        oncontextmenu "oncontextmenu",
+        /// The `oncuechange` attribute specifies the event handler for the cuechange event.
+        oncuechange "oncuechange",
+        /// The `ondblclick` attribute specifies the event handler for the double click event.
+        ondblclick "ondblclick",
+        /// The `ondrag` attribute specifies the event handler for the drag event.
+        ondrag "ondrag",
+        /// The `ondragend` attribute specifies the event handler for the dragend event.
+        ondragend "ondragend",
+        /// The `ondragenter` attribute specifies the event handler for the dragenter event.
+        ondragenter "ondragenter",
+        /// The `ondragleave` attribute specifies the event handler for the dragleave event.
+        ondragleave "ondragleave",
+        /// The `ondragover` attribute specifies the event handler for the dragover event.
+        ondragover "ondragover",
+        /// The `ondragstart` attribute specifies the event handler for the dragstart event.
+        ondragstart "ondragstart",
+        /// The `ondrop` attribute specifies the event handler for the drop event.
+        ondrop "ondrop",
+        /// The `ondurationchange` attribute specifies the event handler for the durationchange event.
+        ondurationchange "ondurationchange",
+        /// The `onemptied` attribute specifies the event handler for the emptied event.
+        onemptied "onemptied",
+        /// The `onended` attribute specifies the event handler for the ended event.
+        onended "onended",
+        /// The `onerror` attribute specifies the event handler for the error event.
+        onerror "onerror",
+        /// The `onfocus` attribute specifies the event handler for the focus event.
+        onfocus "onfocus",
+        /// The `onformdata` attribute specifies the event handler for the formdata event.
+        onformdata "onformdata",
+        /// The `oninput` attribute specifies the event handler for the input event.
+        oninput "oninput",
+        /// The `oninvalid` attribute specifies the event handler for the invalid event.
+        oninvalid "oninvalid",
+        /// The `onkeydown` attribute specifies the event handler for the keydown event.
+        onkeydown "onkeydown",
+        /// The `onkeypress` attribute specifies the event handler for the keypress event.
+        onkeypress "onkeypress",
+        /// The `onkeyup` attribute specifies the event handler for the keyup event.
+        onkeyup "onkeyup",
+        /// The `onlanguagechange` attribute specifies the event handler for the languagechange event.
+        onlanguagechange "onlanguagechange",
+        /// The `onload` attribute specifies the event handler for the load event.
+        onload "onload",
+        /// The `onloadeddata` attribute specifies the event handler for the loadeddata event.
+        onloadeddata "onloadeddata",
+        /// The `onloadedmetadata` attribute specifies the event handler for the loadedmetadata event.
+        onloadedmetadata "onloadedmetadata",
+        /// The `onloadstart` attribute specifies the event handler for the loadstart event.
+        onloadstart "onloadstart",
+        /// The `onmousedown` attribute specifies the event handler for the mousedown event.
+        onmousedown "onmousedown",
+        /// The `onmouseenter` attribute specifies the event handler for the mouseenter event.
+        onmouseenter "onmouseenter",
+        /// The `onmouseleave` attribute specifies the event handler for the mouseleave event.
+        onmouseleave "onmouseleave",
+        /// The `onmousemove` attribute specifies the event handler for the mousemove event.
+        onmousemove "onmousemove",
+        /// The `onmouseout` attribute specifies the event handler for the mouseout event.
+        onmouseout "onmouseout",
+        /// The `onmouseover` attribute specifies the event handler for the mouseover event.
+        onmouseover "onmouseover",
+        /// The `onmouseup` attribute specifies the event handler for the mouseup event.
+        onmouseup "onmouseup",
+        /// The `onpause` attribute specifies the event handler for the pause event.
+        onpause "onpause",
+        /// The `onplay` attribute specifies the event handler for the play event.
+        onplay "onplay",
+        /// The `onplaying` attribute specifies the event handler for the playing event.
+        onplaying "onplaying",
+        /// The `onprogress` attribute specifies the event handler for the progress event.
+        onprogress "onprogress",
+        /// The `onratechange` attribute specifies the event handler for the ratechange event.
+        onratechange "onratechange",
+        /// The `onreset` attribute specifies the event handler for the reset event.
+        onreset "onreset",
+        /// The `onresize` attribute specifies the event handler for the resize event.
+        onresize "onresize",
+        /// The `onscroll` attribute specifies the event handler for the scroll event.
+        onscroll "onscroll",
+        /// The `onsecuritypolicyviolation` attribute specifies the event handler for the securitypolicyviolation event.
+        onsecuritypolicyviolation "onsecuritypolicyviolation",
+        /// The `onseeked` attribute specifies the event handler for the seeked event.
+        onseeked "onseeked",
+        /// The `onseeking` attribute specifies the event handler for the seeking event.
+        onseeking "onseeking",
+        /// The `onselect` attribute specifies the event handler for the select event.
+        onselect "onselect",
+        /// The `onslotchange` attribute specifies the event handler for the slotchange event.
+        onslotchange "onslotchange",
+        /// The `onstalled` attribute specifies the event handler for the stalled event.
+        onstalled "onstalled",
+        /// The `onsubmit` attribute specifies the event handler for the submit event.
+        onsubmit "onsubmit",
+        /// The `onsuspend` attribute specifies the event handler for the suspend event.
+        onsuspend "onsuspend",
+        /// The `ontimeupdate` attribute specifies the event handler for the timeupdate event.
+        ontimeupdate "ontimeupdate",
+        /// The `ontoggle` attribute specifies the event handler for the toggle event.
+        ontoggle "ontoggle",
+        /// The `onvolumechange` attribute specifies the event handler for the volumechange event.
+        onvolumechange "onvolumechange",
+        /// The `onwaiting` attribute specifies the event handler for the waiting event.
+        onwaiting "onwaiting",
+        /// The `onwebkitanimationend` attribute specifies the event handler for the webkitanimationend event.
+        onwebkitanimationend "onwebkitanimationend",
+        /// The `onwebkitanimationiteration` attribute specifies the event handler for the webkitanimationiteration event.
+        onwebkitanimationiteration "onwebkitanimationiteration",
+        /// The `onwebkitanimationstart` attribute specifies the event handler for the webkitanimationstart event.
+        onwebkitanimationstart "onwebkitanimationstart",
+        /// The `onwebkittransitionend` attribute specifies the event handler for the webkittransitionend event.
+        onwebkittransitionend "onwebkittransitionend",
+        /// The `onwheel` attribute specifies the event handler for the wheel event.
+        onwheel "onwheel",
+
+    }
+}
+
+impl<T, Rndr, V> GlobalOnAttributes<Rndr, V> for T
+where
+    T: AddAnyAttr<Rndr>,
+    V: AttributeValue<Rndr>,
+    Rndr: Renderer,
+{
+}
