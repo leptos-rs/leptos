213365e4e902055f48d8d3e41e5100d071a6c67d
Greg Johnston <greg.johnston@gmail.com>
2024-07-31 07:15:27 -0400

    chore: clippy

--- integrations/actix/src/lib.rs
+++ integrations/actix/src/lib.rs
@@ -1324,7 +1324,7 @@ impl LeptosRoutes for &mut ServiceConfig {
         let mut router = self;
 
         // register server functions first to allow for wildcard route in Leptos's Router
-        for (path, method) in server_fn::actix::server_fn_paths() {
+        for (path, _) in server_fn::actix::server_fn_paths() {
             let additional_context = additional_context.clone();
             let handler = handle_server_fns_with_context(additional_context);
             router = router.route(path, handler);
