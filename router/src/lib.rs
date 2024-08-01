c953432659b6eff4fdcc815d0038ae6b8fd9ed9e
Greg Johnston <greg.johnston@gmail.com>
2024-07-31 20:29:45 -0400

    chore: fmt

--- router/src/lib.rs
+++ router/src/lib.rs
@@ -19,10 +19,10 @@ mod ssr_mode;
 mod static_route;
 
 pub use generate_route_list::*;
+pub use leptos_router_macro::path;
 pub use matching::*;
 pub use method::*;
 pub use navigate::*;
 //pub use router::*;
 pub use ssr_mode::*;
 pub use static_route::*;
-pub use leptos_router_macro::path;
