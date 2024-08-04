24851576cd0255a01dc377a6db4b54eb67ce3772
martinfrances107 <martinfrances107@hotmail.com>
2024-08-04 22:41:50 +0100

    counter_isomorphic: BroadcastChannel fixes.

--- examples/counter_isomorphic/src/counters.rs
+++ examples/counter_isomorphic/src/counters.rs
@@ -15,7 +15,7 @@ pub mod ssr_imports {
     pub static COUNT: AtomicI32 = AtomicI32::new(0);
 
     pub static COUNT_CHANNEL: LazyLock<BroadcastChannel<i32>> =
-        LazyLock::new(|| BroadcastChannel::<i32>::new());
+        LazyLock::new(BroadcastChannel::<i32>::new);
 }
 
 #[server]
