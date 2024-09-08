b9cccc6b91dfa4e1ab2fdd41a96d22582ce73d38
Greg Johnston <greg.johnston@gmail.com>
2024-09-08 13:58:45 -0400

    fix: revert change, making writes to children notify parents

--- reactive_stores/src/subfield.rs
+++ reactive_stores/src/subfield.rs
@@ -80,7 +80,7 @@ where
 {
     type Value = T;
     type Reader = Mapped<Inner::Reader, T>;
-    type Writer = MappedMut<WriteGuard<ArcTrigger, Inner::UntrackedWriter>, T>;
+    type Writer = MappedMut<WriteGuard<ArcTrigger, Inner::Writer>, T>;
     type UntrackedWriter =
         MappedMut<WriteGuard<ArcTrigger, Inner::UntrackedWriter>, T>;
 
@@ -102,14 +102,14 @@ where
 
     fn writer(&self) -> Option<Self::Writer> {
         let trigger = self.get_trigger(self.path().into_iter().collect());
-        let inner = WriteGuard::new(trigger, self.inner.untracked_writer()?);
+        let inner = WriteGuard::new(trigger, self.inner.writer()?);
         Some(MappedMut::new(inner, self.read, self.write))
     }
 
     fn untracked_writer(&self) -> Option<Self::UntrackedWriter> {
-        let mut guard = self.writer()?;
-        guard.untrack();
-        Some(guard)
+        let trigger = self.get_trigger(self.path().into_iter().collect());
+        let inner = WriteGuard::new(trigger, self.inner.untracked_writer()?);
+        Some(MappedMut::new(inner, self.read, self.write))
     }
 }
 
