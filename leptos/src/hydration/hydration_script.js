b60b6975534426f20d31b417936f3aa2b7abb368
zakstucke <44890343+zakstucke@users.noreply.github.com>
2024-09-24 20:36:13 +0300

    Fix custom root when not using islands.

--- leptos/src/hydration/hydration_script.js
+++ leptos/src/hydration/hydration_script.js
@@ -1,7 +1,7 @@
 (function (root, pkg_path, output_name, wasm_output_name) {
 	import(`${root}/${pkg_path}/${output_name}.js`)
 		.then(mod => {
-			mod.default(`/${pkg_path}/${wasm_output_name}.wasm`).then(() => {
+			mod.default(`${root}/${pkg_path}/${wasm_output_name}.wasm`).then(() => {
 				mod.hydrate();
 			});
 		})
