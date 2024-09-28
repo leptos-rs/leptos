59bebfa1678e233bf53f7d8ccb217edaec2f72f0
Enzo "raskyld" Nocera <enzo@nocera.eu>
2024-09-28 15:06:41 +0200

    feat(server_fn): add generic types
    This commit adds `From` implementations for the
    `Req` and `Res` types using abstraction that are deemed
    "platform-agnostic".
    
    Indeed, both the `http` and `bytes` crates contains types
    that allows us to represent HTTP Request and Response,
    while being capable to target unconventional platforms
    (they even have `no-std` support). This allows the
    server_fn functions to target new platforms,
    for example, the `wasm32-wasip*` targets.
    
    Signed-off-by: Enzo "raskyld" Nocera <enzo@nocera.eu>

---
+++ server_fn/src/request/generic.rs
@@ -0,0 +1,63 @@
+use std::borrow::Cow;
+
+use bytes::Bytes;
+use futures::{stream::{self, Stream}, StreamExt};
+use http::Request;
+
+use crate::request::Req;
+
+impl<CustErr> Req<CustErr> for Request<Bytes>
+where
+    CustErr: 'static,
+{
+    async fn try_into_bytes(
+            self,
+        ) -> Result<Bytes, crate::ServerFnError<CustErr>> {
+        Ok(self.into_body())
+    }
+
+    async fn try_into_string(
+            self,
+        ) -> Result<String, crate::ServerFnError<CustErr>> {
+        String::from_utf8(self.into_body().into())
+            .map_err(|err| crate::ServerFnError::Deserialization(err.to_string()))
+    }
+
+    fn try_into_stream(
+            self,
+        ) -> Result<
+            impl Stream<Item = Result<Bytes, crate::ServerFnError>> + Send + 'static,
+            crate::ServerFnError<CustErr>,
+        > {
+        Ok(
+            stream::iter(self.into_body().into_iter())
+                .ready_chunks(16)
+                .map(|chunk| Ok(Bytes::from(chunk)))
+        )
+    }
+
+    fn to_content_type(&self) -> Option<Cow<'_, str>> {
+        self
+            .headers()
+            .get(http::header::CONTENT_TYPE)
+            .map(|val| String::from_utf8_lossy(val.as_bytes()))
+    }
+
+    fn accepts(&self) -> Option<Cow<'_, str>> {
+        self
+            .headers()
+            .get(http::header::ACCEPT)
+            .map(|val| String::from_utf8_lossy(val.as_bytes()))
+    }
+
+    fn referer(&self) -> Option<Cow<'_, str>> {
+        self
+            .headers()
+            .get(http::header::REFERER)
+            .map(|val| String::from_utf8_lossy(val.as_bytes()))
+    }
+
+    fn as_query(&self) -> Option<&str> {
+        self.uri().query()
+    }
+}
