64edbc91d41c2a813b122a8e1462c1bf9e58f522
Zak Stucke <zakstucke@hotmail.co.uk>
2024-10-12 23:01:41 +0300

    Clippy + optimisation

--- leptos_macro/src/view/mod.rs
+++ leptos_macro/src/view/mod.rs
@@ -679,33 +679,34 @@ pub(crate) fn element_to_tokens(
 
     // Replace nostrip: with __nostrip_ to access the hidden optional method builder for option fields:
     for attr in node.open_tag.attributes.iter_mut() {
-        match attr {
-            NodeAttribute::Attribute(attr) => {
-                if let Some(name) =
-                    attr.key.to_string().strip_prefix("nostrip:")
-                {
-                    let span = match &attr.key {
-                        NodeName::Punctuated(punct) => {
-                            if let Some(segment) = punct.get(1) {
-                                Some(segment.span())
-                            } else {
-                                None
-                            }
+        if let NodeAttribute::Attribute(attr) = attr {
+            let maybe_cleaned_name_and_span =
+                if let NodeName::Punctuated(punct) = &attr.key {
+                    if punct.len() == 2 {
+                        if let Some(cleaned_name) =
+                            attr.key.to_string().strip_prefix("nostrip:")
+                        {
+                            punct.get(1).map(|segment| {
+                                (cleaned_name.to_string(), segment.span())
+                            })
+                        } else {
+                            None
                         }
-                        _ => None,
-                    };
-                    if let Some(span) = span {
-                        let method_key =
-                            format_ident!("__nostrip_{}", name, span = span,);
-                        attr.key = NodeName::Path(ExprPath {
-                            attrs: vec![],
-                            qself: None,
-                            path: method_key.into(),
-                        });
+                    } else {
+                        None
                     }
-                }
+                } else {
+                    None
+                };
+            if let Some((cleaned_name, span)) = maybe_cleaned_name_and_span {
+                let method_key =
+                    format_ident!("__nostrip_{}", cleaned_name, span = span,);
+                attr.key = NodeName::Path(ExprPath {
+                    attrs: vec![],
+                    qself: None,
+                    path: method_key.into(),
+                });
             }
-            _ => {}
         }
     }
 
