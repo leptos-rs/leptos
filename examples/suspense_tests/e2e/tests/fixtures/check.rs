aa6cd083872c18d63d860c284116d629a8ccbac7
Tommy Yu <y@metatoaster.com>
2024-11-17 08:11:54 +1300

    test: fixtures for testing aria-current (#3202)

--- examples/suspense_tests/e2e/tests/fixtures/check.rs
+++ examples/suspense_tests/e2e/tests/fixtures/check.rs
@@ -81,3 +81,12 @@ pub async fn instrumented_counts(
 
     Ok(())
 }
+
+pub async fn link_text_is_aria_current(client: &Client, text: &str) -> Result<()> {
+    let link = find::link_with_text(client, text).await?;
+
+    link.attr("aria-current").await?
+        .expect(format!("aria-current missing for {text}").as_str());
+
+    Ok(())
+}
