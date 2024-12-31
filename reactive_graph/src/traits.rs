1c002a2df814c2b17fde9ab7ae7f6b55af1e34a1
Michael Scofield <mscofield0@tutanota.com>
2024-12-31 16:34:47 +0100

    chore: add missing `#[track_caller]`s (#3422)

--- reactive_graph/src/traits.rs
+++ reactive_graph/src/traits.rs
@@ -103,6 +103,7 @@ pub trait Dispose {
 /// Allows tracking the value of some reactive data.
 pub trait Track {
     /// Subscribes to this signal in the current reactive scope without doing anything with its value.
+    #[track_caller]
     fn track(&self);
 }
 
@@ -404,6 +405,7 @@ where
 /// Notifies subscribers of a change in this signal.
 pub trait Notify {
     /// Notifies subscribers of a change in this signal.
+    #[track_caller]
     fn notify(&self);
 }
 
--- reactive_stores/src/store_field.rs
+++ reactive_stores/src/store_field.rs
@@ -24,12 +24,15 @@ pub trait StoreField: Sized {
     type Writer: UntrackableGuard<Target = Self::Value>;
 
     /// Returns the trigger that tracks access and updates for this field.
+    #[track_caller]
     fn get_trigger(&self, path: StorePath) -> StoreFieldTrigger;
 
     /// The path of this field (see [`StorePath`]).
+    #[track_caller]
     fn path(&self) -> impl IntoIterator<Item = StorePathSegment>;
 
     /// Reactively tracks this field.
+    #[track_caller]
     fn track_field(&self) {
         let path = self.path().into_iter().collect();
         let trigger = self.get_trigger(path);
@@ -38,12 +41,15 @@ pub trait StoreField: Sized {
     }
 
     /// Returns a read guard to access this field.
+    #[track_caller]
     fn reader(&self) -> Option<Self::Reader>;
 
     /// Returns a write guard to update this field.
+    #[track_caller]
     fn writer(&self) -> Option<Self::Writer>;
 
     /// The keys for this field, if it is a keyed field.
+    #[track_caller]
     fn keys(&self) -> Option<KeyMap>;
 }
 
@@ -55,26 +61,31 @@ where
     type Reader = Plain<T>;
     type Writer = WriteGuard<ArcTrigger, UntrackedWriteGuard<T>>;
 
+    #[track_caller]
     fn get_trigger(&self, path: StorePath) -> StoreFieldTrigger {
         let triggers = &self.signals;
         let trigger = triggers.write().or_poisoned().get_or_insert(path);
         trigger
     }
 
+    #[track_caller]
     fn path(&self) -> impl IntoIterator<Item = StorePathSegment> {
         iter::empty()
     }
 
+    #[track_caller]
     fn reader(&self) -> Option<Self::Reader> {
         Plain::try_new(Arc::clone(&self.value))
     }
 
+    #[track_caller]
     fn writer(&self) -> Option<Self::Writer> {
         let trigger = self.get_trigger(Default::default());
         let guard = UntrackedWriteGuard::try_new(Arc::clone(&self.value))?;
         Some(WriteGuard::new(trigger.children, guard))
     }
 
+    #[track_caller]
     fn keys(&self) -> Option<KeyMap> {
         Some(self.keys.clone())
     }
@@ -89,6 +100,7 @@ where
     type Reader = Plain<T>;
     type Writer = WriteGuard<ArcTrigger, UntrackedWriteGuard<T>>;
 
+    #[track_caller]
     fn get_trigger(&self, path: StorePath) -> StoreFieldTrigger {
         self.inner
             .try_get_value()
@@ -96,6 +108,7 @@ where
             .unwrap_or_else(unwrap_signal!(self))
     }
 
+    #[track_caller]
     fn path(&self) -> impl IntoIterator<Item = StorePathSegment> {
         self.inner
             .try_get_value()
@@ -103,14 +116,17 @@ where
             .unwrap_or_else(unwrap_signal!(self))
     }
 
+    #[track_caller]
     fn reader(&self) -> Option<Self::Reader> {
         self.inner.try_get_value().and_then(|n| n.reader())
     }
 
+    #[track_caller]
     fn writer(&self) -> Option<Self::Writer> {
         self.inner.try_get_value().and_then(|n| n.writer())
     }
 
+    #[track_caller]
     fn keys(&self) -> Option<KeyMap> {
         self.inner.try_get_value().and_then(|inner| inner.keys())
     }
