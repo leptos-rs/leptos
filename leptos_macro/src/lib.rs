c859b0790112061342ff060ed2a947c4da17d179
Greg Johnston <greg.johnston@gmail.com>
2025-01-17 15:15:12 -0500

    feat: `#[lazy]` macros to support lazy loading and code splitting (#3477)

--- leptos_macro/src/lib.rs
+++ leptos_macro/src/lib.rs
@@ -23,6 +23,7 @@ mod params;
 mod view;
 use crate::component::unmodified_fn_name_from_fn_name;
 mod component;
+mod lazy;
 mod memo;
 mod slice;
 mod slot;
@@ -1001,4 +1002,18 @@ pub fn slice(input: TokenStream) -> TokenStream {
 #[proc_macro]
 pub fn memo(input: TokenStream) -> TokenStream {
     memo::memo_impl(input)
+}
+
+/// The `#[lazy]` macro marks an `async` function as a function that can be lazy-loaded from a
+/// separate (WebAssembly) binary.
+///
+/// The first time the function is called, calling the function will first load that other binary,
+/// then call the function. On subsequent call it will be called immediately, but still return
+/// asynchronously to maintain the same API.
+///
+/// All parameters and output types should be concrete types, with no generics.
+#[proc_macro_attribute]
+#[proc_macro_error]
+pub fn lazy(args: proc_macro::TokenStream, s: TokenStream) -> TokenStream {
+    lazy::lazy_impl(args, s)
 }
