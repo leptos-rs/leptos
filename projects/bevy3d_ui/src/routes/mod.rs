e66b8f1419b9422535a44045a9927310435dbc0a
martinfrances107 <martinfrances107@hotmail.com>
2025-02-12 16:26:11 +0000

    projects/bevy3d_ui Migrate to leptos 0.7.7

--- projects/bevy3d_ui/src/routes/mod.rs
+++ projects/bevy3d_ui/src/routes/mod.rs
@@ -1,23 +1,25 @@
 pub mod demo1;
 use demo1::Demo1;
-use leptos::*;
-use leptos_meta::{provide_meta_context, Meta, Stylesheet, Title};
-use leptos_router::*;
-
+use leptos::prelude::*;
+use leptos_meta::Title;
+use leptos_meta::{provide_meta_context, MetaTags, Stylesheet};
+use leptos_router::components::*;
+use leptos_router::StaticSegment;
 #[component]
 pub fn RootPage() -> impl IntoView {
     provide_meta_context();
 
     view! {
-        <Meta name="charset" content="UTF-8"/>
-        <Meta name="description" content="Leptonic CSR template"/>
-        <Meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <Meta name="theme-color" content="#e66956"/>
-        <Stylesheet href="https://fonts.googleapis.com/css?family=Roboto&display=swap"/>
+        <meta name="charset" content="UTF-8"/>
+        <meta name="description" content="Leptonic CSR template"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="theme-color" content="#e66956"/>
         <Title text="Leptos Bevy3D Example"/>
+        <Stylesheet href="https://fonts.googleapis.com/css?family=Roboto&display=swap"/>
+        <MetaTags/>
         <Router>
-            <Routes>
-                <Route path="" view=|| view! { <Demo1/> }/>
+            <Routes fallback=move || "Not found.">
+                <Route path=StaticSegment("") view=Demo1 />
             </Routes>
         </Router>
     }
