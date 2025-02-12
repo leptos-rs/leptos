8f9c6afddb07fcd6964407c02d0518a5f5517a28
martinfrances107 <martinfrances107@hotmail.com>
2025-02-12 19:42:49 +0000

    Minor: projects/bevy3d_ui - Better Meta.

--- projects/bevy3d_ui/src/routes/mod.rs
+++ projects/bevy3d_ui/src/routes/mod.rs
@@ -1,6 +1,7 @@
 pub mod demo1;
 use demo1::Demo1;
 use leptos::prelude::*;
+use leptos_meta::Meta;
 use leptos_meta::Title;
 use leptos_meta::{provide_meta_context, MetaTags, Stylesheet};
 use leptos_router::components::*;
@@ -10,10 +11,10 @@ pub fn RootPage() -> impl IntoView {
     provide_meta_context();
 
     view! {
-        <meta name="charset" content="UTF-8"/>
-        <meta name="description" content="Leptonic CSR template"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <meta name="theme-color" content="#e66956"/>
+        <Meta name="charset" content="UTF-8"/>
+        <Meta name="description" content="Leptonic CSR template"/>
+        <Meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <Meta name="theme-color" content="#e66956"/>
         <Title text="Leptos Bevy3D Example"/>
         <Stylesheet href="https://fonts.googleapis.com/css?family=Roboto&display=swap"/>
         <MetaTags/>
