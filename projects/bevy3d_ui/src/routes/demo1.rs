590728e47e99c0f932cb39abf6c30807c30e5126
martin frances <martinfrances107@hotmail.com>
2025-02-13 23:37:23 +0000

    projects/bevy3d_ui: Bevy - Bugfix, clippy and crate bump (#3603)
    The bugfix is related to access to a signal
    
    "
    bevy3d_ui-b20a0a6a298e7144.js:2637 At src/routes/demo1.rs:24:23, you access a
    reactive_graph::signal::read::ReadSignal<bevy3d_ui::demos::bevydemo1::scene::Scene>
    (defined at src/routes/demo1.rs:19:39) outside a reactive tracking context.
    This might mean your app is not responding to changes in signal values in the way you expect.
    "
    
    The solution is to use .get_untracked() inside an "effect" block.
    
    Lots of small clippy fixes.
    
    Also here are the crates that have been bumped
    
    -wasm-bindgen = "0.2.92"
    -wasm-bindgen-test = "0.3.42"
    -web-sys = "0.3.69"
    +wasm-bindgen = "0.2.100"
    +wasm-bindgen-test = "0.3.50"
    +web-sys = "0.3.77"

--- projects/bevy3d_ui/src/routes/demo1.rs
+++ projects/bevy3d_ui/src/routes/demo1.rs
@@ -21,7 +21,7 @@ pub fn Demo1() -> impl IntoView {
     // We need to add the 3D view onto the canvas post render.
     Effect::new(move |_| {
         request_animation_frame(move || {
-            scene_sig.get().setup();
+            scene_sig.get_untracked().setup();
         });
     });
 
