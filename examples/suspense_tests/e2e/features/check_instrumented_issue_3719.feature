leptos-rs/leptos

commit cdee2a9476109b10f5d8da268684bd1257c6ce9b

feat: "islands router" for client-side navigations when in islands mode (#3502)

examples/islands_router/Cargo.toml
@@ -14,7 +14,7 @@ leptos = { path = "../../leptos", features = ["tracing", "islands"] }
 leptos_router = { path = "../../router" }
 server_fn = { path = "../../server_fn", features = ["serde-lite"] }
 leptos_axum = { path = "../../integrations/axum", features = [
-  "dont-use-islands-router",
+  "islands-router",
 ], optional = true }
 log = "0.4.22"
 serde = { version = "1.0", features = ["derive"] }
@@ -22,7 +22,8 @@ axum = { version = "0.8.1", optional = true }
 tower = { version = "0.4.13", optional = true }
 tower-http = { version = "0.5.2", features = ["fs"], optional = true }
 tokio = { version = "1.39", features = ["full"], optional = true }
-wasm-bindgen = "0.2.93"
+wasm-bindgen = "0.2.100"
+serde_json = "1.0.133"
 
 [features]
 hydrate = ["leptos/hydrate"]
@@ -55,11 +56,11 @@ site-root = "target/site"
 # Defaults to pkg
 site-pkg-dir = "pkg"
 # [Optional] The source CSS file. If it ends with .sass or .scss then it will be compiled by dart-sass into CSS. The CSS is optimized by Lightning CSS before being written to <site-root>/<site-pkg>/app.css
-style-file = "./style.css"
+style-file = "style.css"
 # [Optional] Files in the asset-dir will be copied to the site-root directory
 assets-dir = "public"
 # The IP and port (ex: 127.0.0.1:3000) where the server serves the content. Use it in your server setup.
-site-addr = "127.0.0.1:3000"
+site-addr = "127.0.0.1:3009"
 # The port to use for automatic reload monitoring
 reload-port = 3001
 #  The browserlist query used for optimizing the CSS.

examples/islands_router/public/routing.js
@@ -1,140 +1,0 @@
-window.addEventListener("click", async (ev) => {
-	// confirm that this is an <a> that meets our requirements
-	if (
-		ev.defaultPrevented ||
-		ev.button !== 0 ||
-		ev.metaKey ||
-		ev.altKey ||
-		ev.ctrlKey ||
-		ev.shiftKey
-	      )
-        return;
-
-      /** @type HTMLAnchorElement | undefined;*/
-      const a = ev
-        .composedPath()
-        .find(el => el instanceof Node && el.nodeName.toUpperCase() === "A");
-
-	if (!a) return;
-
-     const svg = a.namespaceURI === "http://www.w3.org/2000/svg";
-	const href = svg ? a.href.baseVal : a.href;
-      const target = svg ? a.target.baseVal : a.target;
-      if (target || (!href && !a.hasAttribute("state"))) return;
-
-      const rel = (a.getAttribute("rel") || "").split(/\s+/);
-      if (a.hasAttribute("download") || (rel && rel.includes("external"))) return;
-
-      const url = svg ? new URL(href, document.baseURI) : new URL(href);
-      if (
-        url.origin !== window.location.origin // ||  
-	      // TODO base
-        //(basePath && url.pathname && !url.pathname.toLowerCase().startsWith(basePath.toLowerCase()))
-      )
-        return;
-
-	ev.preventDefault();
-
-	// fetch the new page
-	const resp = await fetch(url);
-	    const htmlString = await resp.text();
-
-    // Use DOMParser to parse the HTML string
-    const parser = new DOMParser();
-	// TODO parse from the request stream instead?
-    const doc = parser.parseFromString(htmlString, 'text/html');
-
-	// The 'doc' variable now contains the parsed DOM
-	const transition = async () => {
-		const oldDocWalker = document.createTreeWalker(document);
-		const newDocWalker = doc.createTreeWalker(doc);
-		let oldNode = oldDocWalker.currentNode;
-		let newNode = newDocWalker.currentNode;
-		while(oldDocWalker.nextNode() && newDocWalker.nextNode()) {
-			oldNode = oldDocWalker.currentNode;
-			newNode = newDocWalker.currentNode;
-			// if the nodes are different, we need to replace the old with the new
-			// because of the typed view tree, this should never actually happen
-			if (oldNode.nodeType !== newNode.nodeType) {
-				oldNode.replaceWith(newNode);
-			}
-			// if it's a text node, just update the text with the new text
-			else if (oldNode.nodeType === Node.TEXT_NODE) {
-				oldNode.textContent = newNode.textContent;
-			}
-			// if it's an element, replace if it's a different tag, or update attributes
-			else if (oldNode.nodeType === Node.ELEMENT_NODE) {
-				/** @type Element */
-				const oldEl = oldNode;
-				/** @type Element */
-				const newEl = newNode;
-				if (oldEl.tagName !== newEl.tagName) {
-					oldEl.replaceWith(newEl);
-				}
-				else {
-					for(const attr of newEl.attributes) {
-						oldEl.setAttribute(attr.name, attr.value);
-					}
-				}
-			}
-			// we use comment "branch marker" nodes to distinguish between different branches in the statically-typed view tree
-			// if one of these marker is hit, then there are two options
-			// 1) it's the same branch, and we just keep walking until the end 
-			// 2) it's a different branch, in which case the old can be replaced with the new wholesale
-			else if (oldNode.nodeType === Node.COMMENT_NODE) {
-				const oldText = oldNode.textContent;
-				const newText = newNode.textContent;
-				if(oldText.startsWith("bo") && newText !== oldText) {
-					oldDocWalker.nextNode();
-					newDocWalker.nextNode();
-					const oldRange = new Range();
-					const newRange = new Range();
-					let oldBranches = 1;
-					let newBranches = 1;
-					while(oldBranches > 0 && newBranches > 0) {
-						if(oldDocWalker.nextNode() && newDocWalker.nextNode()) {
-						console.log(oldDocWalker.currentNode, newDocWalker.currentNode);
-							if(oldDocWalker.currentNode.nodeType === Node.COMMENT_NODE) {
-								if(oldDocWalker.currentNode.textContent.startsWith("bo")) {
-									oldBranches += 1;
-								} else if(oldDocWalker.currentNode.textContent.startsWith("bc")) {
-
-									oldBranches -= 1;
-								}
-							}
-							if(newDocWalker.currentNode.nodeType === Node.COMMENT_NODE) {
-								if(newDocWalker.currentNode.textContent.startsWith("bo")) {
-									newBranches += 1;
-								} else if(newDocWalker.currentNode.textContent.startsWith("bc")) {
-
-									newBranches -= 1;
-								}
-							}
-						}
-					}
-
-					try {
-						oldRange.setStartAfter(oldNode);
-						oldRange.setEndBefore(oldDocWalker.currentNode);
-						newRange.setStartAfter(newNode);
-						newRange.setEndBefore(newDocWalker.currentNode);
-						const newContents = newRange.extractContents();
-						oldRange.deleteContents();
-						oldRange.insertNode(newContents);
-						oldNode.replaceWith(newNode);
-						oldDocWalker.currentNode.replaceWith(newDocWalker.currentNode);
-					} catch (e) {
-						console.error(e);
-					}
-				} 			}
-		}
-	};
-	// Not all browsers support startViewTransition; see https://caniuse.com/?search=startViewTransition
-	if (document.startViewTransition) {
-		await document.startViewTransition(transition);
-	} else {
-		await transition()
-	}
-	window.history.pushState(undefined, null, url);
-});
-

examples/islands_router/src/app.rs
@@ -1,8 +1,13 @@
-use leptos::prelude::*;
-use leptos_router::{
-    components::{FlatRoutes, Route, Router},
-    StaticSegment,
+use leptos::{
+    either::{Either, EitherOf3},
+    prelude::*,
 };
+use leptos_router::{
+    components::{Route, Router, Routes},
+    hooks::{use_params_map, use_query_map},
+    path,
+};
+use serde::{Deserialize, Serialize};
 
 pub fn shell(options: LeptosOptions) -> impl IntoView {
     view! {
@@ -12,7 +17,7 @@ pub fn shell(options: LeptosOptions) -> impl IntoView {
                 <meta charset="utf-8"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <AutoReload options=options.clone()/>
-                <HydrationScripts options=options islands=true/>
+                <HydrationScripts options=options islands=true islands_router=true/>
                 <link rel="stylesheet" id="leptos" href="/pkg/islands.css"/>
                 <link rel="shortcut icon" type="image/ico" href="/favicon.ico"/>
             </head>
@@ -26,34 +31,180 @@ pub fn shell(options: LeptosOptions) -> impl IntoView {
 #[component]
 pub fn App() -> impl IntoView {
     view! {
-        <script src="/routing.js"></script>
         <Router>
             <header>
-                <h1>"My Application"</h1>
+                <h1>"My Contacts"</h1>
             </header>
             <nav>
-                <a href="/">"Page A"</a>
-                <a href="/b">"Page B"</a>
+                <a href="/">"Home"</a>
+                <a href="/about">"About"</a>
             </nav>
             <main>
-                <p>
-                    <label>"Home Checkbox" <input type="checkbox"/></label>
-                </p>
-                <FlatRoutes fallback=|| "Not found.">
-                    <Route path=StaticSegment("") view=PageA/>
-                    <Route path=StaticSegment("b") view=PageB/>
-                </FlatRoutes>
+                <Routes fallback=|| "Not found.">
+                    <Route path=path!("") view=Home/>
+                    <Route path=path!("user/:id") view=Details/>
+                    <Route path=path!("about") view=About/>
+                </Routes>
             </main>
         </Router>
     }
 }
 
-#[component]
-pub fn PageA() -> impl IntoView {
-    view! { <label>"Page A" <input type="checkbox"/></label> }
+#[server]
+pub async fn search(query: String) -> Result<Vec<User>, ServerFnError> {
+    let users = tokio::fs::read_to_string("./mock_data.json").await?;
+    let data: Vec<User> = serde_json::from_str(&users)?;
+    let query = query.to_ascii_lowercase();
+    Ok(data
+        .into_iter()
+        .filter(|user| {
+            user.first_name.to_ascii_lowercase().contains(&query)
+                || user.last_name.to_ascii_lowercase().contains(&query)
+                || user.email.to_ascii_lowercase().contains(&query)
+        })
+        .collect())
+}
+
+#[server]
+pub async fn delete_user(id: u32) -> Result<(), ServerFnError> {
+    let users = tokio::fs::read_to_string("./mock_data.json").await?;
+    let mut data: Vec<User> = serde_json::from_str(&users)?;
+    data.retain(|user| user.id != id);
+    let new_json = serde_json::to_string(&data)?;
+    tokio::fs::write("./mock_data.json", &new_json).await?;
+    Ok(())
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct User {
+    id: u32,
+    first_name: String,
+    last_name: String,
+    email: String,
 }
 
 #[component]
-pub fn PageB() -> impl IntoView {
-    view! { <label>"Page B" <input type="checkbox"/></label> }
+pub fn Home() -> impl IntoView {
+    let q = use_query_map();
+    let q = move || q.read().get("q");
+    let data = Resource::new(q, |q| async move {
+        if let Some(q) = q {
+            search(q).await
+        } else {
+            Ok(vec![])
+        }
+    });
+    let delete_user_action = ServerAction::<DeleteUser>::new();
+
+    let view = move || {
+        Suspend::new(async move {
+            let users = data.await.unwrap();
+            if q().is_none() {
+                EitherOf3::A(view! {
+                    <p class="note">"Enter a search to begin viewing contacts."</p>
+                })
+            } else if users.is_empty() {
+                EitherOf3::B(view! {
+                    <p class="note">"No users found matching that search."</p>
+                })
+            } else {
+                EitherOf3::C(view! {
+                    <table>
+                        <tbody>
+                            <For
+                                each=move || users.clone()
+                                key=|user| user.id
+                                let:user
+                            >
+                                <tr>
+                                    <td>{user.first_name}</td>
+                                    <td>{user.last_name}</td>
+                                    <td>{user.email}</td>
+                                    <td>
+                                        <a href=format!("/user/{}", user.id)>"Details"</a>
+                                        <input type="checkbox"/>
+                                        <ActionForm action=delete_user_action>
+                                            <input type="hidden" name="id" value=user.id/>
+                                            <input type="submit" value="Delete"/>
+                                        </ActionForm>
+                                    </td>
+                                </tr>
+                            </For>
+                        </tbody>
+                    </table>
+                })
+            }
+        })
+    };
+    view! {
+        <section class="page">
+            <form method="GET" class="search">
+                <input type="search" name="q" value=q autofocus oninput="this.form.requestSubmit()"/>
+                <input type="submit"/>
+            </form>
+            <Suspense fallback=|| view! { <p>"Loading..."</p> }>{view}</Suspense>
+        </section>
+    }
+}
+
+#[component]
+pub fn Details() -> impl IntoView {
+    #[server]
+    pub async fn get_user(id: u32) -> Result<Option<User>, ServerFnError> {
+        let users = tokio::fs::read_to_string("./mock_data.json").await?;
+        let data: Vec<User> = serde_json::from_str(&users)?;
+        Ok(data.iter().find(|user| user.id == id).cloned())
+    }
+    let params = use_params_map();
+    let id = move || {
+        params
+            .read()
+            .get("id")
+            .and_then(|id| id.parse::<u32>().ok())
+    };
+    let user = Resource::new(id, |id| async move {
+        match id {
+            None => Ok(None),
+            Some(id) => get_user(id).await,
+        }
+    });
+
+    move || {
+        Suspend::new(async move {
+            user.await.map(|user| match user {
+                None => Either::Left(view! {
+                    <section class="page">
+                        <h2>"Not found."</h2>
+                        <p>"Sorry — we couldn’t find that user."</p>
+                    </section>
+                }),
+                Some(user) => Either::Right(view! {
+                    <section class="page">
+                        <h2>{user.first_name} " " { user.last_name}</h2>
+                        <p class="email">{user.email}</p>
+                    </section>
+                }),
+            })
+        })
+    }
+}
+
+#[component]
+pub fn About() -> impl IntoView {
+    view! {
+        <section class="page">
+            <h2>"About"</h2>
+            <p>"This demo is intended to show off an experimental “islands router” feature, which mimics the smooth transitions and user experience of client-side routing while minimizing the amount of code that actually runs in the browser."</p>
+            <p>"By default, all the content in this application is only rendered on the server. But you can add client-side interactivity via islands like this one:"</p>
+            <Counter/>
+        </section>
+    }
+}
+
+#[island]
+pub fn Counter() -> impl IntoView {
+    let count = RwSignal::new(0);
+    view! {
+        <button class="counter" on:click=move |_| *count.write() += 1>{count}</button>
+    }
 }

examples/islands_router/style.css
@@ -1,3 +1,52 @@
-.pending {
-	color: purple;
+body {
+	font-family: system-ui, sans-serif;
+	background-color: #f6f6fa;
+}
+
+h1, h2, h3, h4, h5, h6 {
+	font-family: ui-rounded, 'Hiragino Maru Gothic ProN', Quicksand, Comfortaa, Manjari, 'Arial Rounded MT', 'Arial Rounded MT Bold', Calibri, source-sans-pro, sans-serif;
+	text-align: center;
+}
+
+nav {
+	padding: 1rem;
+	text-align: center;
+}
+
+nav a {
+	margin: 1rem;
+}
+
+form.search {
+	display: flex;
+	margin: 2rem auto;
+	justify-content: center;
+}
+
+td {
+	min-width: 10rem;
+	width: 10rem;
+}
+
+table {
+	min-width: 100%;
+}
+
+.page {
+	width: 80%;
+	margin: auto;
+}
+
+td:last-child > * {
+	display: inline-block;
+}
+
+.note, .note {
+	text-align: center;
+}
+
+button.counter {
+	display: block;
+	font-size: 2rem;
+	margin: auto;
 }

examples/tailwind_axum/src/style.css
@@ -1,0 +1,69 @@
+/*! tailwindcss v4.0.0 | MIT License | https://tailwindcss.com */
+@tailwind base;
+@tailwind components;
+.relative {
+  position: relative;
+}
+.m-auto {
+  margin: auto;
+}
+.flex {
+  display: flex;
+}
+.min-h-screen {
+  min-height: 100vh;
+}
+.transform {
+  transform: var(--tw-rotate-x) var(--tw-rotate-y) var(--tw-rotate-z) var(--tw-skew-x) var(--tw-skew-y);
+}
+.flex-col {
+  flex-direction: column;
+}
+.flex-row-reverse {
+  flex-direction: row-reverse;
+}
+.flex-wrap {
+  flex-wrap: wrap;
+}
+.border-b-4 {
+  border-bottom-style: var(--tw-border-style);
+  border-bottom-width: 4px;
+}
+.border-l-2 {
+  border-left-style: var(--tw-border-style);
+  border-left-width: 2px;
+}
+.bg-gradient-to-tl {
+  --tw-gradient-position: to top left in oklab,;
+  background-image: linear-gradient(var(--tw-gradient-stops));
+}
+@property --tw-rotate-x {
+  syntax: "*";
+  inherits: false;
+  initial-value: rotateX(0);
+}
+@property --tw-rotate-y {
+  syntax: "*";
+  inherits: false;
+  initial-value: rotateY(0);
+}
+@property --tw-rotate-z {
+  syntax: "*";
+  inherits: false;
+  initial-value: rotateZ(0);
+}
+@property --tw-skew-x {
+  syntax: "*";
+  inherits: false;
+  initial-value: skewX(0);
+}
+@property --tw-skew-y {
+  syntax: "*";
+  inherits: false;
+  initial-value: skewY(0);
+}
+@property --tw-border-style {
+  syntax: "*";
+  inherits: false;
+  initial-value: solid;
+}

integrations/actix/Cargo.toml
@@ -33,7 +33,7 @@ once_cell = "1"
 rustdoc-args = ["--generate-link-to-definition"]
 
 [features]
-dont-use-islands-router = []
+islands-router = []
 tracing = ["dep:tracing"]
 
 [package.metadata.cargo-all-features]

integrations/actix/src/lib.rs
@@ -23,6 +23,7 @@ use hydration_context::SsrSharedContext;
 use leptos::{
     config::LeptosOptions,
     context::{provide_context, use_context},
+    hydration::IslandsRouterNavigation,
     prelude::expect_context,
     reactive::{computed::ScopedFuture, owner::Owner},
     IntoView,
@@ -654,12 +655,27 @@ where
     IV: IntoView + 'static,
 {
     _ = replace_blocks; // TODO
-    handle_response(method, additional_context, app_fn, |app, chunks| {
-        Box::pin(async move {
-            Box::pin(app.to_html_stream_out_of_order().chain(chunks()))
-                as PinnedStream<String>
-        })
-    })
+    handle_response(
+        method,
+        additional_context,
+        app_fn,
+        |app, chunks, supports_ooo| {
+            Box::pin(async move {
+                let app = if cfg!(feature = "islands-router") {
+                    if supports_ooo {
+                        app.to_html_stream_out_of_order_branching()
+                    } else {
+                        app.to_html_stream_in_order_branching()
+                    }
+                } else if supports_ooo {
+                    app.to_html_stream_out_of_order()
+                } else {
+                    app.to_html_stream_in_order()
+                };
+                Box::pin(app.chain(chunks())) as PinnedStream<String>
+            })
+        },
+    )
 }
 
 /// Returns an Actix [struct@Route](actix_web::Route) that listens for a `GET` request and tries
@@ -685,12 +701,21 @@ pub fn render_app_to_stream_in_order_with_context<IV>(
 where
     IV: IntoView + 'static,
 {
-    handle_response(method, additional_context, app_fn, |app, chunks| {
-        Box::pin(async move {
-            Box::pin(app.to_html_stream_in_order().chain(chunks()))
-                as PinnedStream<String>
-        })
-    })
+    handle_response(
+        method,
+        additional_context,
+        app_fn,
+        |app, chunks, _supports_ooo| {
+            Box::pin(async move {
+                let app = if cfg!(feature = "islands-router") {
+                    app.to_html_stream_in_order_branching()
+                } else {
+                    app.to_html_stream_in_order()
+                };
+                Box::pin(app.chain(chunks())) as PinnedStream<String>
+            })
+        },
+    )
 }
 
 /// Returns an Actix [struct@Route](actix_web::Route) that listens for a `GET` request and tries
@@ -722,12 +747,13 @@ where
 fn async_stream_builder<IV>(
     app: IV,
     chunks: BoxedFnOnce<PinnedStream<String>>,
+    _supports_ooo: bool,
 ) -> PinnedFuture<PinnedStream<String>>
 where
     IV: IntoView + 'static,
 {
     Box::pin(async move {
-        let app = if cfg!(feature = "dont-use-islands-router") {
+        let app = if cfg!(feature = "islands-router") {
             app.to_html_stream_in_order_branching()
         } else {
             app.to_html_stream_in_order()
@@ -767,6 +793,7 @@ fn leptos_corrected_path(req: &HttpRequest) -> String {
     }
 }
 
+#[allow(clippy::type_complexity)]
 fn handle_response<IV>(
     method: Method,
     additional_context: impl Fn() + 'static + Clone + Send,
@@ -774,6 +801,7 @@ fn handle_response<IV>(
     stream_builder: fn(
         IV,
         BoxedFnOnce<PinnedStream<String>>,
+        bool,
     ) -> PinnedFuture<PinnedStream<String>>,
 ) -> Route
 where
@@ -784,6 +812,9 @@ where
         let add_context = additional_context.clone();
 
         async move {
+            let is_island_router_navigation = cfg!(feature = "islands-router")
+                && req.headers().get("Islands-Router").is_some();
+
             let res_options = ResponseOptions::default();
             let (meta_context, meta_output) = ServerMetaContext::new();
 
@@ -794,6 +825,10 @@ where
                 move || {
                     provide_contexts(req, &meta_context, &res_options);
                     add_context();
+
+                    if is_island_router_navigation {
+                        provide_context(IslandsRouterNavigation);
+                    }
                 }
             };
 
@@ -803,6 +838,7 @@ where
                 additional_context,
                 res_options,
                 stream_builder,
+                !is_island_router_navigation,
             )
             .await;
 
@@ -1093,6 +1129,7 @@ impl StaticRouteGenerator {
             app_fn.clone(),
             additional_context,
             async_stream_builder,
+            false,
         );
 
         let sc = owner.shared_context().unwrap();

integrations/axum/Cargo.toml
@@ -36,7 +36,7 @@ tokio = { version = "1.43", features = ["net", "rt-multi-thread"] }
 [features]
 wasm = []
 default = ["tokio/fs", "tokio/sync", "tower-http/fs", "tower/util"]
-dont-use-islands-router = []
+islands-router = []
 tracing = ["dep:tracing"]
 
 [package.metadata.docs.rs]

commit abc56316548d9f3e85179174f12b6ba3ef11cc4d

test: regression from #3502
- Also as reported in #3719, which has an actual minimum example.
- The "quicker" test had a reset but that runs into timing issue with
  the way this reset is done too soon after resource usage, so leaving
  this out and we will just trust the bigger counters.

examples/suspense_tests/e2e/features/check_instrumented_issue_3719.feature
@@ -1,0 +1,99 @@
+@check_instrumented_issue_3719
+Feature: Using instrumented counters to test regression from #3502.
+    Check that the suspend/suspense and the underlying resources are
+    called with the expected number of times.  If this was already in
+    place by #3502 (5c43c18) it should have caught this regression.
+    For a better minimum demonstration see #3719.
+
+    Background:
+
+        Given I see the app
+        And I select the mode Instrumented
+
+    Scenario: follow all paths via CSR avoids #3502
+        Given I select the following links
+            | Item Listing         |
+            | Item 1               |
+            | Inspect path2        |
+            | Inspect path2/field3 |
+        And I click on Reset CSR Counters
+        When I select the following links
+            | Inspect path2/field1 |
+            | Inspect path2/field2 |
+        And I go check the Counters
+        Then I see the following counters under section
+            | Suspend Calls      |   |
+            | item_listing       | 0 |
+            | item_overview      | 0 |
+            | item_inspect       | 2 |
+        And the following counters under section
+            | Server Calls (CSR) |   |
+            | list_items         | 0 |
+            | get_item           | 0 |
+            | inspect_item_root  | 0 |
+            | inspect_item_field | 2 |
+
+    # To show that starting directly from within a param will simply
+    # cause the problem.
+    Scenario: Quicker way to demonstrate regression caused by #3502
+        Given I select the link Target 123
+        # And I click on Reset CSR Counters
+        When I select the following links
+            | Inspect path2/field1 |
+            | Inspect path2/field2 |
+        And I go check the Counters
+        Then I see the following counters under section
+            | Suspend Calls      |   |
+            | item_listing       | 0 |
+            | item_overview      | 0 |
+            | item_inspect       | 3 |
+        And the following counters under section
+            | Server Calls (CSR) |   |
+            | list_items         | 1 |
+            | get_item           | 1 |
+            | inspect_item_root  | 0 |
+            | inspect_item_field | 4 |
+
+    Scenario: Follow paths ordinarily down to a target
+        Given I select the following links
+            | Item Listing         |
+            | Item 1               |
+        And I click on Reset CSR Counters
+        When I select the following links
+            | Target 4## |
+            | Target 3## |
+        And I go check the Counters
+        Then I see the following counters under section
+            | Suspend Calls      |   |
+            | item_listing       | 0 |
+            | item_overview      | 2 |
+            | item_inspect       | 0 |
+        And the following counters under section
+            | Server Calls (CSR) |   |
+            | list_items         | 0 |
+            | get_item           | 2 |
+            | inspect_item_root  | 0 |
+            | inspect_item_field | 0 |
+
+    Scenario: Same as above, but add a refresh to test hydration
+        Given I select the following links
+            | Item Listing         |
+            | Item 1               |
+        And I refresh the page
+        And I click on Reset CSR Counters
+        When I select the following links
+            | Target 4## |
+            | Target 3## |
+        And I go check the Counters
+        Then I see the following counters under section
+            | Suspend Calls      |   |
+            | item_listing       | 0 |
+            | item_overview      | 2 |
+            | item_inspect       | 0 |
+        And the following counters under section
+            | Server Calls (CSR) |   |
+            | list_items         | 0 |
+            | get_item           | 2 |
+            | inspect_item_root  | 0 |
+            | inspect_item_field | 0 |
+