ae23364ef888640645aefe7d68c0e3a3dac01224
Ifiok Jr. <ifiokotung@gmail.com>
2025-03-22 08:04:13 +0000

    chore: add comment explaining annotation

--- server_fn/tests/server_macro.rs
+++ server_fn/tests/server_macro.rs
@@ -1,4 +1,17 @@
-#![cfg(feature = "browser")]
+// The trybuild output has slightly different error message ouptut for
+// different combinations of features. Since tests are run with `test-all-features`
+// multiple combinations of features are tested. This ensures this file is only
+// run when **only** the browser feature is enabled.
+#![cfg(all(
+    feature = "browser",
+    not(any(
+        feature = "postcard",
+        feature = "multipart",
+        feature = "serde-lite",
+        feature = "cbor",
+        feature = "msgpack"
+    ))
+))]
 
 #[test]
 fn aliased_results() {
