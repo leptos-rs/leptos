6006c3330746ced750cf07a21546e5fde404e81f
Saber Haj Rabiee <saber.hajrabiee@gmail.com>
2025-04-03 09:11:14 -0700

    Revert "fix: more atomic orderings"
    This reverts commit 02f4cfe437c33110e3e7702a0f72bc68575d3a2b.

--- any_spawner/tests/futures_runtime.rs
+++ any_spawner/tests/futures_runtime.rs
@@ -26,12 +26,12 @@ fn can_make_local_progress() {
     Executor::spawn_local({
         let counter = Arc::clone(&counter);
         async move {
-            assert_eq!(counter.fetch_add(1, Ordering::SeqCst), 0);
+            assert_eq!(counter.fetch_add(1, Ordering::AcqRel), 0);
             Executor::spawn_local(async {
                 // Should not crash
             });
         }
     });
     Executor::poll_local();
-    assert_eq!(counter.load(Ordering::SeqCst), 1);
+    assert_eq!(counter.load(Ordering::Acquire), 1);
 }
