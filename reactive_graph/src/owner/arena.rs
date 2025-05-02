c616c579e4fbeb41d17bf1d7822e339b426b9cc1
Zak Stucke <zakstucke@hotmail.co.uk>
2025-05-02 15:57:10 +0300

    Remove purposeful CI failure in leptos_debuginfo mode now confirmed CI would catch it

--- reactive_graph/src/owner/arena.rs
+++ reactive_graph/src/owner/arena.rs
@@ -17,9 +17,6 @@ new_key_type! {
     pub struct NodeId;
 }
 
-#[cfg(leptos_debuginfo)]
-compile_error!("Temporary CI check");
-
 pub struct Arena;
 
 pub type ArenaMap = SlotMap<NodeId, Box<dyn Any + Send + Sync>>;
