40c4805090088fb6f9207a915a4dd31337d8abf9
Serhii Shliakhov <husband.sergey@gmail.com>
2025-05-08 18:49:32 +0100

    Fix deprecated parameters js warning

--- leptos/src/hydration/island_script.js
+++ leptos/src/hydration/island_script.js
@@ -55,7 +55,7 @@
 	idle(() => {
 		import(`${root}/${pkg_path}/${output_name}.js`)
 			.then(mod => {
-				mod.default(`${root}/${pkg_path}/${wasm_output_name}.wasm`).then(() => {
+				mod.default({module_or_path: `${root}/${pkg_path}/${wasm_output_name}.wasm`}).then(() => {
 					mod.hydrate();
 					hydrateIslands(document.body, mod);
 				});
