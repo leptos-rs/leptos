89e47945c33cd06186766d7b6370274173d7b28a
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-05-31 20:59:52 +0200

    wip

--- router/src/matching/resolve_path.rs
+++ router/src/matching/resolve_path.rs
@@ -1,21 +1,29 @@
 use std::borrow::Cow;
 
+/// Resolves `path` relative to optional `from` and prefixes `base`,
+/// `from` is the path to navigate from, `path` is the path to navigate to. `base` is the prefix for all paths.
 pub fn resolve_path<'a>(
     base: &'a str,
     path: &'a str,
     from: Option<&'a str>,
 ) -> Cow<'a, str> {
     if has_scheme(path) {
+        // don't change absolute urls
         path.into()
     } else {
         let base_path = normalize(base, false);
         let from_path = from.map(|from| normalize(from, false));
+
+        // calculate the prefix, except if result is empty, then / is the prefix
         let result = if let Some(from_path) = from_path {
             if path.starts_with('/') {
+                // if path is absolute, ignore from, use base
                 base_path
             } else if from_path.find(base_path.as_ref()) != Some(0) {
+                // path is not absolute and from does not start with base, prefix base and from
                 base_path + from_path
             } else {
+                // path is not absolute and from starts with base, prefix from (and therefore also base)
                 from_path
             }
         } else {
@@ -44,6 +52,7 @@ fn has_scheme(path: &str) -> bool {
 }
 
 #[doc(hidden)]
+/// Removes duplicate starting and ending slashes and if omit_slash is true removes the starting slash.
 fn normalize(path: &str, omit_slash: bool) -> Cow<'_, str> {
     let s = path.trim_start_matches('/');
     let trim_end = s
@@ -60,6 +69,7 @@ fn normalize(path: &str, omit_slash: bool) -> Cow<'_, str> {
     }
 }
 
+/// Returns whether the string starts with a `#` or `?`.
 fn begins_with_query_or_hash(text: &str) -> bool {
     matches!(text.chars().next(), Some('#') | Some('?'))
 }
