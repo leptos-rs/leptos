0226ef2223b5c58a9793a1a1ff491814df5658fc
Brian Carlsen <carlsen.bri@gmail.com>
2025-06-15 10:48:36 +0200

    Add inert svg elements.

--- tachys/src/svg/mod.rs
+++ tachys/src/svg/mod.rs
@@ -1,11 +1,18 @@
+use crate::html::attribute::any_attribute::AnyAttribute;
 use crate::{
     html::{
         attribute::Attribute,
         element::{ElementType, ElementWithChildren, HtmlElement},
     },
-    view::Render,
+    hydration::Cursor,
+    prelude::{AddAnyAttr, Mountable},
+    renderer::{
+        dom::{Element, Node},
+        CastFrom, Rndr,
+    },
+    view::{Position, PositionState, Render, RenderHtml},
 };
-use std::fmt::Debug;
+use std::{borrow::Cow, fmt::Debug};
 
 macro_rules! svg_elements {
 	($($tag:ident  [$($attr:ty),*]),* $(,)?) => {
@@ -185,3 +192,123 @@ impl ElementType for Use {
 }
 
 impl ElementWithChildren for Use {}
+
+/// An element that contains no interactivity, and whose contents can be known at compile time.
+pub struct InertElement {
+    html: Cow<'static, str>,
+}
+
+impl InertElement {
+    /// Creates a new inert svg element.
+    pub fn new(html: impl Into<Cow<'static, str>>) -> Self {
+        Self { html: html.into() }
+    }
+}
+
+/// Retained view state for [`InertElement`].
+pub struct InertElementState(Cow<'static, str>, Element);
+
+impl Mountable for InertElementState {
+    fn unmount(&mut self) {
+        self.1.unmount();
+    }
+
+    fn mount(&mut self, parent: &Element, marker: Option<&Node>) {
+        self.1.mount(parent, marker)
+    }
+
+    fn insert_before_this(&self, child: &mut dyn Mountable) -> bool {
+        self.1.insert_before_this(child)
+    }
+
+    fn elements(&self) -> Vec<crate::renderer::types::Element> {
+        vec![self.1.clone()]
+    }
+}
+
+impl Render for InertElement {
+    type State = InertElementState;
+
+    fn build(self) -> Self::State {
+        let el = Rndr::create_element_from_html(&self.html);
+        InertElementState(self.html, el)
+    }
+
+    fn rebuild(self, state: &mut Self::State) {
+        let InertElementState(prev, el) = state;
+        if &self.html != prev {
+            let mut new_el = Rndr::create_element_from_html(&self.html);
+            el.insert_before_this(&mut new_el);
+            el.unmount();
+            *el = new_el;
+            *prev = self.html;
+        }
+    }
+}
+
+impl AddAnyAttr for InertElement {
+    type Output<SomeNewAttr: Attribute> = Self;
+
+    fn add_any_attr<NewAttr: Attribute>(
+        self,
+        _attr: NewAttr,
+    ) -> Self::Output<NewAttr>
+    where
+        Self::Output<NewAttr>: RenderHtml,
+    {
+        panic!(
+            "InertElement does not support adding attributes. It should only \
+             be used as a child, and not returned at the top level."
+        )
+    }
+}
+
+impl RenderHtml for InertElement {
+    type AsyncOutput = Self;
+    type Owned = Self;
+
+    const MIN_LENGTH: usize = 0;
+
+    fn html_len(&self) -> usize {
+        self.html.len()
+    }
+
+    fn dry_resolve(&mut self) {}
+
+    async fn resolve(self) -> Self {
+        self
+    }
+
+    fn to_html_with_buf(
+        self,
+        buf: &mut String,
+        position: &mut Position,
+        _escape: bool,
+        _mark_branches: bool,
+        _extra_attrs: Vec<AnyAttribute>,
+    ) {
+        buf.push_str(&self.html);
+        *position = Position::NextChild;
+    }
+
+    fn hydrate<const FROM_SERVER: bool>(
+        self,
+        cursor: &Cursor,
+        position: &PositionState,
+    ) -> Self::State {
+        let curr_position = position.get();
+        if curr_position == Position::FirstChild {
+            cursor.child();
+        } else if curr_position != Position::Current {
+            cursor.sibling();
+        }
+        let el = crate::renderer::types::Element::cast_from(cursor.current())
+            .unwrap();
+        position.set(Position::NextChild);
+        InertElementState(self.html, el)
+    }
+
+    fn into_owned(self) -> Self::Owned {
+        self
+    }
+}
