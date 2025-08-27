231eff5a0354e803b38822d28f4e5db831d031f8
Gabriel Lopes Veiga <veigo@veigo.dev>
2025-08-26 22:07:28 -0300

    fix: use wss for live reload if on https (#4257)

--- leptos/src/hydration/reload_script.js
+++ leptos/src/hydration/reload_script.js
@@ -1,3 +1,7 @@
+if (window.location.protocol === 'https:') {
+	protocol = 'wss://';
+}
+
 let host = window.location.hostname;
 let ws = new WebSocket(`${protocol}${host}:${reload_port}/live_reload`);
 ws.onmessage = (ev) => {
