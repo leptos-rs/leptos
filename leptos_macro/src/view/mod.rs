4e445f43d688802c38b34af7b43274ff01c19353
Greg Johnston <greg.johnston@gmail.com>
2025-09-24 17:01:25 -0400

    fix: correctly import scoped slots (closes #4311) (#4318)

--- leptos_macro/src/view/mod.rs
+++ leptos_macro/src/view/mod.rs
@@ -25,9 +25,8 @@ use std::{
 use syn::{
     punctuated::Pair::{End, Punctuated},
     spanned::Spanned,
-    Expr,
-    Expr::Tuple,
-    ExprArray, ExprLit, ExprRange, Lit, LitStr, RangeLimits, Stmt,
+    Expr::{self, Tuple},
+    ExprArray, ExprLit, ExprPath, ExprRange, Lit, LitStr, RangeLimits, Stmt,
 };
 
 #[derive(Clone, Copy, PartialEq, Eq)]
@@ -1868,6 +1867,28 @@ pub(crate) fn ident_from_tag_name(tag_name: &NodeName) -> Ident {
             &tag_name.to_string().replace(['-', ':'], "_"),
             tag_name.span(),
         ),
+    }
+}
+
+pub(crate) fn full_path_from_tag_name(tag_name: &NodeName) -> Option<ExprPath> {
+    match tag_name {
+        NodeName::Path(path) => Some(path.clone()),
+        NodeName::Block(_) => {
+            let span = tag_name.span();
+            proc_macro_error2::emit_error!(
+                span,
+                "blocks not allowed in tag-name position"
+            );
+            None
+        }
+        _ => {
+            let span = tag_name.span();
+            proc_macro_error2::emit_error!(
+                span,
+                "punctuated names not allowed in slots"
+            );
+            None
+        }
     }
 }
 
