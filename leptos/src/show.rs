334d03b56e66c0fce77151fa8db35052f61f7e78
maccesch <maccesch@synphonyte.com>
2025-11-24 06:40:00 +0100

    Updated/reverted the Show... components

--- leptos/src/show.rs
+++ leptos/src/show.rs
@@ -1,11 +1,9 @@
 use crate::{
     children::{TypedChildrenFn, ViewFn},
-    prelude::FunctionMarker,
     IntoView,
 };
 use leptos_macro::component;
 use reactive_graph::{computed::ArcMemo, traits::Get};
-use std::{marker::PhantomData, sync::Arc};
 use tachys::either::Either;
 
 /// Shows its children whenever the condition `when` prop is `true`.
@@ -47,71 +45,24 @@ use tachys::either::Either;
 /// # }
 /// ```
 #[component]
-pub fn Show<M, C>(
+pub fn Show<W, C>(
     /// The children will be shown whenever the condition in the `when` closure returns `true`.
     children: TypedChildrenFn<C>,
-    /// When true the children are shown, otherwise the fallback.
-    /// It accepts a closure that returns a boolean value as well as a boolean signal or plain boolean value.
-    when: impl IntoCondition<M>,
+    /// A closure that returns a bool that determines whether this thing runs
+    when: W,
     /// A closure that returns what gets rendered if the when statement is false. By default this is the empty view.
     #[prop(optional, into)]
     fallback: ViewFn,
-
-    /// Marker for generic parameters. Ignore this.
-    #[prop(optional)]
-    _marker: PhantomData<M>,
 ) -> impl IntoView
 where
+    W: Fn() -> bool + Send + Sync + 'static,
     C: IntoView + 'static,
 {
-    let when = when.into_condition();
-    let memoized_when = ArcMemo::new(move |_| when.run());
+    let memoized_when = ArcMemo::new(move |_| when());
     let children = children.into_inner();
 
     move || match memoized_when.get() {
         true => Either::Left(children()),
         false => Either::Right(fallback.run()),
-    }
-}
-
-/// A closure that returns a bool. Can be converted from a closure, a signal, or a boolean value.
-pub struct Condition(Arc<dyn Fn() -> bool + Send + Sync + 'static>);
-
-impl Condition {
-    /// Evaluates the condition and returns its result.
-    pub fn run(&self) -> bool {
-        (self.0)()
-    }
-}
-
-/// Trait to convert various types into a `Condition`.
-/// Implemented for closures, signals, and boolean values.
-pub trait IntoCondition<M> {
-    /// Does the conversion
-    fn into_condition(self) -> Condition;
-}
-
-#[cfg(not(feature = "nightly"))]
-impl<S> IntoCondition<crate::prelude::SignalMarker> for S
-where
-    S: Get<Value = bool> + Send + Sync + 'static,
-{
-    fn into_condition(self) -> Condition {
-        Condition(Arc::new(move || self.get()))
-    }
-}
-
-impl<F> IntoCondition<FunctionMarker> for F
-where
-    F: Fn() -> bool + Send + Sync + 'static,
-{
-    fn into_condition(self) -> Condition {
-        Condition(Arc::new(self))
-    }
-}
-
-impl IntoCondition<Condition> for Condition {
-    fn into_condition(self) -> Condition {
-        self
     }
 }
--- leptos/src/show_let.rs
+++ leptos/src/show_let.rs
@@ -72,7 +72,7 @@ use tachys::either::Either;
 /// # }
 /// ```
 #[component]
-pub fn ShowLet<T, ChFn, V, M>(
+pub fn ShowLet<T, ChFn, V>(
     /// The children will be shown whenever `value` is `Some`.
     ///
     /// They take the inner value as an argument. Use `let:` to bind the value to a variable.
@@ -81,7 +81,8 @@ pub fn ShowLet<T, ChFn, V, M>(
     /// A signal of type `Option` or a closure that returns an `Option`.
     /// If the value is `Some`, the children will be shown.
     /// Otherwise the fallback will be shown, if present.
-    some: impl IntoOptionGetter<T, M>,
+    #[prop(into)]
+    some: Signal<T>,
 
     /// A closure that returns what gets rendered when the value is `None`.
     /// By default this is the empty view.
@@ -89,76 +90,19 @@ pub fn ShowLet<T, ChFn, V, M>(
     /// You can think of it as the closure inside `.unwrap_or_else(|| fallback())`.
     #[prop(optional, into)]
     fallback: ViewFn,
-
-    /// Marker for generic parameters. Ignore this.
-    #[prop(optional)]
-    _marker: PhantomData<(T, M)>,
 ) -> impl IntoView
 where
     ChFn: Fn(T) -> V + Send + Clone + 'static,
     V: IntoView + 'static,
     T: 'static,
 {
-    let getter = some.into_option_getter();
-
     move || {
         let children = children.clone();
         let fallback = fallback.clone();
 
-        getter
-            .run()
+        some
+            .get()
             .map(move |t| Either::Left(children(t)))
             .unwrap_or_else(move || Either::Right(fallback.run()))
-    }
-}
-
-/// Servers as a wrapper for both, an `Option` signal or a closure that returns an `Option`.
-pub struct OptionGetter<T>(Arc<dyn Fn() -> Option<T> + Send + Sync + 'static>);
-
-impl<T> Clone for OptionGetter<T> {
-    fn clone(&self) -> Self {
-        Self(Arc::clone(&self.0))
-    }
-}
-
-impl<T> OptionGetter<T> {
-    /// Runs the getter and returns the result.
-    pub fn run(&self) -> Option<T> {
-        (self.0)()
-    }
-}
-
-/// Conversion trait for creating an `OptionGetter` from a closure or a signal.
-pub trait IntoOptionGetter<T, M> {
-    /// Converts the given value into an `OptionGetter`.
-    fn into_option_getter(self) -> OptionGetter<T>;
-}
-
-/// Marker type for creating an `OptionGetter` from a closure.
-/// Used so that the compiler doesn't complain about double implementations of the trait `IntoOptionGetter`.
-pub struct FunctionMarker;
-
-impl<T, F> IntoOptionGetter<T, FunctionMarker> for F
-where
-    F: Fn() -> Option<T> + Send + Sync + 'static,
-{
-    fn into_option_getter(self) -> OptionGetter<T> {
-        OptionGetter(Arc::new(self))
-    }
-}
-
-#[cfg(not(feature = "nightly"))]
-/// Marker type for creating an `OptionGetter` from a signal.
-/// Used so that the compiler doesn't complain about double implementations of the trait `IntoOptionGetter`.
-pub struct SignalMarker;
-
-#[cfg(not(feature = "nightly"))]
-impl<T, S> IntoOptionGetter<T, SignalMarker> for S
-where
-    S: Get<Value = Option<T>> + Clone + Send + Sync + 'static,
-{
-    fn into_option_getter(self) -> OptionGetter<T> {
-        let cloned = self.clone();
-        OptionGetter(Arc::new(move || cloned.get()))
     }
 }
