c2bb4cfb0f18dcd4608949888ad62c0c77e0f4fc
Tiemen Schuijbroek <t.j.l.schuijbroek@ratio-case.nl>
2025-12-15 17:27:00 +0100

    fix: reduce key bound from Copy to Clone
    Signed-off-by: Tiemen Schuijbroek <t.j.l.schuijbroek@ratio-case.nl>

--- reactive_stores/src/keyed.rs
+++ reactive_stores/src/keyed.rs
@@ -482,7 +482,7 @@ where
 
 impl<Inner, Prev, K, T> AtKeyed<Inner, Prev, K, T>
 where
-    K: Copy + Debug + Send + Sync + PartialEq + Eq + Hash + 'static,
+    K: Clone + Debug + Send + Sync + PartialEq + Eq + Hash + 'static,
     KeyedSubfield<Inner, Prev, K, T>: Clone,
     for<'a> &'a T: IntoIterator,
     Inner: StoreField<Value = Prev>,
@@ -506,7 +506,7 @@ where
 
 impl<Inner, Prev, K, T> StoreField for AtKeyed<Inner, Prev, K, T>
 where
-    K: Copy + Debug + Send + Sync + PartialEq + Eq + Hash + 'static,
+    K: Clone + Debug + Send + Sync + PartialEq + Eq + Hash + 'static,
     KeyedSubfield<Inner, Prev, K, T>: Clone,
     for<'a> &'a T: IntoIterator,
     Inner: StoreField<Value = Prev>,
@@ -573,11 +573,16 @@ where
     fn reader(&self) -> Option<Self::Reader> {
         let inner = self.inner.reader()?;
         let index = self.resolve_index()?;
-        let key = self.key;
         Some(MappedMutArc::new(
             inner,
-            move |n| n.keyed(index, key),
-            move |n| n.keyed_mut(index, key),
+            {
+                let key = self.key.clone();
+                move |n| n.keyed(index, key.clone())
+            },
+            {
+                let key = self.key.clone();
+                move |n| n.keyed_mut(index, key.clone())
+            },
         ))
     }
 
@@ -585,14 +590,19 @@ where
         let mut inner = self.inner.writer()?;
         inner.untrack();
         let index = self.resolve_index()?;
-        let key = self.key;
         let triggers = self.triggers_for_current_path();
         Some(WriteGuard::new(
             triggers,
             MappedMutArc::new(
                 inner,
-                move |n| n.keyed(index, key),
-                move |n| n.keyed_mut(index, key),
+                {
+                    let key = self.key.clone();
+                    move |n| n.keyed(index, key.clone())
+                },
+                {
+                    let key = self.key.clone();
+                    move |n| n.keyed_mut(index, key.clone())
+                },
             ),
         ))
     }
@@ -631,7 +641,7 @@ where
 
 impl<Inner, Prev, K, T> Notify for AtKeyed<Inner, Prev, K, T>
 where
-    K: Copy + Debug + Send + Sync + PartialEq + Eq + Hash + 'static,
+    K: Clone + Debug + Send + Sync + PartialEq + Eq + Hash + 'static,
     KeyedSubfield<Inner, Prev, K, T>: Clone,
     for<'a> &'a T: IntoIterator,
     Inner: StoreField<Value = Prev>,
@@ -648,7 +658,7 @@ where
 
 impl<Inner, Prev, K, T> Track for AtKeyed<Inner, Prev, K, T>
 where
-    K: Copy + Debug + Send + Sync + PartialEq + Eq + Hash + 'static,
+    K: Clone + Debug + Send + Sync + PartialEq + Eq + Hash + 'static,
     KeyedSubfield<Inner, Prev, K, T>: Clone,
     for<'a> &'a T: IntoIterator,
     Inner: StoreField<Value = Prev>,
@@ -663,7 +673,7 @@ where
 
 impl<Inner, Prev, K, T> ReadUntracked for AtKeyed<Inner, Prev, K, T>
 where
-    K: Copy + Debug + Send + Sync + PartialEq + Eq + Hash + 'static,
+    K: Clone + Debug + Send + Sync + PartialEq + Eq + Hash + 'static,
     KeyedSubfield<Inner, Prev, K, T>: Clone,
     for<'a> &'a T: IntoIterator,
     Inner: StoreField<Value = Prev>,
@@ -680,7 +690,7 @@ where
 
 impl<Inner, Prev, K, T> Write for AtKeyed<Inner, Prev, K, T>
 where
-    K: Copy + Debug + Send + Sync + PartialEq + Eq + Hash + 'static,
+    K: Clone + Debug + Send + Sync + PartialEq + Eq + Hash + 'static,
     KeyedSubfield<Inner, Prev, K, T>: Clone,
     for<'a> &'a T: IntoIterator,
     Inner: StoreField<Value = Prev>,
@@ -739,7 +749,7 @@ where
     for<'a> &'a T: IntoIterator,
     Inner: Clone + StoreField<Value = Prev> + 'static,
     Prev: 'static,
-    K: Copy + Debug + Send + Sync + PartialEq + Eq + Hash + 'static,
+    K: Clone + Debug + Send + Sync + PartialEq + Eq + Hash + 'static,
     T: KeyedAccess<Key = K> + 'static,
     T::Value: Sized,
 {
