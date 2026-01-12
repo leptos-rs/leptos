de0e6c0e470605e07e46abed859699b0ed478a41
Tommy Yu <y@metatoaster.com>
2026-01-12 22:09:01 +1300

    Remove hardcoding ports for axum test services
    - Manuallying specifying ports will become annoying to keep track of for
      every different tests, instead require the server to report the port
      it started with and have parse that output into a helper type, which
      in turn provides a method to construct uri required for testing.

--- integrations/axum/tests/axum_integration.rs
+++ integrations/axum/tests/axum_integration.rs
@@ -4,19 +4,25 @@ use reqwest::{
 };
 use std::{
     path::Path,
+    process::Stdio,
     sync::Once,
     time::{Duration, Instant},
 };
-use tokio::process::{Child, Command};
+use tokio::{
+    io::AsyncReadExt,
+    process::{Child, Command},
+    time::timeout,
+};
 
 #[tokio::test]
 async fn bare_no_fallback() -> anyhow::Result<()> {
-    let host = "127.0.0.1:3010";
-    let _service = start_test_service("service_mode", host, "bare").await;
-    let url = url(host);
+    let service = start_test_service("service_mode", "bare").await;
     let client = Client::new();
     // this version has no fallbacks attached, so no other response, no error page.
-    let res = client.get(url.join("/pkg/service_mode.js")?).send().await?;
+    let res = client
+        .get(service.url("/pkg/service_mode.js")?)
+        .send()
+        .await?;
     assert_eq!(res.status(), StatusCode::NOT_FOUND);
     assert_eq!(res.content_length(), Some(0));
     Ok(())
@@ -24,22 +30,23 @@ async fn bare_no_fallback() -> anyhow::Result<()> {
 
 #[tokio::test]
 async fn fallback() -> anyhow::Result<()> {
-    let host = "127.0.0.1:3020";
-    let _service = start_test_service("service_mode", host, "fallback").await;
-    let url = url(host);
+    let service = start_test_service("service_mode", "fallback").await;
     let client = Client::new();
     // should provide the two site artifacts.
-    let res = client.get(url.join("/pkg/service_mode.js")?).send().await?;
+    let res = client
+        .get(service.url("/pkg/service_mode.js")?)
+        .send()
+        .await?;
     assert_eq!(res.status(), StatusCode::OK);
     assert_ne!(res.content_length(), Some(0));
     let res = client
-        .get(url.join("/pkg/service_mode.wasm")?)
+        .get(service.url("/pkg/service_mode.wasm")?)
         .send()
         .await?;
     assert_eq!(res.status(), StatusCode::OK);
     assert_ne!(res.content_length(), Some(0));
     // the basic fallback will also have a shell to render the 404 Not Found
-    let res = client.get(url.join("/pkg/no_such_path")?).send().await?;
+    let res = client.get(service.url("/pkg/no_such_path")?).send().await?;
     assert_eq!(res.status(), StatusCode::NOT_FOUND);
     assert_ne!(res.content_length(), Some(0));
     assert!(res
@@ -52,13 +59,11 @@ async fn fallback() -> anyhow::Result<()> {
 #[tokio::test]
 async fn fallback_with_context() -> anyhow::Result<()> {
     // ensure fixes implemented in #4394 for the headers to show up actually do show up.
-    let host = "127.0.0.1:3030";
-    let _service =
-        start_test_service("service_mode", host, "fallback-with-context").await;
-    let url = url(host);
+    let service =
+        start_test_service("service_mode", "fallback-with-context").await;
     let client = Client::new();
     let res = client
-        .get(url.join("/pkg/service_mode.wasm")?)
+        .get(service.url("/pkg/service_mode.wasm")?)
         .send()
         .await?;
     assert_eq!(res.status(), StatusCode::OK);
@@ -78,13 +83,14 @@ async fn fallback_with_context() -> anyhow::Result<()> {
 
 #[tokio::test]
 async fn error_handler_service() -> anyhow::Result<()> {
-    let host = "127.0.0.1:3040";
-    let _service =
-        start_test_service("service_mode", host, "error-handler-service").await;
-    let url = url(host);
+    let service =
+        start_test_service("service_mode", "error-handler-service").await;
     let client = Client::new();
     // no site artifact, but has the error page as only the error handler is applied
-    let res = client.get(url.join("/pkg/service_mode.js")?).send().await?;
+    let res = client
+        .get(service.url("/pkg/service_mode.js")?)
+        .send()
+        .await?;
     assert_eq!(res.status(), StatusCode::NOT_FOUND);
     assert_ne!(res.content_length(), Some(0));
     assert!(res
@@ -96,27 +102,25 @@ async fn error_handler_service() -> anyhow::Result<()> {
 
 #[tokio::test]
 async fn error_handler_service_fallback() -> anyhow::Result<()> {
-    let host = "127.0.0.1:3050";
-    let _service = start_test_service(
-        "service_mode",
-        host,
-        "error-handler-service-fallback",
-    )
-    .await;
-    let url = url(host);
+    let service =
+        start_test_service("service_mode", "error-handler-service-fallback")
+            .await;
     let client = Client::new();
     // should provide the two site artifacts.
-    let res = client.get(url.join("/pkg/service_mode.js")?).send().await?;
+    let res = client
+        .get(service.url("/pkg/service_mode.js")?)
+        .send()
+        .await?;
     assert_eq!(res.status(), StatusCode::OK);
     assert_ne!(res.content_length(), Some(0));
     let res = client
-        .get(url.join("/pkg/service_mode.wasm")?)
+        .get(service.url("/pkg/service_mode.wasm")?)
         .send()
         .await?;
     assert_eq!(res.status(), StatusCode::OK);
     assert_ne!(res.content_length(), Some(0));
     // this composed service falback setup is similar to the basic non-service fallback setup.
-    let res = client.get(url.join("/pkg/no_such_path")?).send().await?;
+    let res = client.get(service.url("/pkg/no_such_path")?).send().await?;
     assert_eq!(res.status(), StatusCode::NOT_FOUND);
     assert_ne!(res.content_length(), Some(0));
     assert!(res
@@ -128,29 +132,29 @@ async fn error_handler_service_fallback() -> anyhow::Result<()> {
 
 #[tokio::test]
 async fn route_site_pkg_no_fallback() -> anyhow::Result<()> {
-    let host = "127.0.0.1:3060";
-    let _service =
-        start_test_service("service_mode", host, "route-site-pkg-no-fallback")
-            .await;
-    let url = url(host);
+    let service =
+        start_test_service("service_mode", "route-site-pkg-no-fallback").await;
     let client = Client::new();
     // should provide the two site artifacts.
-    let res = client.get(url.join("/pkg/service_mode.js")?).send().await?;
+    let res = client
+        .get(service.url("/pkg/service_mode.js")?)
+        .send()
+        .await?;
     assert_eq!(res.status(), StatusCode::OK);
     assert_ne!(res.content_length(), Some(0));
     let res = client
-        .get(url.join("/pkg/service_mode.wasm")?)
+        .get(service.url("/pkg/service_mode.wasm")?)
         .send()
         .await?;
     assert_eq!(res.status(), StatusCode::OK);
     assert_ne!(res.content_length(), Some(0));
     // there is no fallback assigned to the routes under /pkg/ under this setup, so no error page
-    let res = client.get(url.join("/pkg/no_such_path")?).send().await?;
+    let res = client.get(service.url("/pkg/no_such_path")?).send().await?;
     assert_eq!(res.status(), StatusCode::NOT_FOUND);
     assert_eq!(res.content_length(), Some(0));
     // however, the fallback service will trigger for all other unrouted paths.
     let res = client
-        .get(url.join("/no_such_path_elsewhere")?)
+        .get(service.url("/no_such_path_elsewhere")?)
         .send()
         .await?;
     assert_eq!(res.status(), StatusCode::NOT_FOUND);
@@ -161,8 +165,6 @@ async fn route_site_pkg_no_fallback() -> anyhow::Result<()> {
         .contains("<title>Error from fallback</title>"));
     Ok(())
 }
-
-static BUILDER: Once = Once::new();
 
 // Killing `cargo leptos watch` may not necessarily kill the underlying server task, so rather
 // than running that, build and run the service in separate steps.  This also has the advantage
@@ -195,35 +197,73 @@ fn build_test_service(name: &str) {
     }
 }
 
-async fn start_test_service(name: &str, host: &str, mode: &str) -> Child {
-    BUILDER.call_once(|| build_test_service("service_mode"));
+struct Service {
+    _child: Child,
+    port: u16,
+}
 
+impl Service {
+    fn url(&self, path: &str) -> anyhow::Result<Url> {
+        Ok(format!("http://127.0.0.1:{}/", self.port)
+            .parse::<Url>()?
+            .join(path)?)
+    }
+}
+
+static BUILDER: Once = Once::new();
+
+async fn start_test_service(name: &str, mode: &str) -> Service {
+    BUILDER.call_once(|| build_test_service("service_mode"));
+    // the time limit to wait for service to start and listen
+    let ttl = Duration::from_secs(5);
     // this assumes the current working dir is at the root of this crate, i.e. `integration/axum`.
     let working_dir = Path::new("tests").join(name);
 
-    let child = Command::new(Path::new("target").join("debug").join(name))
+    let mut child = Command::new(Path::new("target").join("debug").join(name))
         .arg(mode)
         .kill_on_drop(true)
         .current_dir(&working_dir)
-        .env("LEPTOS_SITE_ADDR", host)
+        .env("LEPTOS_SITE_ADDR", "127.0.0.1:0")
         // need to manually specify this to avoid mismatch between this value that may be set (e.g.
         // during CI) and the `output-name` defined in Cargo.toml for this relevant project.
         .env("LEPTOS_OUTPUT_NAME", name)
+        .stdout(Stdio::piped())
         .spawn()
         .expect("the service should have been built and can start");
 
+    let mut stdout = child.stdout.take().expect("stdout is not captured");
+
+    let buff = tokio::spawn(timeout(ttl, async move {
+        let mut buff = Vec::new();
+        let _ = stdout.read_buf(&mut buff).await;
+        buff
+    }))
+    .await
+    .unwrap();
+
     let start_time = Instant::now();
 
-    let url = format!("http://{host}");
-    while start_time.elapsed() < Duration::from_secs(300) {
-        if reqwest::get(&url).await.is_ok() {
-            return child;
+    let port = str::from_utf8(&buff.unwrap())
+        .unwrap()
+        .trim()
+        .parse()
+        .unwrap();
+
+    let _child = child;
+    let service = Service { _child, port };
+    let client = Client::new();
+
+    while start_time.elapsed() < ttl {
+        if client
+            .get(service.url("/").unwrap())
+            .timeout(ttl)
+            .send()
+            .await
+            .is_ok()
+        {
+            return service;
         }
         tokio::time::sleep(Duration::from_secs(1)).await;
     }
     panic!("The web server did not become ready within the expected time.");
-}
-
-fn url(host: &str) -> Url {
-    format!("http://{host}").parse().expect("normal valid host")
 }
--- integrations/axum/tests/service_mode/src/main.rs
+++ integrations/axum/tests/service_mode/src/main.rs
@@ -127,6 +127,8 @@ async fn main() {
     let conf = get_configuration(None).unwrap();
     let addr = conf.leptos_options.site_addr;
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    // write out the port from the bounded local_addr to allow the caller to know how to connect.
+    println!("{}", listener.local_addr().unwrap().port());
     axum::serve(listener, app.into_make_service())
         .await
         .unwrap();
