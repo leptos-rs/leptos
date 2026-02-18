0720423bb2033310ade1efacd836649958e0c615
Lukas Potthast <privat@lukas-potthast.de>
2026-02-18 21:41:00 +0100

    Revert example changes

--- examples/counter/src/lib.rs
+++ examples/counter/src/lib.rs
@@ -21,23 +21,3 @@ pub fn SimpleCounter(
         </div>
     }
 }
-
-#[component]
-fn MultipleGenericsFirstInvalid() -> impl IntoView {
-    view! {
-        <div>
-            <Inner fun_a=|| true/>
-        </div>
-    }
-}
-
-#[component]
-fn Inner<F, G>(fun_a: F, fun_b: G) -> impl IntoView
-where
-    F: Fn() -> bool,
-    G: Fn() -> i32,
-{
-    let _ = fun_a();
-    let _ = fun_b();
-    ()
-}
