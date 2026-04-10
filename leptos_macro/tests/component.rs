cc6f60e42640e58438f99c687697fd51e1ad3dd7
Greg Johnston <greg.johnston@gmail.com>
2026-04-10 12:06:58 -0400

    chore: update tests to match new nightly approach to fn signals

--- leptos_macro/tests/component.rs
+++ leptos_macro/tests/component.rs
@@ -120,7 +120,6 @@ fn returns_static_lifetime() {
     }
 }
 
-#[cfg(not(all(feature = "nightly", rustc_nightly)))]
 #[component]
 pub fn IntoReactiveValueTestComponentSignal(
     #[prop(into)] arg1: Signal<String>,
@@ -196,7 +195,6 @@ pub fn IntoReactiveValueTestComponentCallback(
     }
 }
 
-#[cfg(not(all(feature = "nightly", rustc_nightly)))]
 #[test]
 fn test_into_reactive_value_signal() {
     let _ = view! {
