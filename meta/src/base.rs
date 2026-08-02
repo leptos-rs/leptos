4f838c99ca6e5486f45e53d961ee06fc15f13471
Hanyuan Li <work@hanyuone.live>
2026-08-02 17:03:41 +1000

    feat: have <base> handle leading/trailing slashes in `SITE_BASE`

--- meta/src/base.rs
+++ meta/src/base.rs
@@ -4,9 +4,21 @@ use leptos::{component, config::LeptosOptions, html::base, IntoView};
 #[component]
 pub fn Base(
     /// Leptos options, which potentially contains a `site_base` that
-    /// we use as the `href` for the `<base>` element.
+    /// informs the `href` for the `<base>` element.
     options: LeptosOptions,
 ) -> impl IntoView {
-    let base_href = format!("{}{}", options.site_addr, options.site_base);
+    let base_href = if options.site_base.is_empty() {
+        format!("{}/", options.site_addr)
+    } else {
+        // Handles `href` for a variety of `site_base` arrangements
+        // outside of the expected `foo` (no leading/trailing slashes),
+        // e.g. `/`, `/foo`, `/foo/`
+        format!(
+            "{}/{}/",
+            options.site_addr,
+            options.site_base.trim_matches("/")
+        )
+    };
+
     base().href(base_href)
 }
