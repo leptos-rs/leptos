c692c183c8bba3c5f63472d3e682fbbf2e47c27e
Hanyuan Li <work@hanyuone.live>
2026-08-15 20:59:00 +1000

    fix: revert to `site_base` being full URL

--- meta/src/base.rs
+++ meta/src/base.rs
@@ -7,18 +7,5 @@ pub fn Base(
     /// informs the `href` for the `<base>` element.
     options: LeptosOptions,
 ) -> impl IntoView {
-    let base_href = if options.site_base.is_empty() {
-        format!("{}/", options.site_addr)
-    } else {
-        // Handles `href` for a variety of `site_base` arrangements
-        // outside of the expected `foo` (no leading/trailing slashes),
-        // e.g. `/`, `/foo`, `/foo/`
-        format!(
-            "{}/{}/",
-            options.site_addr,
-            options.site_base.trim_matches("/")
-        )
-    };
-
-    base().href(base_href)
+    base().href(options.site_base)
 }
